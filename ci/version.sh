1e81c148853fdb5cb16e70ea6bd77fdda213d038
Ben Chidgey <ben@feedmypixel.com>
2023-04-20 09:05:21 +0100

    Add auto bot details

--- ci/version.sh
+++ ci/version.sh
@@ -4,6 +4,9 @@ IFS=$'\n\t'
 
 SEMVER_STRING=$1
 
+git config --global user.name "GitHub Actions Robot 🤖"
+git config --global user.email ""
+
 echo "Bump code version"
 
 npm version "$SEMVER_STRING"
