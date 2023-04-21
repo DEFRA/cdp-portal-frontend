5233ea702e1fa0dd6973f9887a7569388ad63731
Ben Chidgey <ben@feedmypixel.com>
2023-04-21 10:23:02 +0100

    Add [bot] suffix to bot name

--- ci/version.sh
+++ ci/version.sh
@@ -6,8 +6,8 @@ SEMVER_STRING=$1
 
 echo "Bump code version"
 
-git config user.name 'cdp-gh-bot'
-git config user.email '321835+cdp-gh-bot@users.noreply.github.com'
+git config user.name 'cdp-gh-bot[bot]'
+git config user.email '321835+cdp-gh-bot[bot]@users.noreply.github.com'
 
 npm version "$SEMVER_STRING"
 git push
