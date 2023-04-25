ca89c0ab5a063dfe948bbb6a03126c1369bc2112
Ben Chidgey <ben@feedmypixel.com>
2023-04-25 14:34:26 +0100

    Rejig of versioning

--- ci/version.sh
+++ ci/version.sh
@@ -15,29 +15,26 @@ echo "Fetch all tags"
 
 git fetch --depth=1 origin +refs/tags/*:refs/tags/*
 
-if [ $(git tag -l "$BUILD_VERSION") ]; then
+NPM_VERSION=$(npm pkg get version)
 
-    echo "exists"
+if [ $(git tag -l $NPM_VERSION) ]; then
 # If current version tag exists on GitHub then bump minor version and push commit and tag
     npm version minor
-    NEW_VERSION=$(npm pkg get version)
+    NEW_NPM_VERSION=$(npm pkg get version)
 
-    echo "$NEW_VERSION"
+    echo "NPM version bumped to $NEW_NPM_VERSION"
 
-    git tag "$NEW_VERSION"
+    git tag $NEW_NPM_VERSION
     git push
-    git push origin "$NEW_VERSION"
+    git push origin $NEW_NPM_VERSION
 else
 # If current version tag does not exist on GitHub then tag code and push tag.
 # This will happen with developer created Major or Patch, or initial release
 
-    echo "does not exist"
-    echo "$BUILD_VERSION"
-    $(git tag -l "$BUILD_VERSION")
+    echo "$NPM_VERSION tag does not exist, creating tag $NPM_VERSION"
 
-
-    git tag "$BUILD_VERSION"
-    git push origin "$BUILD_VERSION"
+    git tag $NPM_VERSION
+    git push origin $NPM_VERSION
 fi
 
 
