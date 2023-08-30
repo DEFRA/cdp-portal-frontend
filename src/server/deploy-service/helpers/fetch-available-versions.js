16598290fa869b91e400a8d014f32eeaebc11383
Ben Chidgey <ben@feedmypixel.com>
2023-08-30 12:55:21 +0100

    Update url to point to new backend

--- src/server/deploy-service/helpers/fetch-available-versions.js
+++ src/server/deploy-service/helpers/fetch-available-versions.js
@@ -3,13 +3,8 @@ import fetch from 'node-fetch'
 import { appConfig } from '~/src/config'
 
 async function fetchAvailableVersions(serviceName) {
-  // TODO fix up deployables API. At the moment if no serviceName is provided then it returns /deployables endpoint JSON
-  if (!serviceName) {
-    return []
-  }
-
   const deployablesVersionsEndpoint =
-    appConfig.get('deployablesApiUrl') + `/deployables/${serviceName}`
+    appConfig.get('portalBackendApiUrl') + `/deployables/${serviceName}`
 
   const response = await fetch(deployablesVersionsEndpoint, {
     method: 'get',
