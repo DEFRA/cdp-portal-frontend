7ecec2407c26d05e193b57df38951800b22abc48
Mohammed Abdulrazeg <github@mzawi.me>
2023-09-11 16:50:28 +0100

    attempting to fix health auth race condition

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,6 @@
 import path from 'path'
 import hapi from '@hapi/hapi'
 import bell from '@hapi/bell'
-import jwt from '@hapi/jwt'
 import qs from 'qs'
 
 import { router } from './router'
@@ -12,6 +11,7 @@ import { catchAll } from '~/src/server/common/helpers/errors'
 import { flashMessage } from '~/src/server/common/helpers/flash-message'
 import { requestLogger } from '~/src/server/common/helpers/request-logger'
 import { addFlashMessagesToContext } from '~/src/server/common/helpers/add-flash-messages-to-context'
+import { azureOidc } from '~/src/server/common/helpers/azure-oidc'
 
 async function createServer() {
   const server = hapi.server({
@@ -37,45 +37,6 @@ async function createServer() {
 
   await server.register(bell)
 
-  server.auth.strategy('azure-oidc', 'bell', {
-    provider: {
-      name: 'azure-oidc',
-      protocol: 'oauth2',
-      useParamsAuth: true,
-      auth: `https://login.microsoftonline.com/${appConfig.get(
-        'azureTenantId'
-      )}/oauth2/v2.0/authorize`,
-      token: `https://login.microsoftonline.com/${appConfig.get(
-        'azureTenantId'
-      )}/oauth2/v2.0/token`,
-      scope: [
-        `api://${appConfig.get('azureClientId')}/cdp.user`,
-        'openid',
-        'profile',
-        'email',
-        'offline_access',
-        'user.read'
-      ],
-      profile: async function (credentials, params, get) {
-        const payload = jwt.token.decode(credentials.token).decoded.payload
-        credentials.profile = {
-          id: payload.oid,
-          displayName: payload.name,
-          email: payload.upn ?? payload.preferred_username,
-          groups: payload.groups
-        }
-      }
-    },
-    password: appConfig.get('sessionCookiePassword'),
-    clientId: appConfig.get('azureClientId'),
-    clientSecret: appConfig.get('azureClientSecret'),
-    cookie: 'bell-azure-oidc',
-    isSecure: !appConfig.get('isDevelopment'),
-    config: {
-      tenant: appConfig.get('azureTenantId')
-    }
-  })
-
   server.ext('onPreResponse', addFlashMessagesToContext, {
     before: ['yar']
   })
@@ -83,6 +44,8 @@ async function createServer() {
   await server.register(flashMessage)
 
   await server.register(requestLogger)
+
+  await server.register(azureOidc)
 
   await server.register(router, {
     routes: { prefix: appConfig.get('appPathPrefix') }
