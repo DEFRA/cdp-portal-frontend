b6dd3da4474733963065e6d7c3efca219444dca3
Mohammed Abdulrazeg <github@mzawi.me>
2023-09-12 11:44:15 +0100

    Using base url for logout, cleaning up logging for login

--- src/server/logout/controller.js
+++ src/server/logout/controller.js
@@ -5,7 +5,7 @@ const logoutController = {
     const logoutUrl = `https://login.microsoftonline.com/${appConfig.get(
       'azureTenantId'
     )}/oauth2/logout?post_logout_redirect_uri=${encodeURIComponent(
-      `${appConfig.get('appBaseUrl')}${appConfig.get('appPathPrefix')}`
+      appConfig.get('appBaseUrl')
     )}`
     request.yar.clear('auth')
     return h.redirect(logoutUrl)
