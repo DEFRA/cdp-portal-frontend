15698770b42c567fdd1ec4a3493077bb1e3f5c8e
Ben Chidgey <ben@feedmypixel.com>
2023-09-27 16:39:03 +0100

    237969: Login and logout controllers

--- src/server/logout/controller.js
+++ src/server/logout/controller.js
@@ -1,24 +1,22 @@
 import { appConfig } from '~/src/config'
-import { provideAuth } from '~/src/server/logout/prerequisites/provide-auth'
+import { provideUser } from '~/src/server/logout/prerequisites/provide-user'
 import { sessionNames } from '~/src/server/common/constants/session-names'
 
 const logoutController = {
   options: {
-    pre: [provideAuth]
+    pre: [provideUser]
   },
   handler: async (request, h) => {
-    const appBaseUrl =
-      appConfig.get('appBaseUrl') + appConfig.get('appPathPrefix')
+    const referrer = request.info.referrer
+    const loginHint = request.pre.user.loginHint
+    const azureTenantId = appConfig.get('azureTenantId')
 
     const logoutUrl = encodeURI(
-      `https://login.microsoftonline.com/${appConfig.get(
-        'azureTenantId'
-      )}/oauth2/logout?logout_hint=${
-        request.pre.auth.credentials.profile.loginHint
-      }&post_logout_redirect_uri=${appBaseUrl}`
+      `https://login.microsoftonline.com/${azureTenantId}/oauth2/logout?logout_hint=${loginHint}&post_logout_redirect_uri=${referrer}`
     )
 
-    request.yar.clear(sessionNames.auth)
+    request.yar.clear(sessionNames.user)
+    request.cookieAuth.clear()
 
     return h.redirect(logoutUrl)
   }
