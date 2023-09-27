a91456017c6324cf5a73cea837179b50d6bac7c4
Ben Chidgey <ben@feedmypixel.com>
2023-09-27 16:36:51 +0100

    237969: turn off auth for static files

--- src/server/common/helpers/serve-static-files.js
+++ src/server/common/helpers/serve-static-files.js
@@ -2,21 +2,22 @@ import { appConfig } from '~/src/config'
 
 const serveStaticFiles = {
   plugin: {
-    name: 'Serve static files',
+    name: 'staticFiles',
     register: async (server) => {
       server.route({
+        options: {
+          auth: false,
+          cache: {
+            expiresIn: appConfig.get('staticCacheTimeout'),
+            privacy: 'private'
+          }
+        },
         method: 'GET',
         path: '/public/{param*}',
         handler: {
           directory: {
             path: '.',
             redirectToSlash: true
-          }
-        },
-        config: {
-          cache: {
-            expiresIn: appConfig.get('staticCacheTimeout'),
-            privacy: 'private'
           }
         }
       })
