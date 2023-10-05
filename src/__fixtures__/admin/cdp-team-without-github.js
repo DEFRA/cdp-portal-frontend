3a8cf4924119d6b623c50cf7489f3ba13dee60b8
Ben Chidgey <ben@feedmypixel.com>
2023-10-05 12:09:52 +0100

    239602: update fixtures

---
+++ src/__fixtures__/admin/cdp-team-without-github.js
@@ -0,0 +1,30 @@
+const cdpTeamWithoutGithubFixture = {
+  message: 'success',
+  team: {
+    name: 'Admin',
+    description: 'The Admin team 💪🏻',
+    createdAt: '2023-08-23T16:18:28.742Z',
+    updatedAt: '2023-08-30T08:03:23.657Z',
+    teamId: '47c04343-4c0e-4326-9848-bef7c1e2eedd',
+    users: [
+      {
+        userId: '014ee26b-e738-4bcc-9621-a5289dba7351',
+        name: 'Darth, Vader'
+      },
+      {
+        userId: '02af05d1-2bbb-4094-a847-b9eddcf0d6bb',
+        name: 'Roger, Rabbit'
+      },
+      {
+        userId: '0ddadf17-beaf-4aef-a415-ca044dbdd18d',
+        name: 'The Terminator'
+      },
+      {
+        userId: '1398fa86-98a2-4ee8-84bb-2468cc71d0ec',
+        name: 'RoboCop'
+      }
+    ]
+  }
+}
+
+export { cdpTeamWithoutGithubFixture }
