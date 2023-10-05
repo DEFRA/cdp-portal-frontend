f2db183c551b884e9ce85350e85cb67ecf281fe4
Ben Chidgey <ben@feedmypixel.com>
2023-10-05 12:10:20 +0100

    239602: Switch require to import

--- src/server/common/helpers/logger.js
+++ src/server/common/helpers/logger.js
@@ -1,7 +1,7 @@
 import pino from 'pino'
+import ecsFormat from '@elastic/ecs-pino-format'
 
 import { config } from '~/src/config'
-const ecsFormat = require('@elastic/ecs-pino-format')
 
 function createLogger() {
   return pino({
