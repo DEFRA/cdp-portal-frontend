d943d0e3b8cbb0b9a5acc1dd65ea08158890cf52
Ben Chidgey <ben@feedmypixel.com>
2023-10-10 09:57:19 +0100

    Remove auth logging

--- src/server/auth/controller.js
+++ src/server/auth/controller.js
@@ -9,15 +9,16 @@ const authCallbackController = {
   },
   handler: async (request, h) => {
     if (request.auth.isAuthenticated) {
-      const { profile } = request.auth.credentials
       const expiresInSeconds = request.auth.credentials.expiresIn
       const expiresInMilliSeconds = expiresInSeconds * 1000
       const expiresAt = addSeconds(new Date(), expiresInSeconds)
 
+      const { profile } = request.auth.credentials
       const isAdmin = profile.groups.includes(config.get('azureAdminGroupId'))
       const userGroups = profile.groups
 
       const sessionId = uuidv4()
+
       await request.server.app.cache.set(sessionId, {
         id: profile.id,
         email: profile.email,
@@ -33,12 +34,6 @@ const authCallbackController = {
       })
 
       request.cookieAuth.set({ sessionId })
-
-      request.logger.info('User has been successfully authenticated')
-      request.logger.info(`expiresIn: ${expiresInSeconds}`)
-      request.logger.info(`Admin groups is: ${config.get('azureAdminGroupId')}`)
-      request.logger.info(`User is admin: ${isAdmin}`)
-      request.logger.info(`User groups are: ${userGroups.join(', ')}`)
     }
 
     const redirect =
