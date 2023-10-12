344023fa297df35364cf5d621914073b2301c4b3
Ben Chidgey <ben@feedmypixel.com>
2023-10-12 14:52:45 +0100

    Update all user session with refreshToken payload
    This work will refresh all user properties. So if any of them change
    when a token successfully refreshes the proerties will be updated

--- src/server/common/helpers/auth/user-session.js
+++ src/server/common/helpers/auth/user-session.js
@@ -1,4 +1,7 @@
+import jwt from '@hapi/jwt'
 import { addSeconds } from 'date-fns'
+
+import { config } from '~/src/config'
 
 function removeUserSession(request) {
   request.dropUserSession()
@@ -6,15 +9,27 @@ function removeUserSession(request) {
 }
 
 async function updateUserSession(request, refreshedSession) {
+  const refreshedPayload = jwt.token.decode(refreshedSession.access_token)
+    .decoded.payload
+
   // Update userSession with new access token and new expiry details
   const expiresInSeconds = refreshedSession.expires_in
   const expiresInMilliSeconds = expiresInSeconds * 1000
   const expiresAt = addSeconds(new Date(), expiresInSeconds)
-  const authedUser = await request.getUserSession()
+  const isAdmin = refreshedPayload.groups.includes(
+    config.get('azureAdminGroupId')
+  )
 
   await request.server.app.cache.set(request.state.userSession.sessionId, {
-    ...authedUser,
+    id: refreshedPayload.oid,
+    email: refreshedPayload.preferred_username,
+    displayName: refreshedPayload.name,
+    loginHint: refreshedPayload.login_hint,
+    isAuthenticated: true,
     token: refreshedSession.access_token,
+    refreshToken: refreshedSession.refresh_token,
+    isAdmin,
+    scope: refreshedPayload.groups,
     expiresIn: expiresInMilliSeconds,
     expiresAt
   })
