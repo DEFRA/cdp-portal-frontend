2cbe321fce42424279c89e55eb70cba70780ffe8
Ben Chidgey <ben@feedmypixel.com>
2023-11-21 17:36:49 +0000

    Updates to create service status page

--- src/server/services/transformers/transform-status.js
+++ src/server/services/transformers/transform-status.js
@@ -4,6 +4,8 @@ import { config } from '~/src/config'
 import { unknownValue } from '~/src/server/common/constants/no-value'
 import { statusTagClassMap } from '~/src/server/services/transformers/status-tag-class-map'
 import { removeUrlParts } from '~/src/server/common/helpers/remove-url-parts'
+import { serviceCreationStatuses } from '~/src/server/common/constants/service-creation-statuses'
+import { buildLink } from '~/src/server/common/helpers/build-link'
 
 const githubOrg = config.get('githubOrg')
 
@@ -14,17 +16,26 @@ function transformStatus(service) {
   const cdpAppConfig = serviceStatus?.['cdp-app-config']
   const cdpNginxUpstreams = serviceStatus?.['cdp-nginx-upstreams']
 
-  const failureFlags = ['failed', 'failure'] // TODO confirm whats what with this and ideally remove one
+  const jobStatuses = [
+    createRepository?.status,
+    cdpTfSvcInfra?.status,
+    cdpAppConfig?.status,
+    cdpNginxUpstreams?.status
+  ]
+  const completeJobs = jobStatuses.filter(
+    (status) => status === 'success'
+  ).length
 
   return {
     started: serviceStatus?.started,
-    hasJobFailures: [
-      serviceStatus?.status,
-      createRepository?.status,
-      cdpTfSvcInfra?.status,
-      cdpAppConfig?.status,
-      cdpNginxUpstreams?.status
-    ].some((status) => failureFlags.includes(status)),
+    hasJobFailures: [serviceStatus?.status, ...jobStatuses].some(
+      (status) => status === 'failure'
+    ),
+    progress: {
+      percentage: (100 / jobStatuses.length) * completeJobs,
+      complete: completeJobs,
+      total: jobStatuses.length
+    },
     status: {
       value: serviceStatus.status,
       text: serviceStatus?.status
@@ -32,8 +43,9 @@ function transformStatus(service) {
         : unknownValue,
       classes: statusTagClassMap(serviceStatus?.status)
     },
+    serviceType: serviceStatus.serviceType,
     createRepository: {
-      name: 'Create Repository',
+      name: 'Create Github Repository',
       part: 1,
       url: {
         text: removeUrlParts(service?.githubUrl),
@@ -44,42 +56,28 @@ function transformStatus(service) {
           ? startCase(createRepository?.status)
           : unknownValue,
         classes: statusTagClassMap(createRepository?.status)
+      },
+      info: () => {
+        switch (createRepository?.status) {
+          case serviceCreationStatuses.prOpen:
+            return `Pull request has been raised and will shortly be automatically merged. The Github pull request link below has more information.`
+          case serviceCreationStatuses.inProgress:
+            return `Creating github repository from ${serviceStatus.serviceType} template. The Github action link below has more information.`
+          case serviceCreationStatuses.created:
+          case serviceCreationStatuses.success:
+            return `Github repository created successfully, you can now checkout your new services code and start developing.`
+          case serviceCreationStatuses.failure:
+            return `Something has gone wrong, contact us using the details at the ${buildLink(
+              '#app-help',
+              'top of the page',
+              false
+            )}.`
+        }
       }
     },
-    cdpTfSvcInfra: {
-      name: 'Create Infrastructure',
-      part: 2,
-      url: {
-        text: `${githubOrg}/cdp-tf-svc-infra`,
-        href: `https://github.com/${githubOrg}/cdp-tf-svc-infra`
-      },
-      status: {
-        text: cdpTfSvcInfra?.status
-          ? startCase(cdpTfSvcInfra.status)
-          : unknownValue,
-        classes: statusTagClassMap(cdpTfSvcInfra?.status)
-      },
-      pullRequest: {
-        url: {
-          text: removeUrlParts(cdpTfSvcInfra?.pr?.html_url),
-          href: cdpTfSvcInfra?.pr?.html_url
-        }
-      },
-      githubAction: {
-        name: cdpTfSvcInfra?.main?.workflow?.name,
-        url: {
-          text: removeUrlParts(cdpTfSvcInfra?.main?.workflow?.html_url),
-          href: cdpTfSvcInfra?.main?.workflow?.html_url
-        },
-        started: cdpTfSvcInfra?.main?.workflow?.created_at
-      },
-      errors: cdpTfSvcInfra?.result?.errors
-        ?.map((error) => error?.message)
-        .filter(Boolean)
-    },
     cdpAppConfig: {
-      name: 'Create App Config',
-      part: 3,
+      name: 'Create Service Config',
+      part: 2,
       url: {
         text: `${githubOrg}/cdp-app-config`,
         href: `https://github.com/${githubOrg}/cdp-app-config`
@@ -89,6 +87,24 @@ function transformStatus(service) {
           ? startCase(cdpAppConfig.status)
           : unknownValue,
         classes: statusTagClassMap(cdpAppConfig?.status)
+      },
+      info: () => {
+        switch (cdpAppConfig?.status) {
+          case serviceCreationStatuses.prOpen:
+            return `Pull request has been raised and will shortly be automatically merged. The Github pull request link below has more information.`
+          case serviceCreationStatuses.failure:
+            return `Something has gone wrong, contact us using the details at the ${buildLink(
+              '#app-help',
+              'top of the page',
+              false
+            )}.`
+          default:
+            return `Services on the Core Delivery Platform are configured via environment variables. You can update your services config via pull request on the cdp-app-config
+        repository. For more detailed information see the ${buildLink(
+          `https://github.com/${githubOrg}/cdp-app-config`,
+          'DEFRA/cdp-app-config/README.md'
+        )}`
+        }
       },
       pullRequest: {
         url: {
@@ -104,13 +120,14 @@ function transformStatus(service) {
         },
         started: cdpAppConfig?.main?.workflow?.created_at
       },
-      errors: cdpAppConfig?.result?.errors
-        ?.map((error) => error?.message)
-        .filter(Boolean)
+      errors:
+        cdpAppConfig?.result?.errors
+          ?.map((error) => error?.message)
+          .filter(Boolean) ?? []
     },
     cdpNginxUpstreams: {
-      name: 'Create Nginx Upstreams',
-      part: 4,
+      name: 'Create Service networking',
+      part: 3,
       url: {
         text: `${githubOrg}/cdp-nginx-upstreams`,
         href: `https://github.com/${githubOrg}/cdp-nginx-upstreams`
@@ -120,6 +137,23 @@ function transformStatus(service) {
           ? startCase(cdpNginxUpstreams.status)
           : unknownValue,
         classes: statusTagClassMap(cdpNginxUpstreams?.status)
+      },
+      info: () => {
+        switch (cdpNginxUpstreams?.status) {
+          case serviceCreationStatuses.prOpen:
+            return `Pull request has been raised and will shortly be automatically merged. The Github pull request link below has more information.`
+          case serviceCreationStatuses.inProgress:
+            return `Setting up your service to be accessible to other services/public on the Core Delivery Platform environments. The Github action link below has more information.`
+          case serviceCreationStatuses.created:
+          case serviceCreationStatuses.success:
+            return `Your services path is now configured on the Core Delivery Platform environments.`
+          case serviceCreationStatuses.failure:
+            return `Something has gone wrong, contact us using the details at the ${buildLink(
+              '#app-help',
+              'top of the page',
+              false
+            )}.`
+        }
       },
       pullRequest: {
         url: {
@@ -135,9 +169,66 @@ function transformStatus(service) {
         },
         started: cdpNginxUpstreams?.main?.workflow?.created_at
       },
-      errors: cdpNginxUpstreams?.result?.errors
-        ?.map((error) => error?.message)
-        .filter(Boolean)
+      errors:
+        cdpNginxUpstreams?.result?.errors
+          ?.map((error) => error?.message)
+          .filter(Boolean) ?? []
+    },
+    cdpTfSvcInfra: {
+      name: 'Create Service Infrastructure',
+      part: 4,
+      url: {
+        text: `${githubOrg}/cdp-tf-svc-infra`,
+        href: `https://github.com/${githubOrg}/cdp-tf-svc-infra`
+      },
+      status: {
+        text: cdpTfSvcInfra?.status
+          ? startCase(cdpTfSvcInfra.status)
+          : unknownValue,
+        classes: statusTagClassMap(cdpTfSvcInfra?.status)
+      },
+      info: () => {
+        switch (cdpTfSvcInfra?.status) {
+          case serviceCreationStatuses.prOpen:
+            return `Pull request has been raised and will shortly be automatically merged. The Github pull request link below has more information.`
+          case serviceCreationStatuses.inProgress:
+            return `Setting up:
+                      <ul class="govuk-list govuk-list--spaced">
+                        <li>Elastic container registry</li>
+                        <li>Database credentials</li>
+                        <li>Permissions</li>
+                      </ul>
+                    You will need to wait for this stage to complete before you're able to deploy your service.
+                    The Github action link below has more information.`
+          case serviceCreationStatuses.created:
+          case serviceCreationStatuses.success:
+            return `All the supporting infrastructure for your service has been created.`
+          case serviceCreationStatuses.failure:
+            return `Something has gone wrong, contact us using the details at the ${buildLink(
+              '#app-help',
+              'top of the page',
+              false
+            )}.`
+        }
+      },
+      pullRequest: {
+        url: {
+          text: removeUrlParts(cdpTfSvcInfra?.pr?.html_url),
+          href: cdpTfSvcInfra?.pr?.html_url
+        }
+      },
+      githubAction: {
+        name: cdpTfSvcInfra?.main?.workflow?.name,
+        url: {
+          text: removeUrlParts(cdpTfSvcInfra?.main?.workflow?.html_url),
+          href: cdpTfSvcInfra?.main?.workflow?.html_url
+        },
+        started: cdpTfSvcInfra?.main?.workflow?.created_at
+      },
+      errors:
+        cdpTfSvcInfra?.result?.errors
+          ?.map((error) => error?.message)
+          .filter(Boolean) ?? []
     }
   }
 }
--- src/server/services/transformers/transform-status.test.js
+++ src/server/services/transformers/transform-status.test.js
@@ -14,104 +14,119 @@ describe('#transformStatus', () => {
       serviceGithubDetailFixture.repository
     )
 
-    expect(transformStatus(service)).toEqual({
-      cdpAppConfig: {
-        githubAction: {
-          name: 'Upload config to s3 (infra-dev)',
-          started: '2023-10-27T12:38:40Z',
+    expect(transformStatus(service)).toEqual(
+      expect.objectContaining({
+        cdpAppConfig: {
+          errors: [],
+          githubAction: {
+            name: 'Upload config to s3 (infra-dev)',
+            started: '2023-10-27T12:38:40Z',
+            url: {
+              href: `https://github.com/${githubOrg}/cdp-app-config/actions/runs/6667297592`,
+              text: `${githubOrg}/cdp-app-config/actions/runs/6667297592`
+            }
+          },
+          info: expect.any(Function),
+          name: 'Create Service Config',
+          part: 2,
+          pullRequest: {
+            url: {
+              href: `https://github.com/${githubOrg}/cdp-app-config/pull/180`,
+              text: `${githubOrg}/cdp-app-config/pull/180`
+            }
+          },
+          status: {
+            classes: 'govuk-tag--green',
+            text: 'Success'
+          },
           url: {
-            href: `https://github.com/${githubOrg}/cdp-app-config/actions/runs/6667297592`,
-            text: `${githubOrg}/cdp-app-config/actions/runs/6667297592`
+            href: `https://github.com/${githubOrg}/cdp-app-config`,
+            text: `${githubOrg}/cdp-app-config`
           }
         },
-        name: 'Create App Config',
-        part: 3,
-        pullRequest: {
+        cdpNginxUpstreams: {
+          errors: [],
+          githubAction: {
+            name: 'Push to S3',
+            started: '2023-10-27T12:39:01Z',
+            url: {
+              href: `https://github.com/${githubOrg}/cdp-nginx-upstreams/actions/runs/6667301828`,
+              text: `${githubOrg}/cdp-nginx-upstreams/actions/runs/6667301828`
+            }
+          },
+          info: expect.any(Function),
+          name: 'Create Service networking',
+          part: 3,
+          pullRequest: {
+            url: {
+              href: `https://github.com/${githubOrg}/cdp-nginx-upstreams/pull/123`,
+              text: `${githubOrg}/cdp-nginx-upstreams/pull/123`
+            }
+          },
+          status: {
+            classes: 'govuk-tag--green',
+            text: 'Success'
+          },
           url: {
-            href: `https://github.com/${githubOrg}/cdp-app-config/pull/180`,
-            text: `${githubOrg}/cdp-app-config/pull/180`
+            href: `https://github.com/${githubOrg}/cdp-nginx-upstreams`,
+            text: `${githubOrg}/cdp-nginx-upstreams`
           }
         },
+        cdpTfSvcInfra: {
+          errors: [],
+          githubAction: {
+            name: 'Terraform Apply',
+            started: '2023-10-27T12:40:27Z',
+            url: {
+              href: `https://github.com/${githubOrg}/cdp-tf-svc-infra/actions/runs/6667318902`,
+              text: `${githubOrg}/cdp-tf-svc-infra/actions/runs/6667318902`
+            }
+          },
+          info: expect.any(Function),
+          name: 'Create Service Infrastructure',
+          part: 4,
+          pullRequest: {
+            url: {
+              href: `https://github.com/${githubOrg}/cdp-tf-svc-infra/pull/309`,
+              text: `${githubOrg}/cdp-tf-svc-infra/pull/309`
+            }
+          },
+          status: {
+            classes: 'govuk-tag--green',
+            text: 'Success'
+          },
+          url: {
+            href: `https://github.com/${githubOrg}/cdp-tf-svc-infra`,
+            text: `${githubOrg}/cdp-tf-svc-infra`
+          }
+        },
+        createRepository: {
+          info: expect.any(Function),
+          name: 'Create Github Repository',
+          part: 1,
+          status: {
+            classes: 'govuk-tag--green',
+            text: 'Success'
+          },
+          url: {
+            href: `https://github.com/${githubOrg}`,
+            text: `${githubOrg}`
+          }
+        },
+        hasJobFailures: false,
+        progress: {
+          complete: 4,
+          percentage: 100,
+          total: 4
+        },
+        serviceType: 'cdp-node-backend-template',
+        started: '2023-10-27T12:37:46.915Z',
         status: {
-          classes: 'govuk-tag--green',
-          text: 'Success'
-        },
-        url: {
-          href: `https://github.com/${githubOrg}/cdp-app-config`,
-          text: `${githubOrg}/cdp-app-config`
+          classes: 'govuk-tag--blue',
+          text: 'In Progress',
+          value: 'in-progress'
         }
-      },
-      cdpNginxUpstreams: {
-        githubAction: {
-          name: 'Push to S3',
-          started: '2023-10-27T12:39:01Z',
-          url: {
-            href: `https://github.com/${githubOrg}/cdp-nginx-upstreams/actions/runs/6667301828`,
-            text: `${githubOrg}/cdp-nginx-upstreams/actions/runs/6667301828`
-          }
-        },
-        name: 'Create Nginx Upstreams',
-        part: 4,
-        pullRequest: {
-          url: {
-            href: `https://github.com/${githubOrg}/cdp-nginx-upstreams/pull/123`,
-            text: `${githubOrg}/cdp-nginx-upstreams/pull/123`
-          }
-        },
-        status: {
-          classes: 'govuk-tag--green',
-          text: 'Success'
-        },
-        url: {
-          href: `https://github.com/${githubOrg}/cdp-nginx-upstreams`,
-          text: `${githubOrg}/cdp-nginx-upstreams`
-        }
-      },
-      cdpTfSvcInfra: {
-        githubAction: {
-          name: 'Terraform Apply',
-          started: '2023-10-27T12:40:27Z',
-          url: {
-            href: `https://github.com/${githubOrg}/cdp-tf-svc-infra/actions/runs/6667318902`,
-            text: `${githubOrg}/cdp-tf-svc-infra/actions/runs/6667318902`
-          }
-        },
-        name: 'Create Infrastructure',
-        part: 2,
-        pullRequest: {
-          url: {
-            href: `https://github.com/${githubOrg}/cdp-tf-svc-infra/pull/309`,
-            text: `${githubOrg}/cdp-tf-svc-infra/pull/309`
-          }
-        },
-        status: {
-          classes: 'govuk-tag--green',
-          text: 'Success'
-        },
-        url: {
-          href: `https://github.com/${githubOrg}/cdp-tf-svc-infra`,
-          text: `${githubOrg}/cdp-tf-svc-infra`
-        }
-      },
-      createRepository: {
-        name: 'Create Repository',
-        part: 1,
-        status: {
-          classes: 'govuk-tag--green',
-          text: 'Success'
-        },
-        url: {
-          href: `https://github.com/${githubOrg}`,
-          text: `${githubOrg}`
-        }
-      },
-      hasJobFailures: false,
-      started: '2023-10-27T12:37:46.915Z',
-      status: {
-        value: 'in-progress',
-        classes: 'govuk-tag--blue',
-        text: 'In Progress'
-      }
-    })
+      })
+    )
   })
 })
