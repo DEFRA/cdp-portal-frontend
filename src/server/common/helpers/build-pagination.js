a404d448e15801841d2dc81a23f30e1ee4255db5
Anargyros Akrivos <argy@akrivos.me>
2023-12-06 17:19:42 +0000

    Ellipses for long pagination

--- src/server/common/helpers/build-pagination.js
+++ src/server/common/helpers/build-pagination.js
@@ -1,50 +1,53 @@
 import qs from 'qs'
 
 function buildPagination(page, pageSize, totalPages, queryParams) {
+  if (
+    !Number.isInteger(page) ||
+    !Number.isInteger(pageSize) ||
+    !Number.isInteger(totalPages) ||
+    page <= 0 ||
+    pageSize <= 0 ||
+    totalPages <= 0
+  ) {
+    throw new Error('Invalid parameters')
+  }
+
   const pagination = { items: [] }
+  const isLongPagination = totalPages > 10
+  const shortPaginationPages = new Set([
+    1,
+    page - 1,
+    page,
+    page + 1,
+    totalPages
+  ])
 
   // add items
   for (let i = 1; i <= totalPages; i++) {
-    const params = {
-      ...queryParams,
-      page: i,
-      size: pageSize
+    if (isLongPagination && !shortPaginationPages.has(i)) {
+      if (i === page - 2 || i === page + 2) {
+        pagination.items.push({ ellipsis: true })
+      }
+      continue
     }
-
-    const item = {
-      number: i,
-      href: '?' + qs.stringify(params)
-    }
-
+    const params = { ...queryParams, page: i, size: pageSize }
+    const item = { number: i, href: `?${qs.stringify(params)}` }
     if (i === page) {
       item.current = true
     }
-
     pagination.items.push(item)
   }
 
   // add previous
   if (pagination.items.length > 1 && page > 1) {
-    const params = {
-      ...queryParams,
-      page: parseInt(page) - 1,
-      size: pageSize
-    }
-    pagination.previous = {
-      href: '?' + qs.stringify(params)
-    }
+    const params = { ...queryParams, page: page - 1, size: pageSize }
+    pagination.previous = { href: `?${qs.stringify(params)}` }
   }
 
   // add next
   if (page < totalPages) {
-    const params = {
-      ...queryParams,
-      page: parseInt(page) + 1,
-      size: pageSize
-    }
-    pagination.next = {
-      href: '?' + qs.stringify(params)
-    }
+    const params = { ...queryParams, page: page + 1, size: pageSize }
+    pagination.next = { href: `?${qs.stringify(params)}` }
   }
 
   return pagination
