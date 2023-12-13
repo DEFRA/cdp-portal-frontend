76a98e676b878ec5c4ad68a4968c397d376b939a
Ben Chidgey <ben@feedmypixel.com>
2023-12-13 20:18:21 +0000

    Remove global

--- jest-setup.js
+++ jest-setup.js
@@ -3,7 +3,7 @@ import fetchMock from 'jest-fetch-mock'
 // Client side fetch mocking, use nock for node-fetch work
 global.fetch = fetchMock
 
-global.afterEach(() => {
+afterEach(() => {
   // Clear down JSDOM document after each test
   document.getElementsByTagName('html')[0].innerHTML = ''
 })
