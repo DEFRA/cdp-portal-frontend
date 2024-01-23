c9ada9a864d0bacdfb8dbd0a28f09194662e4f70
Ben Chidgey <ben@feedmypixel.com>
2024-01-23 09:36:31 +0000

    Handle not found deployments

--- src/server/deployments/helpers/pre/provide-deployment.js
+++ src/server/deployments/helpers/pre/provide-deployment.js
@@ -1,16 +1,11 @@
-import Boom from '@hapi/boom'
-
 import { transformDeployment } from '~/src/server/deployments/transformers/transform-deployment'
 import { fetchDeployment } from '~/src/server/deployments/helpers/fetch/fetch-deployment'
 
 const provideDeployment = {
   method: async function (request) {
     const deploymentId = request.params?.deploymentId
-    const deployment = transformDeployment(await fetchDeployment(deploymentId))
-
-    if (!deployment) {
-      throw Boom.boomify(Boom.notFound())
-    }
+    const deploymentResponse = await fetchDeployment(deploymentId)
+    const deployment = transformDeployment(deploymentResponse)
 
     // If is an XHR call and when the deployment has finished and is running return null to make the poller refresh
     // the page.
