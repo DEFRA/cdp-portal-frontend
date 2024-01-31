5946c1902f1bff089df7d03f382b62704da8df7e
Ben Chidgey <ben@feedmypixel.com>
2024-01-31 13:55:20 +0000

    Revert secure context work

--- src/server/index.js
+++ src/server/index.js
@@ -19,7 +19,6 @@ import { requestLogger } from '~/src/server/common/helpers/logging/request-logge
 import { dropUserSession } from '~/src/server/common/helpers/auth/drop-user-session'
 import { userHasTeamScopeDecorator } from '~/src/server/common/helpers/user/user-has-team-scope'
 import { addFlashMessagesToContext } from '~/src/server/common/helpers/add-flash-messages-to-context'
-import { secureContext } from '~/src/server/common/helpers/secure-context'
 
 const client = buildRedisClient()
 
@@ -70,8 +69,6 @@ async function createServer() {
   server.decorate('request', 'userHasTeamScope', userHasTeamScopeDecorator, {
     apply: true
   })
-
-  await server.register(secureContext)
 
   await server.register([
     sessionManager,
