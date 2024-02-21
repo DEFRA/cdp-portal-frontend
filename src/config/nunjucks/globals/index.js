b1388218e64a58fd29e7d18b288b818b518bf6d1
Ben Chidgey <ben@feedmypixel.com>
2024-02-21 16:15:33 +0000

    Add new Kibana links
    - A bit of a tidy up of code
    - A bit of UI jazz

--- src/config/nunjucks/globals/index.js
+++ src/config/nunjucks/globals/index.js
@@ -1,3 +1,4 @@
 import { getComponent } from '~/src/config/nunjucks/globals/get-component'
+import { buildDashboardLink } from '~/src/server/common/helpers/build-dashboard-link'
 
-export { getComponent }
+export { getComponent, buildDashboardLink }
