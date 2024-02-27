64f7a92a433b143c7332e8a98e7365b3a0afbfcb
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2024-02-27 11:51:02 +0000

    Removes explicit cred provider so client uses defaults

--- src/server/common/helpers/aws/build-s3-presigned-url.js
+++ src/server/common/helpers/aws/build-s3-presigned-url.js
@@ -1,4 +1,3 @@
-import { fromEnv } from '@aws-sdk/credential-providers'
 import { S3RequestPresigner } from '@aws-sdk/s3-request-presigner'
 import { formatUrl } from '@aws-sdk/util-format-url'
 import { HttpRequest } from '@smithy/protocol-http'
@@ -8,7 +7,6 @@ import { Hash } from '@smithy/hash-node'
 async function buildS3PresignedUrl({ region, bucket, key }) {
   const url = parseUrl(`https://${bucket}.s3.${region}.amazonaws.com/${key}`)
   const presigner = new S3RequestPresigner({
-    credentials: fromEnv(),
     region,
     sha256: Hash.bind(null, 'sha256')
   })
