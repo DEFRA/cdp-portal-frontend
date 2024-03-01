fc74ffbec5b9b499d836c2f538fd1aa1965661e9
Ben Chidgey <ben@feedmypixel.com>
2024-03-01 08:19:20 +0000

    Add test for logs dashboard link

--- src/server/common/components/logs-dashboard-link/template.test.js
+++ src/server/common/components/logs-dashboard-link/template.test.js
@@ -1,1 +1,26 @@
-test.todo('#logs-dashboard-link')
+import { renderTestComponent } from '~/test-helpers/component-helpers'
+
+describe('Logs Dashboard Link Component', () => {
+  let logsDashBoardLink
+
+  beforeEach(() => {
+    const $component = renderTestComponent('logs-dashboard-link', {
+      serviceName: 'cdp-portal-frontend',
+      environment: 'management'
+    })
+
+    logsDashBoardLink = $component(
+      '[data-testid="app-logs-dashboard-link"]'
+    ).first()
+  })
+
+  test('Should render with expected href attribute', () => {
+    expect(logsDashBoardLink.attr('href')).toEqual(
+      "https://logs.management.cdp-int.defra.cloud/_dashboards/app/dashboards#/view/89f63d50-b6eb-11ee-a385-15667195f827?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-15m,to:now))&_a=(description:'',filters:!(('$state':(store:appState),meta:(alias:!n,controlledBy:'1705679462997',disabled:!f,index:e55f3890-5d4a-11ee-8f40-670c9b0b8093,key:container_name,negate:!f,params:(query:cdp-portal-frontend),type:phrase),query:(match_phrase:(container_name:cdp-portal-frontend)))),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),query:(language:kuery,query:''),timeRestore:!f,title:'CDP%20Service%20Dashboard',viewMode:view)"
+    )
+  })
+
+  test('Should render with expected button text', () => {
+    expect(logsDashBoardLink.text().trim()).toEqual('logs.management')
+  })
+})
