b2785c7ac1589210924c685663e8a2d29ce4f62f
Ben Chidgey <ben@feedmypixel.com>
2024-03-04 16:52:12 +0000

    Abstract to decorator file
    Align this work with how its done elsewhere in the app. No need for a
    plugin here its just a simple decorator

---
+++ src/server/common/helpers/route-lookup/route-lookup-decorator.js
@@ -0,0 +1,9 @@
+import { routeLookup } from '~/src/server/common/helpers/route-lookup/route-lookup'
+
+function routeLookupDecorator(request) {
+  const server = request.server
+
+  return (id, params = {}) => routeLookup(server, id, params)
+}
+
+export { routeLookupDecorator }
