0a2ad40c394962e41b42e1a364ab850ed4fc2306
Ben Chidgey <ben@feedmypixel.com>
2024-03-13 17:14:01 +0000

    Autocomplete component
    Our own Vanilla JavaScript Autocomplete component :rocket:
    
    - Accessible
    - Progressive, enhances a select element
    - Looks lovely

---
+++ src/server/common/components/autocomplete/template.test.js
@@ -0,0 +1,75 @@
+import { renderTestComponent } from '~/test-helpers/component-helpers'
+
+describe('Autocomplete Component', () => {
+  let $autocompleteFromGroup
+  let $select
+
+  beforeEach(() => {
+    const $component = renderTestComponent('autocomplete', {
+      label: {
+        text: 'By'
+      },
+      hint: {
+        text: 'Choose a user'
+      },
+      id: 'user',
+      name: 'user',
+      suggestions: [
+        {
+          text: 'RoboCop',
+          value: 'RoboCop',
+          helper: 'User Id: 12454878'
+        },
+        {
+          text: 'Roger Rabbit',
+          value: 'Roger Rabbit',
+          helper: 'User Id: 556456465'
+        },
+        {
+          text: 'Barbie',
+          value: 'Barbie',
+          helper: 'User Id: 67567576'
+        }
+      ]
+    })
+
+    $autocompleteFromGroup = $component(
+      '[data-testid="app-autocomplete-group"]'
+    ).first()
+
+    $select = $autocompleteFromGroup.find(
+      '[data-testid="app-progressive-input"]'
+    )
+  })
+
+  test('Should render with expected label', () => {
+    const $label = $autocompleteFromGroup.find(
+      '[data-testid="app-autocomplete-label"]'
+    )
+
+    expect($label.length).toEqual(1)
+    expect($label.text().trim()).toEqual('By')
+  })
+
+  test('Should render with expected hint input', () => {
+    const $hint = $autocompleteFromGroup.find(
+      '[data-testid="app-autocomplete-hint"]'
+    )
+
+    expect($hint.length).toEqual(1)
+    expect($hint.text().trim()).toEqual('Choose a user')
+  })
+
+  test('Should render with expected select input', () => {
+    expect($select.length).toEqual(1)
+  })
+
+  test('Should render with expected suggestions', () => {
+    const $options = $select.children('option')
+
+    expect($options.length).toEqual(3)
+    expect($options.eq(0).text().trim()).toEqual('RoboCop')
+    expect($options.eq(1).text().trim()).toEqual('Roger Rabbit')
+    expect($options.eq(2).text().trim()).toEqual('Barbie')
+  })
+})
