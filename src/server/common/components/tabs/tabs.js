f18cf20a67c554c31abbab2cafc698923122d864
Ben Chidgey <ben@feedmypixel.com>
2024-05-17 09:02:06 +0100

    Updates to tabs component
    - Set up listener to update query param changes on tab hrefs
    - Styling tweaks
    - Markup tweaks

--- src/server/common/components/tabs/tabs.js
+++ src/server/common/components/tabs/tabs.js
@@ -1,29 +1,27 @@
 import qs from 'qs'
-import { isEmpty } from 'lodash'
 
-import { subscribe } from '~/src/client/common/helpers/event-emitter'
-import { eventName } from '~/src/client/common/constants/event-name'
+function prependQueryParams($tab, params) {
+  const hrefParts = $tab.href.split('?')
+  const href = hrefParts.at(0)
 
-function prependQueryParams($tab, queryParamString) {
-  const href = $tab.href.includes('?') ? $tab.href.split('?')[0] : $tab.href
-
-  $tab.href = href + queryParamString
+  $tab.href = href + qs.stringify(params, { addQueryPrefix: true })
 }
 
 function tabs($module) {
-  const queryParamString = location?.search
+  const params = qs.parse(location.search, { ignoreQueryPrefix: true })
   const $tabs = Array.from($module.querySelectorAll(`[data-js="app-tab"]`))
 
   document.addEventListener('DOMContentLoaded', () => {
-    $tabs.forEach(($tab) => prependQueryParams($tab, queryParamString))
+    $tabs.forEach(($tab) => prependQueryParams($tab, params))
   })
 
-  subscribe(eventName.xhrUpdate, (event) => {
-    const queryParamString = isEmpty(event.detail.params)
-      ? ''
-      : `?${qs.stringify(event.detail.params)}`
+  window.navigation.addEventListener('navigate', (event) => {
+    const searchString = event.destination.url.split('?').at(1)
+    const destinationParams = qs.parse(searchString, {
+      ignoreQueryPrefix: true
+    })
 
-    $tabs.forEach(($tab) => prependQueryParams($tab, queryParamString))
+    $tabs.forEach(($tab) => prependQueryParams($tab, destinationParams))
   })
 }
 
