ad3467c0b96d75b552ed8bd808c6cbe50c69fa26
Ben Chidgey <ben@feedmypixel.com>
2024-07-18 17:41:33 +0100

    Add secrets home page

--- src/server/services/controllers/index.js
+++ src/server/services/controllers/index.js
@@ -1,11 +1,13 @@
 import { serviceController } from '~/src/server/services/controllers/service'
 import { serviceSecretsController } from '~/src/server/services/controllers/service-secrets'
+import { serviceEnvironmentSecretsController } from '~/src/server/services/controllers/service-environment-secrets'
 import { serviceListController } from '~/src/server/services/controllers/service-list'
 import { serviceStatusController } from '~/src/server/services/controllers/service-status'
 
 export {
   serviceController,
   serviceSecretsController,
+  serviceEnvironmentSecretsController,
   serviceListController,
   serviceStatusController
 }
--- src/server/services/controllers/service-secrets.js
+++ src/server/services/controllers/service-secrets.js
@@ -1,16 +1,11 @@
-import Joi from 'joi'
-import Boom from '@hapi/boom'
-
 import { scopes } from '~/src/server/common/constants/scopes'
-import { provideService } from '~/src/server/services/helpers/pre/provide-service'
-import { provideCanDeploy } from '~/src/server/services/helpers/pre/provide-can-deploy'
-import { buildRunningServicesRowHeadings } from '~/src/server/common/helpers/build-running-services-row-headings'
-import { getEnvironmentsByTeam } from '~/src/server/common/helpers/environments/get-environments-by-team'
 import { addServiceOwnerScope } from '~/src/server/services/helpers/add-service-owner-scope'
+import { provideService } from '~/src/server/services/helpers/pre/provide-service'
 
 const serviceSecretsController = {
   options: {
     id: 'services/{serviceId}/secrets',
+    pre: [provideService],
     ext: {
       onCredentials: addServiceOwnerScope()
     },
@@ -19,23 +14,14 @@ const serviceSecretsController = {
       access: {
         scope: [scopes.admin, scopes.serviceOwner]
       }
-    },
-    pre: [[provideService], provideCanDeploy],
-    validate: {
-      params: Joi.object({
-        serviceId: Joi.string().required()
-      }),
-      failAction: () => Boom.boomify(Boom.notFound())
     }
   },
   handler: async (request, h) => {
     const service = request.pre.service
-    const environments = getEnvironmentsByTeam(service.teams)
 
     return h.view('services/views/service-secrets', {
-      pageTitle: `${service.serviceName} microservice`,
+      pageTitle: `${service.serviceName} - Secrets`,
       heading: service.serviceName,
-      rowHeadings: buildRunningServicesRowHeadings(environments),
       service,
       breadcrumbs: [
         {
