20da7f472ec13f699fbf250e935af687fd420a71
Ben Chidgey <ben@feedmypixel.com>
2024-07-24 18:16:58 +0100

    Add notAllowed text and choose validation helper

--- src/server/common/constants/validation.js
+++ src/server/common/constants/validation.js
@@ -1,7 +1,12 @@
+import { startsWithVowel } from '~/src/server/common/helpers/starts-with-vowel'
+
 const validation = {
   enterValue: 'Enter value',
   chooseAnEntry: 'Choose an entry',
   chooseUser: 'Choose a user',
+  notAllowed: 'Not allowed',
+  choose: (choice) =>
+    `Choose ${startsWithVowel(choice) ? 'an' : 'a'} ${choice}`,
   minCharacters: (value) => `${value} characters or more`,
   maxCharacters: (value) => `${value} characters or less`
 }
