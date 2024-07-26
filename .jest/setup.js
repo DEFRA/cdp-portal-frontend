db6877fa0c3aa9200ede701717a73b25b4bbbecc
Ben Chidgey <ben@feedmypixel.com>
2024-07-26 13:02:46 +0100

    Globally mock redis in tests

--- .jest/setup.js
+++ .jest/setup.js
@@ -2,6 +2,14 @@ import fetchMock from 'jest-fetch-mock'
 import { TextEncoder, TextDecoder } from 'node:util'
 import { ReadableStream, TransformStream } from 'node:stream/web'
 
+import { config } from '~/src/config'
+
+// We support both node and client side unit tests. Using jsdom and poly-filling individual Node.js server needs
+
+// Globally mock redis
+jest.mock('ioredis')
+
+// Node mocks/polyfills
 global.TextEncoder = TextEncoder
 global.TextDecoder = TextDecoder
 global.ReadableStream = ReadableStream
