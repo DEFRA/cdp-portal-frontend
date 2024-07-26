3f4ffc0ce5cade3138b874eb0baf743dd94c082e
Ben Chidgey <ben@feedmypixel.com>
2024-07-26 15:32:25 +0100

    Add jest envs

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
   silent: process.env.NODE_ENV !== 'development',
   testMatch: ['<rootDir>/src/**/*.test.js'],
   reporters: ['default', ['github-actions', { silent: false }], 'summary'],
+  setupFiles: ['<rootDir>/.jest/setup-envs.js'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.js'],
   collectCoverageFrom: ['<rootDir>/src/**/*.js'],
   coveragePathIgnorePatterns: [
