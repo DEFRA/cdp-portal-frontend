86d0479e187e3d6b122127a4b2517dcc88771743
Ben Chidgey <ben@feedmypixel.com>
2024-07-26 15:37:21 +0100

    Refactor feature flag work
    - Add FEATURE_FLAG_SECRETS env
    - Put this secrets behind a feature flag env rather than just commenting
    things out
    - Add comments '// FEATURE-FLAG - secrets added: 26/07/2024'
    - Turn feature flag off for tests, so the implementation behind the
      feature flag is tested
    - Remove skips on tests

--- src/server/services/helpers/provide-tabs.js
+++ src/server/services/helpers/provide-tabs.js
@@ -1,6 +1,10 @@
+import { config } from '~/src/config'
+
+const secretsIsFeatureFlagged = config.get('featureFlags.secrets')
+
 async function provideTabs(request, h) {
   const authedUser = await request.getUserSession()
-  // const isAuthenticated = authedUser?.isAuthenticated
+  const isAuthenticated = authedUser?.isAuthenticated
   const isAdmin = authedUser?.isAdmin
   const response = request.response
 
@@ -10,9 +14,9 @@ async function provideTabs(request, h) {
     }
 
     const imageName = response.source?.context?.service?.imageName
-    // const teams = response.source?.context?.service?.teams ?? []
-    // const serviceTeamIds = teams.map((team) => team.teamId)
-    // const isServiceOwner = await request.userIsServiceOwner(serviceTeamIds)
+    const teams = response.source?.context?.service?.teams ?? []
+    const serviceTeamIds = teams.map((team) => team.teamId)
+    const isServiceOwner = await request.userIsServiceOwner(serviceTeamIds)
 
     response.source.context.tabs = [
       {
@@ -26,23 +30,42 @@ async function provideTabs(request, h) {
       }
     ]
 
-    // TODO feature flag so only admin can access secrets
-    // if (isAdmin || isServiceOwner) {
-    if (isAdmin) {
-      response.source.context.tabs.push({
-        isActive: request.path.startsWith(`/services/${imageName}/secrets`),
-        url: request.routeLookup('services/{serviceId}/secrets', {
-          params: {
-            serviceId: imageName
-          }
-        }),
-        label: 'Secrets'
-      })
+    // FEATURE-FLAG - secrets added: 26/07/2024
+    if (secretsIsFeatureFlagged) {
+      if (isAdmin) {
+        response.source.context.tabs.push({
+          isActive: request.path.startsWith(`/services/${imageName}/secrets`),
+          url: request.routeLookup('services/{serviceId}/secrets', {
+            params: {
+              serviceId: imageName
+            }
+          }),
+          label: 'Secrets'
+        })
+      }
+
+      if (!isAdmin) {
+        response.source.context.displayTabs = false
+      }
     }
 
-    // if (!isAuthenticated) {
-    if (!isAdmin) {
-      response.source.context.displayTabs = false
+    // FEATURE-FLAG - secrets added: 26/07/2024
+    if (secretsIsFeatureFlagged === false) {
+      if (isAdmin || isServiceOwner) {
+        response.source.context.tabs.push({
+          isActive: request.path.startsWith(`/services/${imageName}/secrets`),
+          url: request.routeLookup('services/{serviceId}/secrets', {
+            params: {
+              serviceId: imageName
+            }
+          }),
+          label: 'Secrets'
+        })
+      }
+
+      if (!isAuthenticated) {
+        response.source.context.displayTabs = false
+      }
     }
   }
 
