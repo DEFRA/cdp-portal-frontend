23545214b0e9970601053b11fad2e8e2602233b9
Ben Chidgey <ben@feedmypixel.com>
2024-07-26 13:07:17 +0100

    Use root in all of config

--- jest.config.js
+++ jest.config.js
@@ -4,11 +4,11 @@ module.exports = {
   verbose: true,
   resetModules: true,
   clearMocks: true,
-  testMatch: ['**/src/**/*.test.js'],
   silent: process.env.NODE_ENV !== 'development',
+  testMatch: ['<rootDir>/src/**/*.test.js'],
   reporters: ['default', ['github-actions', { silent: false }], 'summary'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.js'],
-  collectCoverageFrom: ['src/**/*.js'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.js'],
   coveragePathIgnorePatterns: [
     '<rootDir>/node_modules/',
     '<rootDir>/.server',
