3acd1fc709c21a4575fb6681182e4070b874dcad
Ben Chidgey <ben@feedmypixel.com>
2024-07-30 11:20:11 +0100

    Add can run pre
    - Pass canRun flag into transformer
    - Pass canRun flag to view

---
+++ src/server/test-suites/helpers/pre/provide-can-run.js
@@ -0,0 +1,31 @@
+const provideCanRun = {
+  /**
+   * Check if the user can run the test suite
+   * @param request
+   * @returns {Promise<boolean>}
+   */
+  method: async function (request) {
+    const authedUser = await request.getUserSession()
+
+    if (authedUser && authedUser.isAuthenticated) {
+      const { isAdmin } = authedUser
+
+      if (isAdmin) {
+        return true
+      }
+
+      const isServiceOwner = await request.userIsMemberOfATeam(
+        request.pre.testSuite.teams.map((team) => team.teamId)
+      )
+
+      if (isServiceOwner) {
+        return true
+      }
+    }
+
+    return false
+  },
+  assign: 'canRun'
+}
+
+export { provideCanRun }
---
+++ src/server/test-suites/helpers/pre/provide-can-run.test.js
@@ -0,0 +1,60 @@
+import { provideCanRun } from './provide-can-run'
+
+describe('#provideCanRun', () => {
+  let request
+
+  beforeEach(() => {
+    request = {
+      getUserSession: jest.fn(),
+      userIsMemberOfATeam: jest.fn(),
+      pre: {
+        testSuite: {
+          teams: [{ teamId: 'team1' }, { teamId: 'team2' }]
+        }
+      }
+    }
+  })
+
+  test('Should return true if user is authenticated and is an admin', async () => {
+    request.getUserSession.mockResolvedValue({
+      isAuthenticated: true,
+      isAdmin: true
+    })
+
+    const result = await provideCanRun.method(request)
+
+    expect(result).toBe(true)
+  })
+
+  test('should return true true if user is authenticated and is a service owner', async () => {
+    request.getUserSession.mockResolvedValue({
+      isAuthenticated: true,
+      isAdmin: false
+    })
+    request.userIsMemberOfATeam.mockResolvedValue(true)
+
+    const result = await provideCanRun.method(request)
+
+    expect(result).toBe(true)
+  })
+
+  test('Should return false if user is authenticated but not an admin or service owner', async () => {
+    request.getUserSession.mockResolvedValue({
+      isAuthenticated: true,
+      isAdmin: false
+    })
+    request.userIsMemberOfATeam.mockResolvedValue(false)
+
+    const result = await provideCanRun.method(request)
+
+    expect(result).toBe(false)
+  })
+
+  test('Should return false if user is not authenticated', async () => {
+    request.getUserSession.mockResolvedValue(null)
+
+    const result = await provideCanRun.method(request)
+
+    expect(result).toBe(false)
+  })
+})
