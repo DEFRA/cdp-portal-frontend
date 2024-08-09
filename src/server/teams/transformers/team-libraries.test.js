835802af1b90c3ecc571a4f3ea0958212d3943e5
Ben Chidgey <ben@feedmypixel.com>
2024-08-09 11:14:49 +0100

    Little bit of a tidy up on teams page
    More soon

--- src/server/teams/transformers/team-libraries.test.js
+++
@@ -1,8 +0,0 @@
-import { teamFixture } from '~/src/__fixtures__/team'
-import { teamLibraries } from '~/src/server/teams/transformers/team-libraries'
-
-describe('#teamLibraries', () => {
-  test('Should provide expected team libraries transformation', () => {
-    expect(teamLibraries(teamFixture.libraries)).toEqual([])
-  })
-})
