baddf1db9539e45528cd1f8997cde30ecc5157e6
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2024-08-14 09:35:09 +0100

    fixes github link, refactors tests

--- src/__fixtures__/create/service-status-in-progress.js
+++ src/__fixtures__/create/service-status-in-progress.js
@@ -19,22 +19,23 @@ const createServiceStatusInProgressFixture = {
     },
     zone: 'protected',
     'cdp-create-workflows': {
-      status: 'in-progress',
-      result: {} // TODO add in v2 result when available
+      status: 'success',
+      main: {
+        workflow: {
+          name: 'cdp-portal-frontend',
+          id: 6667318902,
+          html_url: `https://github.com/${githubOrg}/cdp-create-flows/actions/runs/6667318902`,
+          created_at: '2023-10-27T12:40:27Z',
+          updated_at: '2023-10-27T12:43:55Z',
+          path: '.github/workflows/create-service.yml'
+        }
+      }
     },
     'cdp-tf-svc-infra': {
       status: 'in-progress',
-      pr: {
-        number: 309,
-        sha: 'e8bdd73d9380f4417d9fc7701518e4c0d8ab6e67',
-        ref: 'add-cdp-portal-frontend-to-tenant-services-1698410293717',
-        html_url: `https://github.com/${githubOrg}/cdp-tf-svc-infra/pull/309`,
-        node_id: 'PR_kwDOJVWcQM5d9_9_'
-      },
-      merged_sha: 'b622ed3e68d7ecab868d700fbe73b8696e7a39dc',
       main: {
         workflow: {
-          name: 'Terraform Apply',
+          name: 'cdp-portal-frontend',
           id: 6667318902,
           html_url: `https://github.com/${githubOrg}/cdp-tf-svc-infra/actions/runs/6667318902`,
           created_at: '2023-10-27T12:40:27Z',
@@ -45,17 +46,9 @@ const createServiceStatusInProgressFixture = {
     },
     'cdp-app-config': {
       status: 'in-progress',
-      pr: {
-        number: 180,
-        sha: 'ebb9ac9e250c7849ed79a5c9a1e1eef1a3b15d83',
-        ref: 'add-cdp-portal-frontend-config-1698410297725',
-        html_url: `https://github.com/${githubOrg}/cdp-app-config/pull/180`,
-        node_id: 'PR_kwDOJ1mq8M5d9__U'
-      },
-      merged_sha: '1717bb6a43ad21fb12e4545e4194d7bb3fb4dcf7',
       main: {
         workflow: {
-          name: 'Upload config to s3 (infra-dev)',
+          name: 'cdp-portal-frontend',
           id: 6667297592,
           html_url: `https://github.com/${githubOrg}/cdp-app-config/actions/runs/6667297592`,
           created_at: '2023-10-27T12:38:40Z',
@@ -66,17 +59,9 @@ const createServiceStatusInProgressFixture = {
     },
     'cdp-nginx-upstreams': {
       status: 'in-progress',
-      pr: {
-        number: 123,
-        sha: '40ad087c9195ee541318c760531ca33e586ecaee',
-        ref: 'add-cdp-portal-frontend-config-1698410301096',
-        html_url: `https://github.com/${githubOrg}/cdp-nginx-upstreams/pull/123`,
-        node_id: 'PR_kwDOJxxeSs5d-AAl'
-      },
-      merged_sha: '2395d47bec5c9692c0d56bafbbc40d2533627ec6',
       main: {
         workflow: {
-          name: 'Push to S3',
+          name: 'cdp-portal-frontend',
           id: 6667301828,
           html_url: `https://github.com/${githubOrg}/cdp-nginx-upstreams/actions/runs/6667301828`,
           created_at: '2023-10-27T12:39:01Z',
@@ -89,7 +74,7 @@ const createServiceStatusInProgressFixture = {
       status: 'in-progress',
       main: {
         workflow: {
-          name: 'final-perf-test-1',
+          name: 'cdp-portal-frontend',
           id: null,
           html_url: 'http://localhost:3939/#local-stub',
           created_at: '2024-07-09T13:07:05.842Z',
@@ -102,7 +87,7 @@ const createServiceStatusInProgressFixture = {
       status: 'in-progress',
       main: {
         workflow: {
-          name: 'final-perf-test-1',
+          name: 'cdp-portal-frontend',
           id: null,
           html_url: 'http://localhost:3939/#local-stub',
           created_at: '2024-07-09T13:07:05.842Z',
