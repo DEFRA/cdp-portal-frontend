f5b31394f27abd1b4a766cb6501f5117f11dd5e7
Ben Chidgey <ben@feedmypixel.com>
2024-08-20 17:09:38 +0100

    Guard optional param better

--- src/server/common/helpers/route-lookup/route-lookup.js
+++ src/server/common/helpers/route-lookup/route-lookup.js
@@ -11,9 +11,13 @@ function removeOptionalParams(path, params) {
   const optionalParams = path.matchAll(/{(\w+)\??}/g) ?? []
   let result = path
 
-  for (const match of optionalParams) {
-    if (!paramKeys.includes(match.at(1))) {
-      result = path.replace(match.at(0), '')
+  for (const optionalParam of optionalParams) {
+    if (!paramKeys.includes(optionalParam.at(1))) {
+      const param = optionalParam.at(0)
+
+      if (param) {
+        result = path.replace(param, '')
+      }
     }
   }
 
