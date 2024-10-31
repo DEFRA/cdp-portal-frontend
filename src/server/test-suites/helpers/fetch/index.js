b182c513dae308b63d27b04c3f1a07e6fe887b42
Ben Chidgey <ben@feedmypixel.com>
2024-10-31 11:23:37 +0000

    Switch to use /test-suite endpoint

--- src/server/test-suites/helpers/fetch/index.js
+++ src/server/test-suites/helpers/fetch/index.js
@@ -2,14 +2,6 @@ import { fetchTestRun } from '~/src/server/test-suites/helpers/fetch/fetch-test-
 import { fetchTestRuns } from '~/src/server/test-suites/helpers/fetch/fetch-test-runs.js'
 import { fetchTestSuite } from '~/src/server/test-suites/helpers/fetch/fetch-test-suite.js'
 import { fetchTestSuites } from '~/src/server/test-suites/helpers/fetch/fetch-test-suites.js'
-import { fetchTestSuitesWithLastTestRun } from '~/src/server/test-suites/helpers/fetch/fetch-test-suites-with-last-test-run.js'
 import { runTest } from '~/src/server/test-suites/helpers/fetch/run-test.js'
 
-export {
-  fetchTestRun,
-  fetchTestRuns,
-  fetchTestSuite,
-  fetchTestSuites,
-  fetchTestSuitesWithLastTestRun,
-  runTest
-}
+export { fetchTestRun, fetchTestRuns, fetchTestSuite, fetchTestSuites, runTest }
