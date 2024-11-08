42ebc5de067ea1046490435c6a711580eb79ae6a
Ben Chidgey <ben@feedmypixel.com>
2024-11-08 18:40:26 +0000

    Add submitForm helper
    To prevent form submission and then firing the form submit event to
    trigger the button loader

--- src/server/common/components/button/button.test.js
+++ src/server/common/components/button/button.test.js
@@ -1,9 +1,20 @@
 import { renderTestComponent } from '~/test-helpers/component-helpers.js'
 import { button } from '~/src/server/common/components/button/button.js'
 
+/**
+ *  Helper to preventDefault for the form, and then fire the submit form event. ClientSide JavaScript reacts to the
+ *  submit event and displays the loader. We wish to avoid actually submitting the form in these tests.
+ * @param {HTMLFormElement}form
+ */
+const submitForm = (form) => {
+  form.addEventListener('submit', (event) => event.preventDefault())
+  form.submit()
+}
+
 describe('#button', () => {
   let buttonElem
   let buttonLoaderElem
+  let mockForm
 
   beforeEach(() => {
     jest.useFakeTimers()
@@ -31,6 +42,7 @@ describe('#button', () => {
 
     buttonElem = document.querySelector('[data-testid="app-button"]')
     buttonLoaderElem = document.querySelector('[data-testid="app-loader"]')
+    mockForm = document.getElementById('mock-form')
   })
 
   afterEach(() => {
@@ -38,32 +50,28 @@ describe('#button', () => {
   })
 
   test('Should not disable button before specified delay', () => {
-    document.getElementById('mock-form').submit()
-
+    submitForm(mockForm)
     jest.advanceTimersByTime(100)
 
     expect(buttonElem.getAttribute('disabled')).toBeNull()
   })
 
   test('Should not add loading class to loader before specified delay', () => {
-    document.getElementById('mock-form').submit()
-
+    submitForm(mockForm)
     jest.advanceTimersByTime(100)
 
     expect(buttonElem.getAttribute('disabled')).toBeNull()
   })
 
   test('Should disable button on form submit after specified delay', () => {
-    document.getElementById('mock-form').submit()
-
+    submitForm(mockForm)
     jest.advanceTimersByTime(200)
 
     expect(buttonElem.getAttribute('disabled')).toBe('disabled')
   })
 
   test('Should add loading class to loader on form submit after specified delay', () => {
-    document.getElementById('mock-form').submit()
-
+    submitForm(mockForm)
     jest.advanceTimersByTime(200)
 
     expect(buttonLoaderElem.getAttribute('class')).toContain(
