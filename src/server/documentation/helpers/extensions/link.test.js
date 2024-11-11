cdfdee6c8eab64177d8c55e71744681403d1c7e2
Ben Chidgey <ben@feedmypixel.com>
2024-11-11 11:31:04 +0000

    Support markdown inside of links and headers

--- src/server/documentation/helpers/extensions/link.test.js
+++ src/server/documentation/helpers/extensions/link.test.js
@@ -1,13 +1,21 @@
+import { Marked } from 'marked'
 import { load } from 'cheerio'
 
 import { linkExtension } from '~/src/server/documentation/helpers/extensions/link.js'
 
 describe('#linkExtension', () => {
-  test('Should render an external link', () => {
-    const html = linkExtension.renderer({
-      href: 'https://example.com',
-      text: 'Example'
+  let testMarked
+
+  beforeEach(() => {
+    testMarked = new Marked({
+      pedantic: false,
+      gfm: true,
+      extensions: [linkExtension]
     })
+  })
+
+  test('Should render an external link', async () => {
+    const html = await testMarked.parse('[Example](https://example.com)')
     const $html = load(html)
     const $link = $html('a')
 
@@ -16,11 +24,8 @@ describe('#linkExtension', () => {
     expect($link.attr('rel')).toBe('noopener noreferrer')
   })
 
-  test('Should render a portal link', () => {
-    const html = linkExtension.renderer({
-      href: 'https://portal/home',
-      text: 'Home'
-    })
+  test('Should render a portal link', async () => {
+    const html = await testMarked.parse('[Home](https://portal/home)')
     const $html = load(html)
     const $link = $html('a')
 
@@ -30,11 +35,10 @@ describe('#linkExtension', () => {
     expect($link.attr('rel')).toBeUndefined()
   })
 
-  test('Should render an in page link', () => {
-    const html = linkExtension.renderer({
-      href: 'in-page/link#section-heading',
-      text: 'Section Heading'
-    })
+  test('Should render an in page link', async () => {
+    const html = await testMarked.parse(
+      '[Section Heading](in-page/link#section-heading)'
+    )
     const $html = load(html)
     const $link = $html('a')
 
@@ -42,5 +46,22 @@ describe('#linkExtension', () => {
     expect($link.text()).toBe('Section Heading')
     expect($link.attr('target')).toBeUndefined()
     expect($link.attr('rel')).toBeUndefined()
+  })
+
+  test('Should render a link, containing markdown, as expected', async () => {
+    const html = await testMarked.parse(
+      '[Section `with highlight` link](https://portal/home)'
+    )
+    const $html = load(html)
+    const $link = $html('a')
+    const $code = $link.find('code')
+
+    expect($link.attr('href')).toBe('https://portal/home')
+    expect($link.text()).toBe('Section with highlight link')
+    expect($link.html()).toBe('Section <code>with highlight</code> link')
+    expect($link.attr('target')).toBeUndefined()
+    expect($link.attr('rel')).toBeUndefined()
+
+    expect($code.text()).toBe('with highlight')
   })
 })
