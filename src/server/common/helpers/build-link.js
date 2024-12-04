e8ba8291fdf07e21d1c1bd1ee0b59c13f26bf7ea
Ben Chidgey <ben@feedmypixel.com>
2024-12-04 07:34:37 +0000

    add applink testid

--- src/server/common/helpers/build-link.js
+++ src/server/common/helpers/build-link.js
@@ -1,5 +1,5 @@
 function buildLink(href, textContent, newTab = true) {
-  return `<a class="app-link" href="${href}" ${
+  return `<a class="app-link" href="${href}" data-testid="app-link" ${
     newTab ? `target="_blank" rel="noopener noreferrer"` : ''
   }>${textContent}</a>`
 }
--- src/server/common/helpers/build-link.test.js
+++ src/server/common/helpers/build-link.test.js
@@ -3,13 +3,13 @@ import { buildLink } from '~/src/server/common/helpers/build-link.js'
 describe('#buildLink', () => {
   test('Should build expected link', () => {
     expect(buildLink('https://awesome-cakes.com', 'AweSome Cakes')).toBe(
-      `<a class="app-link" href="https://awesome-cakes.com" target="_blank" rel="noopener noreferrer">AweSome Cakes</a>`
+      `<a class="app-link" href="https://awesome-cakes.com" data-testid="app-link" target="_blank" rel="noopener noreferrer">AweSome Cakes</a>`
     )
   })
 
   test('Should build expected link for same page', () => {
     expect(buildLink('#awesome-cakes', 'In page link to AweSome Cakes')).toBe(
-      `<a class="app-link" href="#awesome-cakes" target="_blank" rel="noopener noreferrer">In page link to AweSome Cakes</a>`
+      `<a class="app-link" href="#awesome-cakes" data-testid="app-link" target="_blank" rel="noopener noreferrer">In page link to AweSome Cakes</a>`
     )
   })
 })
