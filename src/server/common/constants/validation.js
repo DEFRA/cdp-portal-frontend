a18b07d9dd7bf3f9ae94509941c2931d66c0b28c
Ben Chidgey <ben@feedmypixel.com>
2024-12-04 07:34:05 +0000

    Add choose a team validation string

--- src/server/common/constants/validation.js
+++ src/server/common/constants/validation.js
@@ -4,6 +4,7 @@ const validation = {
   enterValue: 'Enter value',
   chooseAnEntry: 'Choose an entry',
   chooseUser: 'Choose a user',
+  chooseTeam: 'Choose a team',
   choose: (choice) =>
     `Choose ${startsWithVowel(choice) ? 'an' : 'a'} ${choice}`,
   minCharacters: (value) => `${value} characters or more`,
