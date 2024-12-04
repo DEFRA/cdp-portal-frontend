243124f250a5519aa2d34e2966065fdb31204268
Ben Chidgey <ben@feedmypixel.com>
2024-12-04 08:26:54 +0000

    Remove unneeded index files

--- src/server/admin/users/controllers/delete/index.js
+++
@@ -1,4 +0,0 @@
-import { confirmDeleteUserController } from '~/src/server/admin/users/controllers/delete/confirm-delete-user.js'
-import { deleteUserController } from '~/src/server/admin/users/controllers/delete/delete-user.js'
-
-export { confirmDeleteUserController, deleteUserController }
--- src/server/admin/users/controllers/index.js
+++ src/server/admin/users/controllers/index.js
@@ -1,22 +1,18 @@
 import { userController } from '~/src/server/admin/users/controllers/user.js'
 import { usersListController } from '~/src/server/admin/users/controllers/users-list.js'
-import {
-  confirmDeleteUserController,
-  deleteUserController
-} from '~/src/server/admin/users/controllers/delete/index.js'
-import {
-  startCreateUserController,
-  findAadUserFormController,
-  findAadUserController,
-  findGithubUserFormController,
-  findGithubUserController,
-  userDetailsController,
-  userDetailsFormController,
-  userSummaryController,
-  createUserController,
-  startEditUserController,
-  editUserController
-} from '~/src/server/admin/users/controllers/save/index.js'
+import { confirmDeleteUserController } from '~/src/server/admin/users/controllers/delete/confirm-delete-user.js'
+import { deleteUserController } from '~/src/server/admin/users/controllers/delete/delete-user.js'
+import { editUserController } from '~/src/server/admin/users/controllers/save/edit-user.js'
+import { startCreateUserController } from '~/src/server/admin/users/controllers/save/start-create-user.js'
+import { findAadUserFormController } from '~/src/server/admin/users/controllers/save/find-aad-user-form.js'
+import { findAadUserController } from '~/src/server/admin/users/controllers/save/find-aad-user.js'
+import { findGithubUserFormController } from '~/src/server/admin/users/controllers/save/find-github-user-form.js'
+import { findGithubUserController } from '~/src/server/admin/users/controllers/save/find-github-user.js'
+import { userDetailsController } from '~/src/server/admin/users/controllers/save/user-details.js'
+import { userDetailsFormController } from '~/src/server/admin/users/controllers/save/user-details-form.js'
+import { userSummaryController } from '~/src/server/admin/users/controllers/save/user-summary.js'
+import { createUserController } from '~/src/server/admin/users/controllers/save/create-user.js'
+import { startEditUserController } from '~/src/server/admin/users/controllers/save/start-edit-user.js'
 
 export {
   userController,
