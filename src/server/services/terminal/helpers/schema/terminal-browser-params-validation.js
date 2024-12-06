78ca6c42fa60c5c719ecfaf0d8b3e238de74ac56
Phil Segal <phil@phisec.co.uk>
2024-12-06 12:03:14 +0000

    481471: Fix terminal browser validation to validate token

---
+++ src/server/services/terminal/helpers/schema/terminal-browser-params-validation.js
@@ -0,0 +1,24 @@
+import Joi from 'joi'
+import { terminalEnvironments } from '~/src/server/services/terminal/helpers/can-launch-terminal.js'
+
+function terminalBrowserParamsValidation(params, options) {
+  const scopes = options.context.auth.credentials?.scope
+
+  const validEnvironments = terminalEnvironments(scopes)
+
+  const validationResult = Joi.object({
+    serviceId: Joi.string().required(),
+    environment: Joi.string()
+      .valid(...validEnvironments)
+      .required(),
+    token: Joi.string().required()
+  }).validate(params, options)
+
+  if (validationResult?.error) {
+    throw validationResult.error
+  }
+
+  return validationResult.value
+}
+
+export { terminalBrowserParamsValidation }
