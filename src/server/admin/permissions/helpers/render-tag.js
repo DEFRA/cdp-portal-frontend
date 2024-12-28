d5bab59412bcd3d564d295803c519f5514e255be
Ben Chidgey <ben@feedmypixel.com>
2024-12-28 09:23:14 +0000

    483148: Remove value format in tag helper

--- src/server/admin/permissions/helpers/render-tag.js
+++ src/server/admin/permissions/helpers/render-tag.js
@@ -1,9 +1,8 @@
 import { renderComponent } from '~/src/server/common/helpers/nunjucks/render-component.js'
-import { formatText } from '~/src/config/nunjucks/filters/index.js'
 
 const renderTag = (value, classes = []) =>
   renderComponent('tag', {
-    text: formatText(value),
+    text: value,
     classes: ['app-link--without-underline', ...classes].join(' '),
     attributes: {
       'data-testid': 'govuk-tag'
