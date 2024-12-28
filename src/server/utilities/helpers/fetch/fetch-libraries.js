666d97e716b2a4efdaffb112c8667d09eba796bd
Ben Chidgey <ben@feedmypixel.com>
2024-12-28 10:13:58 +0000

    483148: Fix endpoints

--- src/server/utilities/helpers/fetch/fetch-libraries.js
+++ src/server/utilities/helpers/fetch/fetch-libraries.js
@@ -1,11 +1,10 @@
 import { config } from '~/src/config/config.js'
 import { fetcher } from '~/src/server/common/helpers/fetch/fetcher.js'
 
-async function fetchLibraries(name = null) {
+async function fetchLibraries(name) {
   const endpoint =
-    config.get('portalBackendUrl') + '/repositories/libraries' + name
-      ? `/${name}`
-      : ''
+    config.get('portalBackendUrl') +
+    `/repositories/libraries${name ? '/name' : ''}`
 
   const { data } = await fetcher(endpoint)
   return data
