1a65770438ff8f8016b6b24c07e496e71d9f5269
Ben Chidgey <ben@feedmypixel.com>
2024-12-28 10:13:58 +0000

    483148: Fix endpoints

--- src/server/utilities/helpers/fetch/fetch-templates.js
+++ src/server/utilities/helpers/fetch/fetch-templates.js
@@ -1,11 +1,10 @@
 import { config } from '~/src/config/config.js'
 import { fetcher } from '~/src/server/common/helpers/fetch/fetcher.js'
 
-async function fetchTemplates(name = null) {
+async function fetchTemplates(name) {
   const endpoint =
-    config.get('portalBackendUrl') + '/repositories/templates' + name
-      ? `/${name}`
-      : ''
+    config.get('portalBackendUrl') +
+    `/repositories/templates${name ? '/name' : ''}`
 
   const { data } = await fetcher(endpoint)
   return data
