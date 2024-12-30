2754b1803d9cf6028b6e20a4ea5f3b7f3d3ac265
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2024-12-30 14:09:00 +0000

    485767: updates portal for new vanity url data

--- src/__fixtures__/vanity-urls.js
+++ src/__fixtures__/vanity-urls.js
@@ -1,18 +1,54 @@
 // Response from portalBackendApi/vanity-urls/cdp-portal-frontend
 const vanityUrlsFixture = {
   'infra-dev': {
-    vanityUrls: ['https://portal-test.cdp-int.defra.cloud']
+    vanityUrls: [
+      {
+        url: 'portal-test.cdp-int.defra.cloud',
+        environment: 'infra-dev',
+        serviceName: 'cdp-portal-frontend',
+        enabled: false,
+        shuttered: false
+      }
+    ]
   },
   management: {
-    vanityUrls: ['https://portal.cdp-int.defra.cloud']
+    vanityUrls: [
+      {
+        url: 'portal.cdp-int.defra.cloud',
+        environment: 'management',
+        serviceName: 'cdp-portal-frontend',
+        enabled: false,
+        shuttered: false
+      }
+    ]
   },
   dev: {
-    vanityUrls: ['https://portal-dev.cdp-int.defra.cloud']
+    vanityUrls: [
+      {
+        url: 'portal-dev.cdp-int.defra.cloud',
+        environment: 'dev',
+        serviceName: 'cdp-portal-frontend',
+        enabled: false,
+        shuttered: false
+      }
+    ]
   },
   test: {
     vanityUrls: [
-      'https://portal-test.cdp-int.defra.cloud',
-      'https://portal-test-other.cdp-int.defra.cloud'
+      {
+        url: 'portal-test.cdp-int.defra.cloud',
+        environment: 'test',
+        serviceName: 'cdp-portal-frontend',
+        enabled: false,
+        shuttered: false
+      },
+      {
+        url: "portal-test-other.cdp-int.defra.cloud'",
+        environment: 'test',
+        serviceName: 'cdp-portal-frontend',
+        enabled: false,
+        shuttered: false
+      }
     ]
   }
 }
