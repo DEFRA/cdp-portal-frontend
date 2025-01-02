7042f42559cf58b062a2756a4827f5b928cf9095
Ben Chidgey <ben@feedmypixel.com>
2025-01-02 18:53:59 +0000

    485776: Make clear filters generic

--- src/client/common/helpers/fetch/filters/clear-filters.js
+++ src/client/common/helpers/fetch/filters/clear-filters.js
@@ -1,13 +1,14 @@
-function clearDeploymentsListFilters(event) {
+function clearFilters(event) {
   event.preventDefault()
 
   const queryParams = new URLSearchParams(window.location.search)
 
   queryParams.delete('service')
+  queryParams.delete('team')
   queryParams.delete('user')
   queryParams.delete('status')
 
   window.location.search = queryParams.toString()
 }
 
-export { clearDeploymentsListFilters }
+export { clearFilters }
