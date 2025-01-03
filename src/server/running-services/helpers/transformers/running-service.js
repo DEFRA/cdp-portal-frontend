68bc54aeba67e25a9d4dcd5ef02847731f75e5ee
Ben Chidgey <ben@feedmypixel.com>
2025-01-03 16:18:00 +0000

    485776: Also filter out non CDP teams on running services page

--- src/server/running-services/helpers/transformers/running-service.js
+++ src/server/running-services/helpers/transformers/running-service.js
@@ -11,7 +11,7 @@ async function transformRunningService(serviceName) {
       ...rs,
       statusClassname: provideStatusClassname(rs.status)
     })),
-    teams: deployableService?.teams ?? []
+    teams: deployableService?.teams?.filter((team) => team.teamId) ?? []
   }
 }
 
