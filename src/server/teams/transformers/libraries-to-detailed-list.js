ccaed1e5ef07a995c45b6386227b4f0f94ca880a
Ben Chidgey <ben@feedmypixel.com>
2025-01-09 17:03:34 +0000

    411709: Updates to admin, teams, utils headers

--- src/server/teams/transformers/libraries-to-detailed-list.js
+++ src/server/teams/transformers/libraries-to-detailed-list.js
@@ -2,14 +2,14 @@ import { buildLink } from '~/src/server/common/helpers/view/build-link.js'
 import { renderTag } from '~/src/server/admin/permissions/helpers/render-tag.js'
 
 function librariesToDetailedList(libraries = []) {
-  const doNotInclude = ['cdp', 'service', 'template', 'repository']
+  const doInclude = ['library']
   const items = libraries.map((library) => ({
     title: {
       html: buildLink(`/utilities/libraries/${library.id}`, library.id, false)
     },
     info: {
       html: library.topics
-        ?.filter((topic) => !doNotInclude.includes(topic))
+        ?.filter((topic) => doInclude.includes(topic))
         .sort()
         .map((topic) => renderTag(topic))
         .join('')
@@ -17,6 +17,7 @@ function librariesToDetailedList(libraries = []) {
   }))
 
   return {
+    isInverse: true,
     items
   }
 }
