8c8c815c2cbaeba6957d1e3ff9cc8657d3105f33
Ben Chidgey <ben@feedmypixel.com>
2025-02-12 10:04:43 +0000

    501233: Mock oidc call

--- src/server/health/controller.test.js
+++ src/server/health/controller.test.js
@@ -1,10 +1,31 @@
+import { config } from '~/src/config/config.js'
 import { createServer } from '~/src/server/index.js'
+import { fetcher } from '~/src/server/common/helpers/fetch/fetcher.js'
+
+jest.mock('~/src/server/common/helpers/fetch/fetcher')
 
 describe('#healthController', () => {
   /** @type {import('@hapi/hapi').Server} */
   let server
 
   beforeAll(async () => {
+    jest.mocked(fetcher).mockImplementationOnce((url) => {
+      // Setting up oidc fetcher call
+      if (url === config.get('oidcWellKnownConfigurationUrl')) {
+        return Promise.resolve({
+          res: {
+            statusCode: 200
+          },
+          payload: {
+            token_endpoint: 'https://mock-login/oauth2/v2.0/token',
+            authorization_endpoint: 'https://mock-login/oauth2/v2.0/authorize'
+          }
+        })
+      }
+
+      return Promise.resolve({})
+    })
+
     server = await createServer()
     await server.initialize()
   })
