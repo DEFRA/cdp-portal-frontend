2bbd16e7382eafdaa9abef66d965413b23853392
Ben Chidgey <ben@feedmypixel.com>
2025-02-20 17:05:53 +0000

    507211: Bits and bobs
    - Add search icon to service typeaheads in lists
    - Remove some index.js :muscle:
    - Guard xhr routes
    - Add xmlhttp header to fetch calls from client
    - Add section highlight styles

--- src/server/deploy-service/controllers/available-versions.js
+++ src/server/deploy-service/controllers/available-versions.js
@@ -1,8 +1,14 @@
+import Boom from '@hapi/boom'
+
 import { fetchAvailableVersions } from '~/src/server/deploy-service/helpers/fetch/fetch-available-versions.js'
 import { relativeDate } from '~/src/server/common/helpers/date/relative-date.js'
 
 const availableVersionsController = {
   handler: async (request, h) => {
+    if (!request.isXhr()) {
+      return Boom.methodNotAllowed('This route is only available via XHR')
+    }
+
     try {
       const availableVersions = await fetchAvailableVersions(
         request.query?.serviceName
