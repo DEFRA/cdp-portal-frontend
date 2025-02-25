dc1cf206c3a62d2a3fd565297cb60a201234770a
Phil Segal <phil@phisec.co.uk>
2025-02-25 13:51:04 +0000

    504612: Add page rendering tests for service page

--- .jest/setup.js
+++ .jest/setup.js
@@ -2,6 +2,8 @@ import { TextEncoder, TextDecoder } from 'node:util'
 import { ReadableStream, TransformStream } from 'node:stream/web'
 import { clearImmediate, setImmediate } from 'node:timers'
 
+import { FileMatcherOptions, toMatchFile } from 'jest-file-snapshot'
+
 // TODO - split into separate projects for client and server side tests.
 // We support both node and client side unit tests. Using jsdom and poly-filling individual Node.js server needs
 
@@ -19,6 +21,16 @@ global.clearImmediate = clearImmediate
 // Stub scroll functions not available in JSDOM
 Element.prototype.scrollIntoView = jest.fn()
 Element.prototype.scroll = jest.fn()
+
+function toMatchFileWithOptions(
+  content,
+  filename,
+  options = { fileExtension: '.html' }
+) {
+  return toMatchFile.call(this, content, filename, options)
+}
+
+expect.extend({ toMatchFile: toMatchFileWithOptions })
 
 afterEach(() => {
   // Clear down JSDOM document after each test
