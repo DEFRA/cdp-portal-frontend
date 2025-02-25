3c815e233c2883ceacada6c458a65d3e34d87407
Ben Chidgey <ben@feedmypixel.com>
2025-02-25 12:52:56 +0000

    498518: Use nav.md from docs repo
    - Use the nav.md file supplied by cdp-documentation to provide the nav
      for docs
    - Add is-active class to current page
    - Update styling
    - Increase test coverage

---
+++ src/server/documentation/helpers/extensions/nav-link.test.js
@@ -0,0 +1,41 @@
+import { Marked } from 'marked'
+import { load } from 'cheerio'
+
+import { navLinkExtension } from '~/src/server/documentation/helpers/extensions/nav-link.js'
+
+describe('#navLinkExtension', () => {
+  let testMarked
+
+  beforeEach(() => {
+    testMarked = new Marked({
+      pedantic: false,
+      gfm: true
+    })
+  })
+
+  const markdownLink = '[A link to some of our docs](/how-to/develop)'
+
+  test('Should mark matching link as active', async () => {
+    testMarked.use({ extensions: [navLinkExtension('how-to/develop')] })
+
+    const html = await testMarked.parse(markdownLink)
+    const $html = load(html)
+    const $link = $html('a')
+
+    expect($link.attr('href')).toBe('/documentation/how-to/develop')
+    expect($link.attr('class')).toBe('app-link is-active')
+  })
+
+  test('Should not mark non-matching link as active', async () => {
+    testMarked.use({
+      extensions: [navLinkExtension('how-to/do-awesome-cake-things')]
+    })
+
+    const html = await testMarked.parse(markdownLink)
+    const $html = load(html)
+    const $link = $html('a')
+
+    expect($link.attr('href')).toBe('/documentation/how-to/develop')
+    expect($link.attr('class')).toBe('app-link')
+  })
+})
