10dd067c4877d47e4c0a82673066ad20fc22c786
Ben Chidgey <ben@feedmypixel.com>
2025-02-28 16:39:24 +0000

    514787: Add some tests around auth work

---
+++ src/__fixtures__/scopes.js
@@ -0,0 +1,11 @@
+// Response from userServiceBackend/scopes
+const scopesFixture = {
+  message: 'success',
+  scopes: ['aabe63e7-87ef-4beb-a596-c810631fc474', 'admin', 'tenant'],
+  scopeFlags: {
+    isAdmin: true,
+    isTenant: false
+  }
+}
+
+export { scopesFixture }
--- src/server/teams/helpers/fetch/fetch-scopes.js
+++ src/server/teams/helpers/fetch/fetch-scopes.js
@@ -3,7 +3,7 @@ import { config } from '~/src/config/config.js'
 import { authedFetchJson } from '~/src/server/common/helpers/fetch/authed-fetch-json.js'
 
 async function fetchScopes(token) {
-  const endpoint = config.get('userServiceBackendUrl') + `/scopes`
+  const endpoint = config.get('userServiceBackendUrl') + '/scopes'
 
   const { payload } = await authedFetchJson(endpoint, token)
   return payload
