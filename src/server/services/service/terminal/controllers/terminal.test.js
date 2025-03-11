67e9c2517f0e7fb84c99d68d453a69d10372ac08
Ben Chidgey <ben@feedmypixel.com>
2025-03-11 17:42:37 +0000

    513759: Update tests to use nock
    So we can mock out the resposne and test the fetch fucntionality

--- src/server/services/service/terminal/controllers/terminal.test.js
+++ src/server/services/service/terminal/controllers/terminal.test.js
@@ -1,11 +1,27 @@
-import { getTerminalEnvs } from '~/src/server/services/service/terminal/controllers/terminal.js'
-import { fetchTenantService } from '~/src/server/common/helpers/fetch/fetch-tenant-service.js'
-import { scopes } from '~/src/server/common/constants/scopes.js'
+import nock from 'nock'
 
-jest.mock('~/src/server/common/helpers/fetch/fetch-tenant-service.js', () => ({
-  fetchTenantService: jest.fn()
+import { getTerminalEnvs } from '~/src/server/services/service/terminal/controllers/terminal.js'
+import { scopes } from '~/src/server/common/constants/scopes.js'
+import { config } from '~/src/config/config.js'
+
+const mockInfoLogger = jest.fn()
+const mockErrorLogger = jest.fn()
+
+jest.mock('~/src/server/common/helpers/logging/logger.js', () => ({
+  createLogger: () => ({
+    info: (value) => mockInfoLogger(value),
+    error: (value) => mockErrorLogger(value)
+  })
 }))
 
+const mockService = 'mock-service'
+const tenantServicesEndpointUrl = new URL(
+  config.get('portalBackendUrl') + `/tenant-services/${mockService}`
+)
+const notFound = {
+  output: { statusCode: 404 },
+  message: 'Not Found'
+}
 const tenantService = {
   'perf-test': { serviceCode: 'RSI', zone: 'protected' },
   'ext-test': { serviceCode: 'RSI', zone: 'protected' },
@@ -17,39 +33,88 @@ const tenantService = {
 }
 
 describe('#getTerminalEnvs', () => {
-  test('Should show standard envs without prod if tenant exists in those environments', async () => {
-    fetchTenantService.mockResolvedValue(tenantService)
-    const envs = await getTerminalEnvs({ serviceName: 'foo' }, [
-      'dev',
-      'test',
-      'perf-test',
-      'prod'
-    ])
-    expect(envs).toStrictEqual(['dev', 'test', 'perf-test'])
+  afterEach(() => {
+    nock.cleanAll()
   })
 
-  test('should only show envs the service exists in', async () => {
-    fetchTenantService.mockResolvedValue({
-      dev: { serviceCode: 'RSI', zone: 'protected' }
-    })
-    const envs = await getTerminalEnvs({ serviceName: 'foo' }, [
+  test('Should show standard envs without prod if tenant exists in those environments', async () => {
+    nock(tenantServicesEndpointUrl.origin)
+      .get(tenantServicesEndpointUrl.pathname)
+      .reply(200, tenantService)
+
+    const result = await getTerminalEnvs(mockService, [
       'dev',
       'test',
       'perf-test',
       'prod'
     ])
-    expect(envs).toStrictEqual(['dev'])
+
+    expect(result).toStrictEqual(['dev', 'test', 'perf-test'])
+  })
+
+  test('Should only show envs the service exists in', async () => {
+    nock(tenantServicesEndpointUrl.origin)
+      .get(tenantServicesEndpointUrl.pathname)
+      .reply(200, {
+        dev: { serviceCode: 'RSI', zone: 'protected' }
+      })
+
+    const result = await getTerminalEnvs(mockService, [
+      'dev',
+      'test',
+      'perf-test',
+      'prod'
+    ])
+
+    expect(result).toStrictEqual(['dev'])
   })
 
   test('Should show prod if they have the break-glass scope', async () => {
-    fetchTenantService.mockResolvedValue(tenantService)
-    const envs = await getTerminalEnvs({ serviceName: 'foo' }, [
+    nock(tenantServicesEndpointUrl.origin)
+      .get(tenantServicesEndpointUrl.pathname)
+      .reply(200, tenantService)
+
+    const result = await getTerminalEnvs(mockService, [
       'dev',
       'test',
       'perf-test',
       'prod',
       scopes.breakGlass
     ])
-    expect(envs).toStrictEqual(['dev', 'test', 'perf-test', 'prod'])
+
+    expect(result).toStrictEqual(['dev', 'test', 'perf-test', 'prod'])
+  })
+
+  test('Should provide empty array when details not found', async () => {
+    nock(tenantServicesEndpointUrl.origin)
+      .get(tenantServicesEndpointUrl.pathname)
+      .reply(404, notFound)
+
+    const result = await getTerminalEnvs(mockService, [])
+
+    expect(result).toEqual([])
+  })
+
+  test('Should provide empty array when serviceName not provided', async () => {
+    nock(tenantServicesEndpointUrl.origin)
+      .get(tenantServicesEndpointUrl.pathname)
+      .reply(404, notFound)
+
+    const result = await getTerminalEnvs()
+
+    expect(result).toEqual([])
+  })
+
+  test('Should log error when error is not a 404', async () => {
+    nock(tenantServicesEndpointUrl.origin)
+      .get(tenantServicesEndpointUrl.pathname)
+      .replyWithError('Boom!')
+
+    const result = await getTerminalEnvs(mockService, [])
+
+    expect(result).toEqual([])
+    expect(mockErrorLogger).toHaveBeenCalledWith(
+      Error('Client request error: Boom!')
+    )
   })
 })
