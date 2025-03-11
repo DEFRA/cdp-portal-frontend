50ad0c300535df4e1c327396e243f24399aecd10
Ben Chidgey <ben@feedmypixel.com>
2025-03-11 17:02:50 +0000

    513759: Update JSDOC and log all errors

--- src/server/common/helpers/fetch/fetch-tenant-service.js
+++ src/server/common/helpers/fetch/fetch-tenant-service.js
@@ -5,26 +5,29 @@ import { createLogger } from '~/src/server/common/helpers/logging/logger.js'
 const logger = createLogger()
 
 /**
- * @typedef {object} TenantService
+ * @typedef {object} ServiceInfo
  * @property {string} serviceCode - Service Code
- * @property {zone} region - CDP Platform Zone
+ * @property {("public" | "protected")} zone - CDP Platform Zone
+ */
+
+/**
+ * @typedef {{[key: ("prod" | "perf-test" | "dev" | "test" | "management" | "infra-dev" | "ext-test")]: {ServiceInfo}}} TenantService
  */
 
 /**
  * @typedef {object} TenantServices
- * @property {TenantService=} dev
- * @property {TenantService=} test
- * @property {TenantService=} perf-test
- * @property {TenantService=} ext-test
- * @property {TenantService=} prod
- * @property {TenantService=} management
- * @property {TenantService=} infra-dev
+ * @property {TenantService} dev
+ * @property {TenantService} test
+ * @property {TenantService} perf-test
+ * @property {TenantService} ext-test
+ * @property {TenantService} prod
+ * @property {TenantService} management
+ * @property {TenantService} infra-dev
  */
 
 /**
- *
  * @param {string} serviceName
- * @returns {Promise<TenantServices|null>}
+ * @returns {Promise<TenantServices|{}>}
  */
 async function fetchTenantService(serviceName) {
   try {
@@ -35,8 +38,15 @@ async function fetchTenantService(serviceName) {
     return payload
   } catch (error) {
     // We are catching here because a 404 can be thrown when a tenant service has not been created
-    logger.info(error, 'Tenant Service not found.')
-    return null
+    const statusCode = error.output.statusCode
+
+    if (statusCode === 404) {
+      logger.info(error, 'Tenant Service not found.')
+    } else {
+      logger.error(error)
+    }
+
+    return {}
   }
 }
 
