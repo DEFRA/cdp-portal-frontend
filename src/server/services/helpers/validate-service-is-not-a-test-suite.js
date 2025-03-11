1941743ff70b37239a835b5f0bf410e72b5a6002
Ben Chidgey <ben@feedmypixel.com>
2025-03-11 17:20:40 +0000

    513759: Update check to be ok if isTestSuite does not exist

--- src/server/services/helpers/validate-service-is-not-a-test-suite.js
+++ src/server/services/helpers/validate-service-is-not-a-test-suite.js
@@ -3,7 +3,7 @@ import Boom from '@hapi/boom'
 export function validateServiceIsNotATestSuite(request, h) {
   const service = request.app.service
 
-  if (service === null || service.isTestSuite) {
+  if (service === null || service?.isTestSuite) {
     return Boom.notFound()
   }
 
