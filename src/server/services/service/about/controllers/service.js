d60609e1f5632d8ef7636b7022dce8081de65646
Ben Chidgey <ben@feedmypixel.com>
2025-03-14 11:35:11 +0000

    513759: Tidy up service page
    - Show links for all environments a user can delpoy to
    - Remove icons that are not needed
    - Update layout to be better
    - Update tests

--- src/server/services/service/about/controllers/service.js
+++ src/server/services/service/about/controllers/service.js
@@ -6,10 +6,20 @@ import { provideIsServiceOwner } from '~/src/server/services/helpers/pre/provide
 import { fetchAvailableVersions } from '~/src/server/deploy-service/helpers/fetch/fetch-available-versions.js'
 import { provideVanityUrls } from '~/src/server/services/service/about/transformers/vanity-urls.js'
 import { transformServiceToSummary } from '~/src/server/services/service/about/transformers/service-to-summary.js'
-import { getEnvironments } from '~/src/server/common/helpers/environments/get-environments.js'
 import { transformRunningServices } from '~/src/server/services/service/about/transformers/running-services.js'
 import { sortBy } from '~/src/server/common/helpers/sort/sort-by.js'
 import { provideApiGateways } from '~/src/server/services/service/about/transformers/api-gateways.js'
+import { fetchTenantService } from '~/src/server/common/helpers/fetch/fetch-tenant-service.js'
+import { sortByEnv } from '~/src/server/common/helpers/sort/sort-by-env.js'
+import { getEnvironments } from '~/src/server/common/helpers/environments/get-environments.js'
+
+const availableEnvironments = ({ scopes, tenantServiceInfo }) => {
+  const environments = getEnvironments(scopes)
+
+  return Object.keys(tenantServiceInfo)
+    .filter((e) => environments.includes(e))
+    .sort(sortByEnv)
+}
 
 const serviceController = {
   options: {
@@ -24,36 +34,40 @@ const serviceController = {
   },
   handler: async (request, h) => {
     const service = request.pre.service
+    const serviceName = service.serviceName
     const isServiceOwner = request.pre.isServiceOwner
 
     if (service === null) {
       return Boom.notFound()
     }
 
-    const [availableVersions, vanityUrls, apiGateways] = await Promise.all([
-      fetchAvailableVersions(service.serviceName),
-      provideVanityUrls(request),
-      provideApiGateways(request)
-    ])
+    const [availableVersions, vanityUrls, apiGateways, tenantServiceInfo] =
+      await Promise.all([
+        fetchAvailableVersions(serviceName),
+        provideVanityUrls(request),
+        provideApiGateways(request),
+        fetchTenantService(serviceName)
+      ])
 
     const latestPublishedImageVersions = availableVersions
       .sort(sortBy('created'))
       .slice(0, 6)
-    const authedUser = await request.getUserSession()
-    const environments = getEnvironments(authedUser?.scope)
 
-    const { runningServices, environmentsWithADeployment } =
-      await transformRunningServices(service.serviceName)
+    const { runningServices } = await transformRunningServices(serviceName)
+
+    const availableServiceEnvironments = availableEnvironments({
+      scopes: request.auth?.credentials?.scope,
+      tenantServiceInfo
+    })
 
     return h.view('services/service/about/views/service', {
-      pageTitle: `${service.serviceName} microservice`,
+      pageTitle: `${serviceName} microservice`,
       summaryList: transformServiceToSummary(service),
       vanityUrls,
       apiGateways,
       service,
       isServiceOwner,
-      environmentsWithADeployment,
-      environments,
+      availableServiceEnvironments,
       runningServices,
       latestPublishedImageVersions,
       breadcrumbs: [
@@ -62,7 +76,7 @@ const serviceController = {
           href: '/services'
         },
         {
-          text: service.serviceName
+          text: serviceName
         }
       ]
     })
