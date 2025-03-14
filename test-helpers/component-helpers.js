e34f1340a30f79dde4102ae2c97ffa0dda184e85
Ben Chidgey <ben@feedmypixel.com>
2025-03-14 11:16:06 +0000

    513759: Add test version of getAssetPath
    This is so we can have css in the page render unit tests, to make it
    easier to understand what has changed when the tests fail and a dev
    views the generated html via IntelliJ

--- test-helpers/component-helpers.js
+++ test-helpers/component-helpers.js
@@ -5,6 +5,18 @@ import { load } from 'cheerio'
 import { camelCase, upperFirst } from 'lodash'
 
 import * as filters from '~/src/config/nunjucks/filters/filters.js'
+import { getAssetPath } from '~/src/config/nunjucks/context/context.js'
+
+/**
+ * Test version of get asset path. Same as app one, but the url references local assets. You need to have run the
+ * build command to have the assets available
+ * @param {string} asset
+ * @returns {string}
+ */
+function getTestAssetPath(asset) {
+  const assetPath = getAssetPath(asset)
+  return assetPath.replace(/^\//, '/.')
+}
 
 const dirname = path.dirname(fileURLToPath(import.meta.url))
 const nunjucksTestEnv = nunjucks.configure(
@@ -27,6 +39,11 @@ const nunjucksTestEnv = nunjucks.configure(
 
 Object.keys(filters).forEach((filter) => {
   nunjucksTestEnv.addFilter(filter, filters[filter])
+})
+
+const globals = { getAssetPath: getTestAssetPath }
+Object.keys(globals).forEach((global) => {
+  nunjucksTestEnv.addGlobal(global, globals[global])
 })
 
 function renderTestComponent(name, params, callBlock) {
