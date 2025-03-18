1f2c4710aaa707725b32b0debe8755c4d1e77a25
Ben Chidgey <ben@feedmypixel.com>
2025-03-18 15:39:52 +0000

    CORE-513759: Add test globals and routeLookup passthrough

---
+++ test-helpers/test-globals.js
@@ -0,0 +1,26 @@
+import { getAssetPath } from '~/src/config/nunjucks/context/context.js'
+
+/**
+ * Ideally minmise mocks or creating test version of context or globals. But if you need to...
+ * This file is the place to put test/mocked context or globals that are used in the Nunjucks templates whilst testing
+ */
+
+/**
+ * Test version of get asset path. Same as app one, but the url references local assets. You need to have run the
+ * build command to have the assets available. Which is automatically done via the `pretest` npm script
+ * @param {string} asset
+ * @returns {string}
+ */
+function getTestAssetPath(asset) {
+  const assetPath = getAssetPath(asset)
+  return assetPath.replace(/^\//, '/.')
+}
+
+/**
+ * Test version of routeLookup. This returns the id so links using routeLookup in tests will have the id as their href
+ * @param {string} id
+ * @returns {string}
+ */
+const routeLookup = (id) => id
+
+export const testGlobals = { getAssetPath: getTestAssetPath, routeLookup }
