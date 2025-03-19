0c7a43db23764a62a0735626d8f0bb55b45db7c0
Phil Segal <phil@phisec.co.uk>
2025-03-19 11:45:11 +0000

    CORE-427: Use plural test-suites endpoint

--- src/server/test-suites/helpers/fetch/fetch-test-suite.js
+++ src/server/test-suites/helpers/fetch/fetch-test-suite.js
@@ -2,7 +2,8 @@ import { config } from '~/src/config/config.js'
 import { fetchJson } from '~/src/server/common/helpers/fetch/fetch-json.js'
 
 async function fetchTestSuite(testSuiteId) {
-  const endpoint = config.get('portalBackendUrl') + `/test-suite/${testSuiteId}`
+  const endpoint =
+    config.get('portalBackendUrl') + `/test-suites/${testSuiteId}`
 
   const { payload } = await fetchJson(endpoint)
   return payload
