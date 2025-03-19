dde399ab2950526a4c5a5d6dc3f76b1c82ef113c
Ben Chidgey <ben@feedmypixel.com>
2025-03-19 17:34:35 +0000

    CORE-420: Updates to docs
    - Open roadmap in new winodw
    - Add ability to open linked images in new window
    - Link pages in docs to pages in cdp-documentation
    - Update initial page uri

--- src/server/documentation/helpers/extensions/link.js
+++ src/server/documentation/helpers/extensions/link.js
@@ -1,8 +1,15 @@
-function isExternalLink(href) {
-  return (
+/**
+ * Should this link open in a new page? If it's a non portal link or localhost link, or a png or jpg linked image
+ * @param {string} href
+ * @returns {boolean}
+ */
+function isNewPageLink(href) {
+  const isExternalLink =
     href.startsWith('http') &&
-    !(href.startsWith('https://portal') || href.startsWith('http://localhost'))
-  )
+    !href.startsWith('https://portal') &&
+    !href.startsWith('http://localhost')
+  const isImageLink = href.endsWith('.png') || href.endsWith('.jpg')
+  return isExternalLink || isImageLink
 }
 
 const linkExtension = {
@@ -11,7 +18,7 @@ const linkExtension = {
   renderer(token) {
     const parsedText = this.parser.parseInline(token.tokens)
 
-    if (isExternalLink(token.href)) {
+    if (isNewPageLink(token.href)) {
       return `<a href="${token.href}" target="_blank" rel="noopener noreferrer">${parsedText}</a>`
     } else {
       return `<a href="${token.href}">${parsedText}</a>`
