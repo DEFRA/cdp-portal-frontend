8f1e53a75feafa093339ee0f2b156ba219a78602
Ben Chidgey <ben@feedmypixel.com>
2025-04-16 10:46:16 +0100

    Fix issue with xhr
    Where autocompletes where not clearing themselves correctly. Theres no
    need to spread the url parmas into xhr params

--- src/client/common/helpers/xhr.test.js
+++ src/client/common/helpers/xhr.test.js
@@ -1,4 +1,3 @@
-import qs from 'qs'
 import fetchMock from 'jest-fetch-mock'
 
 import { xhrRequest } from '~/src/client/common/helpers/xhr.js'
@@ -17,18 +16,6 @@ jest.mock('history', () => ({
     replace: (url, options) => mockHistoryReplace(url, options)
   })
 }))
-
-function setUrlQueryParam(params) {
-  global.window = Object.create(window)
-  Object.defineProperty(window, 'location', {
-    value: {
-      search: qs.stringify(params, {
-        arrayFormat: 'repeat',
-        addQueryPrefix: true
-      })
-    }
-  })
-}
 
 describe('#xhr', () => {
   const mockXhrResponse = '<div data-xhr="xhr-container">Xhr response</div>'
@@ -54,7 +41,6 @@ describe('#xhr', () => {
 
   describe('When making an xhr request', () => {
     beforeEach(async () => {
-      setUrlQueryParam({})
       await xhrRequest(mockUrl, { q: 'mock-param' })
     })
 
@@ -85,31 +71,23 @@ describe('#xhr', () => {
 
   describe('When url has query params', () => {
     beforeEach(async () => {
-      setUrlQueryParam({ ted: true, animals: ['dog', 'cat'] })
       await xhrRequest(mockUrl, { q: 'mock-param' })
     })
 
     test('Should call history push as expected', () => {
-      expect(mockHistoryPush).toHaveBeenCalledWith(
-        '?ted=true&animals=dog&animals=cat&q=mock-param'
-      )
+      expect(mockHistoryPush).toHaveBeenCalledWith('?q=mock-param')
     })
 
     test('Should call history replace as expected', () => {
-      expect(mockHistoryReplace).toHaveBeenCalledWith(
-        '?ted=true&animals=dog&animals=cat&q=mock-param',
-        {
-          xhrData: '<div data-xhr="xhr-container">Xhr response</div>'
-        }
-      )
+      expect(mockHistoryReplace).toHaveBeenCalledWith('?q=mock-param', {
+        xhrData: '<div data-xhr="xhr-container">Xhr response</div>'
+      })
     })
 
     test('Should fire "xhrUpdate" subscribed event', () => {
       expect(mockEventListener.mock.calls[0][0].detail).toEqual({
         params: {
-          animals: ['dog', 'cat'],
-          q: 'mock-param',
-          ted: 'true'
+          q: 'mock-param'
         }
       })
     })
