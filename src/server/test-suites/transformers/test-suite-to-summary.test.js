58f8722d62b31ba688325a95444fd50dda8a122a
Ben Chidgey <ben@feedmypixel.com>
2025-04-22 17:16:13 +0100

    CORE-571: Invert test about section

--- src/server/test-suites/transformers/test-suite-to-summary.test.js
+++ src/server/test-suites/transformers/test-suite-to-summary.test.js
@@ -14,7 +14,7 @@ describe('#testSuiteToEntityDataList', () => {
         attributes: {
           'data-testid': 'govuk-summary-list'
         },
-        classes: 'app-summary-list govuk-!-margin-bottom-8',
+        classes: 'app-summary-list app-summary-list--inverse',
         rows: [
           {
             key: {
