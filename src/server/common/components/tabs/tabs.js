42a5a124570adbda7c7df1e51785ef0ac890c99b
Ben Chidgey <ben@feedmypixel.com>
2025-05-06 07:52:34 +0100

    CORE-571: Reset to pagination defaults on tab url

--- src/server/common/components/tabs/tabs.js
+++ src/server/common/components/tabs/tabs.js
@@ -1,5 +1,7 @@
 import qs from 'qs'
+
 import { history } from '~/src/client/common/helpers/history.js'
+import { pagination } from '~/src/server/common/constants/pagination.js'
 
 function prependQueryParams($elem, params) {
   const hrefParts = $elem.href.split('?')
@@ -11,6 +13,9 @@ function prependQueryParams($elem, params) {
 function tabs($module) {
   const params = qs.parse(location.search, { ignoreQueryPrefix: true })
   const $tabs = Array.from($module.querySelectorAll(`[data-js="app-tab"]`))
+
+  params.page = pagination.page
+  params.size = pagination.size
 
   document.addEventListener('DOMContentLoaded', () => {
     $tabs.forEach(($tab) => prependQueryParams($tab, params))
