de47c9cf452aee6c6f108b252ef07fd228698a2b
Ben Chidgey <ben@feedmypixel.com>
2025-07-08 16:09:32 +0100

    CORE-681: Update gov frontend template
    - use rebrand styles
    - switch our greys out for rebrand colours
    - use defra org colour on home page hero
    - add rebrand global to nunjucks

--- src/config/nunjucks/globals/globals.js
+++ src/config/nunjucks/globals/globals.js
@@ -1,4 +1,6 @@
 import { getComponent } from '~/src/config/nunjucks/globals/get-component.js'
 import { errorMessageHelper } from '~/src/config/nunjucks/globals/error-message-helper.js'
 
-export { getComponent, errorMessageHelper }
+const govukRebrand = true
+
+export { getComponent, errorMessageHelper, govukRebrand }
