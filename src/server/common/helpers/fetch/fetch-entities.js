51077870be7464583adfccf8049a58546550d373
Ben Chidgey <ben@feedmypixel.com>
2025-07-09 15:11:59 +0100

    CORE-696: Remove authed fetch

--- src/server/common/helpers/fetch/fetch-entities.js
+++ src/server/common/helpers/fetch/fetch-entities.js
@@ -3,25 +3,24 @@ import qs from 'qs'
 import { config } from '~/src/config/config.js'
 import { fetchJson } from '~/src/server/common/helpers/fetch/fetch-json.js'
 
+const portalBackendUrl = config.get('portalBackendUrl')
+
 async function fetchEntityStatus(entityName) {
-  const endpoint = `${config.get('portalBackendUrl')}/entities/${entityName}/status`
+  const endpoint = `${portalBackendUrl}/entities/${entityName}/status`
 
   const { payload } = await fetchJson(endpoint)
   return payload
 }
 
 async function fetchEntity(entityName) {
-  const endpoint = `${config.get('portalBackendUrl')}/entities/${entityName}`
+  const endpoint = `${portalBackendUrl}/entities/${entityName}`
 
   const { payload } = await fetchJson(endpoint)
   return payload
 }
 
 async function fetchEntities(queryParams = {}) {
-  const endpoint =
-    config.get('portalBackendUrl') +
-    '/entities' +
-    qs.stringify(queryParams, { arrayFormat: 'repeat', addQueryPrefix: true })
+  const endpoint = `${portalBackendUrl}/entities${qs.stringify(queryParams, { arrayFormat: 'repeat', addQueryPrefix: true })}`
 
   const { payload } = await fetchJson(endpoint)
   return payload ?? []
