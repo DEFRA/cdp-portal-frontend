57c2b94ebc9bad32a4899aef9263c58c6bc5bc87
Ben Chidgey <ben@feedmypixel.com>
2025-07-23 16:11:41 +0100

    Add test hooks back in

--- src/server/create/prototype/transformers/summary-prototype-rows.js
+++ src/server/create/prototype/transformers/summary-prototype-rows.js
@@ -8,7 +8,9 @@ function buildRow(name, value, stepPath) {
 
   return {
     key: { text: name, classes: 'app-summary__heading' },
-    value: { html: value },
+    value: {
+      html: `<span data-testid='row-${name.replaceAll(/\s/g, '')}'>${value}</span>`
+    },
     ...(stepPath && {
       actions: {
         classes: 'app-summary__action',
