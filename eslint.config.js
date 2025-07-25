934d78b8da8767e76e7d8e2f36444974f2135a6b
Ben Chidgey <ben@feedmypixel.com>
2025-07-25 16:31:21 +0100

    CORE-356: Use neostandard and eslint 9

---
+++ eslint.config.js
@@ -0,0 +1,191 @@
+import neostandard from 'neostandard'
+import tsEslintPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import vitest from 'eslint-plugin-vitest'
+import prettier from 'eslint-plugin-prettier'
+import importPlugin from 'eslint-plugin-import'
+import n from 'eslint-plugin-n'
+import promise from 'eslint-plugin-promise'
+import jsdoc from 'eslint-plugin-jsdoc'
+
+const customIgnores = [
+  '.server',
+  '.public',
+  'src/__fixtures__',
+  'coverage',
+  '.husky',
+  '.github',
+  'node_modules',
+  '.sonarlint',
+  'raw-assets',
+  '.prettierrc.js',
+  '.vite/setup-files.js',
+  '*.config.*',
+  'vite.config.*',
+  'vitest.config.*'
+]
+
+export default [
+  ...neostandard({
+    env: ['node', 'vitest'],
+    ignores: [...neostandard.resolveIgnoresFromGitignore(), ...customIgnores],
+    noJsx: true,
+    noStyle: true
+  }),
+  {
+    files: ['**/*.{js,cjs,ts}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      parser: tsParser,
+      sourceType: 'module',
+      globals: {
+        document: true,
+        KeyboardEvent: true,
+        Element: true,
+        HTMLElement: true,
+        location: true,
+        window: true,
+        localStorage: true,
+        fetchMock: true,
+        Option: true
+      },
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: new URL('.', import.meta.url).pathname
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tsEslintPlugin,
+      import: importPlugin,
+      jsdoc,
+      n,
+      promise,
+      prettier,
+      vitest
+    },
+    rules: {
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      'no-console': 'error',
+
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+
+      'jsdoc/require-jsdoc': 'off',
+      'jsdoc/require-param-description': 'off',
+      'jsdoc/require-param-type': 'error',
+      'jsdoc/require-param': 'off',
+      'jsdoc/require-property-description': 'off',
+      'jsdoc/require-returns-description': 'off',
+      'jsdoc/require-returns-type': 'off',
+      'jsdoc/require-returns': 'off',
+
+      'import/extensions': ['error', 'always', { ignorePackages: true }],
+      'import/default': 'off',
+      'import/namespace': 'off',
+      'n/no-extraneous-require': 'off',
+      'n/no-extraneous-import': 'off',
+      'n/no-missing-require': 'off',
+      'n/no-missing-import': 'off',
+
+      'n/no-unpublished-import': [
+        'error',
+        {
+          allowModules: [
+            'copy-webpack-plugin',
+            'clean-webpack-plugin',
+            'terser-webpack-plugin',
+            'webpack-assets-manifest',
+            'webpack-shell-plugin-next'
+          ]
+        }
+      ],
+
+      'vitest/expect-expect': [
+        'error',
+        { assertFunctionNames: ['expect', 'expect*'] }
+      ]
+    },
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.cjs', '.js']
+      },
+      'import/resolver': {
+        node: true,
+        typescript: true
+      }
+    }
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'module'
+    }
+  },
+  {
+    files: ['**/*.cjs'],
+    languageOptions: {
+      sourceType: 'commonjs'
+    },
+    rules: {
+      '@typescript-eslint/no-var-requires': 'off',
+      'n/no-unpublished-require': ['error', { allowModules: [] }]
+    }
+  },
+  {
+    files: ['src/client/**/*.js'],
+    languageOptions: {
+      globals: {
+        document: true,
+        window: true
+      }
+    }
+  },
+  {
+    files: [
+      '.vite/**/*.js',
+      '**/*.test.{js,cjs}',
+      '**/__mocks__/**',
+      '**/__fixtures__/**',
+      'vitest.config.js',
+      'test-helpers/**'
+    ],
+    plugins: { vitest },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./tsconfig.json']
+      },
+      globals: {
+        ...vitest.environments.env.globals
+      }
+    },
+    rules: {
+      ...vitest.configs.recommended.rules,
+      'n/no-unpublished-import': [
+        'error',
+        {
+          allowModules: [
+            'vitest',
+            'filenamify',
+            'nock',
+            'cheerio',
+            '@hapi/catbox-memory',
+            '@testing-library/dom'
+          ]
+        }
+      ]
+    }
+  },
+  {
+    files: ['types/**/*.d.ts'],
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off'
+    }
+  }
+]
