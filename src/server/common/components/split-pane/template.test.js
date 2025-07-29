e11e9dc3d39eb08212ccab5eb065c47e0b8dc808
Ben Chidgey <ben@feedmypixel.com>
2025-07-29 14:52:59 +0100

    CORE-1095: Write todo tests
    - Write these tests, I've been meaning to for a whike
    - Update templates with testing hooks where needed

--- src/server/common/components/split-pane/template.test.js
+++ src/server/common/components/split-pane/template.test.js
@@ -1,2 +1,66 @@
-import { test } from 'vitest'
-test.todo('Split pane component')
+import { renderTestComponent } from '../../../../../test-helpers/component-helpers.js'
+
+describe('Split Pane Component', () => {
+  let $splitPane, $splitPaneNav, $splitPaneContent
+
+  beforeEach(() => {
+    const $component = renderTestComponent('split-pane', {
+      params: {
+        isWide: true
+      },
+      callBlock: [`<p>Welcome to the secrets page</p>`],
+      context: {
+        subNavigation: [
+          {
+            isActive: false,
+            url: 'services/cdp-portal-frontend',
+            label: { text: 'About' }
+          },
+          {
+            isActive: true,
+            url: 'services/cdp-portal-frontend/secrets',
+            label: { text: 'Secrets' }
+          }
+        ]
+      }
+    })
+
+    $splitPane = $component('[data-testid="app-split-pane"]')
+    $splitPaneNav = $component('[data-testid="app-split-pane-nav"]').first()
+    $splitPaneContent = $component(
+      '[data-testid="app-split-pane-content"]'
+    ).first()
+  })
+
+  test('Should render with expected class', () => {
+    expect($splitPane.attr('class')).toEqual('app-split-pane')
+  })
+
+  test('Should render with expected nav items', () => {
+    const $firstLi = $splitPaneNav.find('li').eq(0)
+    const $firstAnchor = $firstLi.find('a')
+    const $secondLi = $splitPaneNav.find('li').eq(1)
+    const $secondAnchor = $secondLi.find('a')
+
+    expect($firstLi.attr('class')).toEqual('app-subnav__section-item')
+    expect($firstAnchor.text()).toEqual('About')
+    expect($firstAnchor.attr('href')).toEqual('services/cdp-portal-frontend')
+
+    expect($secondLi.attr('class')).toEqual(
+      'app-subnav__section-item app-subnav__section-item--current'
+    )
+    expect($secondAnchor.text()).toEqual('Secrets')
+    expect($secondAnchor.attr('href')).toEqual(
+      'services/cdp-portal-frontend/secrets'
+    )
+  })
+
+  test('Should render with expected content', () => {
+    expect($splitPaneContent.find('section').attr('class')).toEqual(
+      'app-content'
+    )
+    expect($splitPaneContent.find('p').text()).toEqual(
+      'Welcome to the secrets page'
+    )
+  })
+})
--- src/server/common/components/step-navigation/template.test.js
+++ src/server/common/components/step-navigation/template.test.js
@@ -1,2 +1,152 @@
-import { test } from 'vitest'
-test.todo('Step Navigation')
+import { renderTestComponent } from '../../../../../test-helpers/component-helpers.js'
+
+describe('StepNavigation', () => {
+  let $component, $stepNavigation
+
+  beforeEach(() => {
+    $component = renderTestComponent('step-navigation', {
+      params: {
+        classes: 'app-step-navigation--slim',
+        width: 100,
+        steps: [
+          {
+            text: 'Step 1',
+            isCurrent: false,
+            isComplete: true,
+            url: '/step-1'
+          },
+          { text: 'Step 2', isCurrent: true },
+          { text: 'Step 3', isCurrent: false }
+        ]
+      }
+    })
+    $stepNavigation = $component('[data-testid="app-step-navigation"]')
+  })
+
+  test('Should render with expected css classes', () => {
+    expect(
+      $component('[data-testid="app-step-navigation__container"]').attr('class')
+    ).toEqual('app-step-navigation-container app-step-navigation--slim')
+  })
+
+  test('Should render all steps with correct text and attributes', () => {
+    const $firstLi = $stepNavigation.find('li').eq(0)
+    const $firstAnchor = $firstLi.find('a')
+    const $firstText = $firstLi.find(
+      '[data-testid="app-step-navigation__text"]'
+    )
+    const $firstMarker = $firstLi.find(
+      '[data-testid="app-step-navigation__marker"]'
+    )
+
+    const $secondLi = $stepNavigation.find('li').eq(1)
+    const $secondAnchor = $secondLi.find('a')
+    const $secondText = $secondLi.find(
+      '[data-testid="app-step-navigation__text"]'
+    )
+    const $secondMarker = $secondLi.find(
+      '[data-testid="app-step-navigation__marker"]'
+    )
+
+    const $thirdLi = $stepNavigation.find('li').eq(2)
+    const $thirdAnchor = $thirdLi.find('a')
+    const $thirdText = $thirdLi.find(
+      '[data-testid="app-step-navigation__text"]'
+    )
+    const $thirdMarker = $thirdLi.find(
+      '[data-testid="app-step-navigation__marker"]'
+    )
+
+    expect($firstLi.attr('class')).toEqual('app-step-navigation__item')
+    expect($firstLi.attr('aria-current')).toBeUndefined()
+    expect($firstAnchor.attr('href')).toEqual('/step-1')
+    expect($firstText.text()).toContain('Step 1')
+    expect($firstMarker.attr('class')).toEqual(
+      'app-step-navigation__point app-step-navigation__point--complete'
+    )
+
+    expect($secondLi.attr('class')).toEqual(
+      'app-step-navigation__item app-step-navigation__item--current'
+    )
+    expect($secondLi.attr('aria-current')).toEqual('step')
+    expect($secondAnchor).toHaveLength(0) // No link for current step
+    expect($secondText.text()).toContain('Step 2')
+    expect($secondMarker.attr('class')).toEqual(
+      'app-step-navigation__point app-step-navigation__point--current'
+    )
+
+    expect($thirdLi.attr('class')).toEqual('app-step-navigation__item')
+    expect($thirdLi.attr('aria-current')).toBeUndefined()
+    expect($thirdAnchor).toHaveLength(0) // No link for yet to be reached step
+    expect($thirdText.text()).toContain('Step 3')
+    expect($thirdMarker.attr('class')).toEqual('app-step-navigation__point')
+  })
+
+  test('Should apply correct styles and text for current step', () => {
+    const $secondLi = $stepNavigation.find('li').eq(1)
+    const $secondAnchor = $secondLi.find('a')
+    const $secondText = $secondLi.find(
+      '[data-testid="app-step-navigation__text"]'
+    )
+    const $secondMarker = $secondLi.find(
+      '[data-testid="app-step-navigation__marker"]'
+    )
+
+    expect($secondLi.attr('class')).toEqual(
+      'app-step-navigation__item app-step-navigation__item--current'
+    )
+    expect($secondLi.attr('aria-current')).toEqual('step')
+    expect($secondAnchor).toHaveLength(0) // No link for current step
+    expect($secondText.text()).toContain('Step 2')
+    expect($secondMarker.attr('class')).toEqual(
+      'app-step-navigation__point app-step-navigation__point--current'
+    )
+  })
+
+  test('Should apply correct styles and text for completed step', () => {
+    const $firstLi = $stepNavigation.find('li').eq(0)
+    const $firstAnchor = $firstLi.find('a')
+    const $firstText = $firstLi.find(
+      '[data-testid="app-step-navigation__text"]'
+    )
+    const $firstMarker = $firstLi.find(
+      '[data-testid="app-step-navigation__marker"]'
+    )
+
+    expect($firstLi.attr('class')).toEqual('app-step-navigation__item')
+    expect($firstLi.attr('aria-current')).toBeUndefined()
+    expect($firstAnchor.attr('href')).toEqual('/step-1')
+    expect($firstText.text()).toContain('Step 1')
+    expect($firstMarker.attr('class')).toEqual(
+      'app-step-navigation__point app-step-navigation__point--complete'
+    )
+  })
+
+  test('Should apply correct styles and text for incomplete step', () => {
+    const $thirdLi = $stepNavigation.find('li').eq(2)
+    const $thirdAnchor = $thirdLi.find('a')
+    const $thirdText = $thirdLi.find(
+      '[data-testid="app-step-navigation__text"]'
+    )
+    const $thirdMarker = $thirdLi.find(
+      '[data-testid="app-step-navigation__marker"]'
+    )
+
+    expect($thirdLi.attr('class')).toEqual('app-step-navigation__item')
+    expect($thirdLi.attr('aria-current')).toBeUndefined()
+    expect($thirdAnchor).toHaveLength(0) // No link for current step
+    expect($thirdText.text()).toContain('Step 3')
+    expect($thirdMarker.attr('class')).toEqual('app-step-navigation__point')
+  })
+
+  test('Should not render a link for incomplete or current steps', () => {
+    const $secondLi = $stepNavigation.find('li').eq(1)
+    const $secondAnchor = $secondLi.find('a')
+
+    const $thirdLi = $stepNavigation.find('li').eq(2)
+    const $thirdAnchor = $thirdLi.find('a')
+
+    expect($secondAnchor).toHaveLength(0) // No link for current step
+    expect($thirdAnchor).toHaveLength(0) // No link for yet to be reached step
+  })
+})
