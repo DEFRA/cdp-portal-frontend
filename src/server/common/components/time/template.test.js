4dc479d8486b5d76d70669620bb499b7360752bb
Ben Chidgey <ben@feedmypixel.com>
2025-09-29 11:05:40 +0100

    switch audit dates to 'dd/MM/yyyy - HH:mm:ss' format

--- src/server/common/components/time/template.test.js
+++ src/server/common/components/time/template.test.js
@@ -12,27 +12,83 @@ describe('Time Component', () => {
     vi.useRealTimers()
   })
 
-  beforeEach(() => {
-    $time = renderTestComponent('time', {
-      params: {
-        datetime: '2023-04-11T16:11:31.722Z'
-      }
+  describe('Default datetime', () => {
+    beforeEach(() => {
+      $time = renderTestComponent('time', {
+        params: {
+          datetime: '2023-04-11T16:11:31.722Z'
+        }
+      })
+    })
+
+    test('Should render app time component', () => {
+      expect($time('[data-testid="app-time"]')).toHaveLength(1)
+    })
+
+    test('Should contain expected rendered date', () => {
+      expect($time('[data-testid="app-time"]').text().trim()).toBe(
+        'Tue 11th Apr 2023 at 16:11'
+      )
+    })
+
+    test('Should have expected datetime attribute', () => {
+      expect($time('[data-testid="app-time"]').attr('datetime')).toBe(
+        '2023-04-11T16:11:31.722Z'
+      )
     })
   })
 
-  test('Should render app time component', () => {
-    expect($time('[data-testid="app-time"]')).toHaveLength(1)
+  describe('With seconds', () => {
+    beforeEach(() => {
+      $time = renderTestComponent('time', {
+        params: {
+          datetime: '2023-04-11T16:11:31.722Z',
+          withSeconds: true
+        }
+      })
+    })
+
+    test('Should render app time component', () => {
+      expect($time('[data-testid="app-time"]')).toHaveLength(1)
+    })
+
+    test('Should contain expected rendered date', () => {
+      expect($time('[data-testid="app-time"]').text().trim()).toBe(
+        'Tue 11th Apr 2023 at 16:11:31'
+      )
+    })
+
+    test('Should have expected datetime attribute', () => {
+      expect($time('[data-testid="app-time"]').attr('datetime')).toBe(
+        '2023-04-11T16:11:31.722Z'
+      )
+    })
   })
 
-  test('Should contain expected rendered date', () => {
-    expect($time('[data-testid="app-time"]').text().trim()).toBe(
-      'Tue 11th Apr 2023 at 16:11'
-    )
-  })
+  describe('With format string', () => {
+    beforeEach(() => {
+      $time = renderTestComponent('time', {
+        params: {
+          datetime: '2023-04-11T16:11:31.722Z',
+          formatString: 'dd/MM/yyyy - HH:mm:ss'
+        }
+      })
+    })
 
-  test('Should have expected datetime attribute', () => {
-    expect($time('[data-testid="app-time"]').attr('datetime')).toBe(
-      '2023-04-11T16:11:31.722Z'
-    )
+    test('Should render app time component', () => {
+      expect($time('[data-testid="app-time"]')).toHaveLength(1)
+    })
+
+    test('Should contain expected rendered date', () => {
+      expect($time('[data-testid="app-time"]').text().trim()).toBe(
+        '11/04/2023 - 16:11:31'
+      )
+    })
+
+    test('Should have expected datetime attribute', () => {
+      expect($time('[data-testid="app-time"]').attr('datetime')).toBe(
+        '2023-04-11T16:11:31.722Z'
+      )
+    })
   })
 })
