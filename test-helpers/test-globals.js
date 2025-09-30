bce6119721166b1f3e0807d8af86e062cb4efbbe
Ben Chidgey <ben@feedmypixel.com>
2025-09-30 16:35:40 +0100

    CORE-1140: set assets folder in tests
    - set assets folder to locally rendered assets via the config ENV
    - use the getAssetPath method in tests
    - remove find and replace for asset path

--- test-helpers/test-globals.js
+++ test-helpers/test-globals.js
@@ -7,17 +7,6 @@ import { getAssetPath } from '../src/config/nunjucks/context/context.js'
  */
 
 /**
- * Test version of get asset path. Same as app one, but the url references local assets. You need to have run the
- * build command to have the assets available. Which is automatically done via the `pretest` npm script
- * @param {string} asset
- * @returns {string}
- */
-function getTestAssetPath(asset) {
-  const assetPath = getAssetPath(asset)
-  return assetPath.replace(/^\//, '/.')
-}
-
-/**
  * Test version of routeLookup. This returns the id so links using routeLookup in tests will have the id as their href
  * @param {string} id
  * @returns {string}
@@ -26,6 +15,6 @@ const routeLookup = (id) => id
 
 export const testGlobals = {
   ...globals,
-  getAssetPath: getTestAssetPath,
+  getAssetPath,
   routeLookup
 }
