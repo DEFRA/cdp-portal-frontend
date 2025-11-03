e1edec0e71e5803e7802b943dd1fcd6f1e870806
Ben Chidgey <ben@feedmypixel.com>
2025-11-03 19:11:34 +0000

    CORE-1613: Update shuttering endpoint

--- src/server/services/helpers/fetch/fetch-shuttering-url.js
+++ src/server/services/helpers/fetch/fetch-shuttering-url.js
@@ -1,8 +1,9 @@
 import { config } from '../../../../config/config.js'
 import { fetchJson } from '../../../common/helpers/fetch/fetch-json.js'
 
-async function fetchShutteringUrl(url) {
-  const endpoint = config.get('portalBackendUrl') + `/shuttering/url/${url}`
+async function fetchShutteringUrl(serviceName, url) {
+  const endpoint =
+    config.get('portalBackendUrl') + `/shuttering/${serviceName}/${url}`
 
   const { payload } = await fetchJson(endpoint)
   return payload
