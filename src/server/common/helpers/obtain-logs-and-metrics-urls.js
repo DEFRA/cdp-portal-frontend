373470df5a8ed2f73fd0d084a8475a1061c3145a
Ben Chidgey <ben@feedmypixel.com>
2025-11-06 10:15:52 +0000

    CORE-1721: Filter and sort logs and metrics links

--- src/server/common/helpers/obtain-logs-and-metrics-urls.js
+++ src/server/common/helpers/obtain-logs-and-metrics-urls.js
@@ -1,19 +1,29 @@
 import { sortKeyByEnv } from './sort/sort-by-env.js'
 
-function obtainLogsAndMetricsUrls(environmentDetails) {
+function obtainLogsAndMetricsUrls(
+  environmentDetails,
+  availableServiceEnvironments
+) {
   const logsDetails = Object.entries(environmentDetails)
+    .filter(([environment]) =>
+      availableServiceEnvironments.includes(environment)
+    )
     .flatMap(([environment, details]) => ({ environment, ...details.logs }))
     .sort(sortKeyByEnv('environment'))
 
-  const metricsDetails = Object.entries(environmentDetails).reduce(
-    (metricUrls, [environment, details]) => {
-      return {
-        ...metricUrls,
-        [environment]: details.metrics
-      }
-    },
-    {}
-  )
+  const metricsDetails = Object.entries(environmentDetails)
+    .filter(([environment]) =>
+      availableServiceEnvironments.includes(environment)
+    )
+    .map(([environment, details]) => ({
+      environment,
+      metrics: details.metrics
+    }))
+    .sort(sortKeyByEnv('environment'))
+    .reduce((metricsLinks, { environment, metrics }) => {
+      metricsLinks[environment] = metrics
+      return metricsLinks
+    }, {})
 
   return { logsDetails, metricsDetails }
 }
