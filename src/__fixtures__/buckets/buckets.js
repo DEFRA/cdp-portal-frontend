aaec62eb23c2ecb82514b486848cd464f7476652
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2025-11-11 18:21:16 +0000

    CORE-1758 removes unused fixtures. clients

--- src/__fixtures__/buckets/buckets.js
+++
@@ -1,9 +0,0 @@
-// Response from portalBackendApi/buckets/infra-dev/cdp-portal-backend
-
-const bucketsWithPendingFixture = {
-  service: 'cdp-portal-backend',
-  environment: 'infra-dev',
-  buckets: ['SERVICE_BUCKET', 'z_ordered_bucket', 'a_lower_case_bucket']
-}
-
-export { bucketsWithPendingFixture }
