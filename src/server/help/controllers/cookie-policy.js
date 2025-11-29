fd2140bfc35c4b8f3c45928036e381fba2551051
Ben Chidgey <ben@feedmypixel.com>
2025-11-29 10:36:31 +0000

    Add in server extension points to check Hapi tracing
    capabilities

--- src/server/help/controllers/cookie-policy.js
+++ src/server/help/controllers/cookie-policy.js
@@ -1,6 +1,19 @@
+const serverExtensionPoint = (extName) => (request, h) => {
+  request.logger.info(`Call from ${extName} extension point`)
+
+  return h.continue
+}
+
 const cookiePolicy = {
   options: {
-    id: 'help/cookies'
+    id: 'help/cookies',
+    ext: {
+      onPreAuth: { method: serverExtensionPoint('onPreAuth') },
+      onPostAuth: { method: serverExtensionPoint('onPostAuth') },
+      onPreHandler: { method: serverExtensionPoint('onPreHandler') },
+      onPostHandler: { method: serverExtensionPoint('onPostHandler') },
+      onPreResponse: { method: serverExtensionPoint('onPreResponse') }
+    }
   },
   handler: (_request, h) =>
     h.view('help/views/cookie-policy', {
