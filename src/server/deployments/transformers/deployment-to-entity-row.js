d4563005096f51ca72a6c11e1b77869a5a43d7b1
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2026-01-07 09:39:45 +0000

    CORE-1852 rework deployment list

--- src/server/deployments/transformers/deployment-to-entity-row.js
+++ src/server/deployments/transformers/deployment-to-entity-row.js
@@ -2,99 +2,21 @@ import { formatText } from '../../../config/nunjucks/filters/filters.js'
 import { augmentStatus } from '../helpers/augment-status.js'
 import { sanitiseUser } from '../../common/helpers/sanitisation/sanitise-user.js'
 import { provideDeploymentStatusClassname } from '../helpers/provide-deployment-status-classname.js'
-import {
-  renderComponent,
-  renderIcon
-} from '../../common/helpers/nunjucks/render-component.js'
-
-function buildDescription(deployment) {
-  const tooltipText = `Service Deployment: ${deployment.version} - ${formatText(deployment.status)}`
-
-  return `<a class="app-link app-entity-table__row-header" href="/deployments/${deployment.environment.toLowerCase()}/${deployment.cdpDeploymentId}" data-testid="app-link">${deployment.service}</a>
-          <div class="app-!-layout-centered govuk-!-margin-top-1">
-            ${renderComponent('tool-tip', { text: tooltipText }, [
-              renderIcon('instance-icon', {
-                classes: 'app-icon--tiny govuk-!-margin-right-1'
-              })
-            ])}
-            <div class="app-entity-table__row-caption">Microservice deployment</div>
-          </div>`
-}
 
 function deploymentToEntityRow(deployment) {
   const status = augmentStatus(deployment)
-  const teams = deployment?.teams
-    ?.filter((team) => team.teamId)
-    ?.map((team) => ({
-      kind: 'link',
-      value: team.name,
-      url: `/teams/${team.teamId}`
-    }))
-
-  const icon = deployment.isOwner
-    ? renderIcon('star-icon', { classes: 'app-icon--minuscule' })
-    : ''
-
   return {
-    cells: [
-      {
-        headers: 'owner',
-        isCentered: true,
-        classes: 'app-entity-table__cell--owned',
-        entity: { kind: 'html', value: icon }
-      },
-      {
-        headers: 'description',
-        html: buildDescription(deployment)
-      },
-      {
-        headers: 'version',
-        entity: {
-          kind: 'link',
-          value: deployment.version,
-          url: `https://github.com/DEFRA/${deployment.service}/releases/tag/${deployment.version}`,
-          newWindow: true
-        }
-      },
-      {
-        headers: 'status',
-        entity: {
-          kind: 'tag',
-          value: formatText(status),
-          classes: provideDeploymentStatusClassname(status)
-        }
-      },
-      {
-        headers: 'kind',
-        entity: {
-          kind: 'tag',
-          value: 'Deployment',
-          classes: 'govuk-tag--blue'
-        }
-      },
-      {
-        headers: 'by',
-        entity: {
-          kind: 'text',
-          value: sanitiseUser(deployment.user?.displayName)
-        }
-      },
-      {
-        headers: 'team',
-        entity: {
-          kind: 'list',
-          value: teams?.length ? teams : null
-        }
-      },
-      {
-        headers: 'started',
-        entity: {
-          kind: 'date',
-          value: deployment.created,
-          withSeconds: true
-        }
-      }
-    ]
+    isOwner: deployment.isOwner,
+    service: deployment.service,
+    version: deployment.version,
+    kind: 'deployment',
+    kindText: 'Deployment',
+    kindClass: 'govuk-tag--blue',
+    statusText: formatText(status),
+    statusClass: provideDeploymentStatusClassname(status),
+    teams: deployment.teams,
+    by: sanitiseUser(deployment.user?.displayName),
+    started: deployment.created
   }
 }
 
--- src/server/deployments/transformers/migration-to-entity-row.js
+++ src/server/deployments/transformers/migration-to-entity-row.js
@@ -1,104 +1,20 @@
-import { noValue } from '../../common/constants/no-value.js'
 import { formatText } from '../../../config/nunjucks/filters/filters.js'
 import { sanitiseUser } from '../../common/helpers/sanitisation/sanitise-user.js'
 import { provideStatusClassname } from '../helpers/provide-status-classname.js'
-import {
-  renderComponent,
-  renderIcon
-} from '../../common/helpers/nunjucks/render-component.js'
-
-function buildDescription(migration) {
-  const tooltipText = `Database Update: ${migration.version} - ${formatText(migration.status)}`
-
-  return `<a class="app-link app-entity-table__row-header" href="/deployments/database-updates/${migration.environment.toLowerCase()}/${migration.cdpMigrationId}" data-testid="app-link">${migration.service}</a>
-         <div class="app-!-layout-centered govuk-!-margin-top-1">
-            ${renderComponent('tool-tip', { text: tooltipText }, [
-              renderIcon('database-icon', {
-                classes: 'app-icon--tiny govuk-!-margin-right-1'
-              })
-            ])}
-            <div class="app-entity-table__row-caption">Database update</div>
-         </div>`
-}
 
 function migrationToEntityRow(migration) {
-  const teams = migration?.teams
-    ?.filter((team) => team.teamId)
-    ?.map((team) => ({
-      kind: 'link',
-      value: team.name,
-      url: `/teams/${team.teamId}`
-    }))
-
-  const icon = migration.isOwner
-    ? renderComponent(
-        'tool-tip',
-        {
-          text: 'Owned Service',
-          classes: 'app-tool-tip--small'
-        },
-        [renderIcon('star-icon', { classes: 'app-icon--minuscule' })]
-      )
-    : ''
-
   return {
-    cells: [
-      {
-        headers: 'owner',
-        isCentered: true,
-        classes: 'app-entity-table__cell--owned',
-        entity: { kind: 'html', value: icon }
-      },
-      {
-        headers: 'description',
-        html: buildDescription(migration)
-      },
-      {
-        headers: 'version',
-        entity: {
-          kind: 'text',
-          value: migration.version ?? noValue
-        }
-      },
-      {
-        headers: 'status',
-        entity: {
-          kind: 'tag',
-          value: formatText(migration.status),
-          classes: provideStatusClassname(migration.status)
-        }
-      },
-      {
-        headers: 'kind',
-        entity: {
-          kind: 'tag',
-          value: 'Update',
-          classes: 'govuk-tag--blue'
-        }
-      },
-      {
-        headers: 'by',
-        entity: {
-          kind: 'text',
-          value: sanitiseUser(migration.user?.displayName)
-        }
-      },
-      {
-        headers: 'team',
-        entity: {
-          kind: 'list',
-          value: teams?.length ? teams : null
-        }
-      },
-      {
-        headers: 'started',
-        entity: {
-          kind: 'date',
-          value: migration.created,
-          withSeconds: true
-        }
-      }
-    ]
+    isOwner: migration.isOwner,
+    service: migration.service,
+    version: migration.version,
+    kind: 'update',
+    kindText: 'Update',
+    kindClass: 'govuk-tag--blue',
+    statusText: formatText(migration.status),
+    statusClass: provideStatusClassname(migration.status),
+    teams: migration.teams,
+    by: sanitiseUser(migration.user?.displayName),
+    started: migration.created
   }
 }
 
