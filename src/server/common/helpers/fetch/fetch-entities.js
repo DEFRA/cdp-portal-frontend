8f90daa020ef1b16730af01443bc1b472107dc93
David Beale <david.beale@bealearts.co.uk>
2026-02-12 15:07:12 +0000

    Lint

--- src/server/common/helpers/fetch/fetch-entities.js
+++ src/server/common/helpers/fetch/fetch-entities.js
@@ -18,13 +18,12 @@ async function fetchEntities(queryParams = {}) {
     arrayFormat: 'repeat',
     addQueryPrefix: true
   })}`
-  console.log(endpoint)
+
   const { payload } = await fetchJson(endpoint)
   return payload ?? []
 }
 
 function fetchTestSuites(queryParams) {
-  console.log(queryParams)
   return fetchEntities({
     type: entityTypes.testSuite,
     status: [entityStatuses.created, entityStatuses.creating],
