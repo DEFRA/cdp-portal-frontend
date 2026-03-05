1e5fb9606d6f1e61801da949ab352eacdaf117f8
David Beale <david.beale@bealearts.co.uk>
2026-03-05 17:07:53 +0000

    Only use string paths

--- src/server/common/helpers/build-error-details.js
+++ src/server/common/helpers/build-error-details.js
@@ -2,12 +2,14 @@ function buildErrorDetails(errorDetails) {
   return errorDetails.reduce((errors, detail) => {
     return {
       ...Object.fromEntries(
-        detail.path.map((path) => [
-          path,
-          {
-            message: detail.message
-          }
-        ])
+        detail.path
+          .filter((path) => typeof path === 'string')
+          .map((path) => [
+            path,
+            {
+              message: detail.message
+            }
+          ])
       ),
       ...errors
     }
