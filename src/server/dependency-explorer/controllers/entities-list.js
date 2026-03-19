42741d4d17979543d3717149f0ce11cd49fdf6d6
David Beale <david.beale@bealearts.co.uk>
2026-03-19 17:21:58 +0000

    tag

--- src/server/dependency-explorer/controllers/entities-list.js
+++ src/server/dependency-explorer/controllers/entities-list.js
@@ -5,7 +5,18 @@ export default {
   handler: async (request, h) => {
     const rows = [
       {
-        entity: 'cdp-portal-frontend'
+        entity: 'cdp-portal-frontend',
+        entityVersion: '1.1.1',
+        entityVersionTag: 'latest',
+        teams: ['one', 'two'],
+        dependencyVersion: '0.4.5'
+      },
+      {
+        entity: 'cdp-portal-backend',
+        entityVersion: '2.1.0',
+        entityVersionTag: 'latest',
+        teams: ['one', 'two'],
+        dependencyVersion: '0.4.5'
       }
     ]
 
@@ -21,7 +32,7 @@ export default {
             isLeftAligned: true
           },
           { id: 'entityVersion', text: 'Service version', width: '10' },
-          { id: 'teams', text: 'Teams', width: '15' },
+          { id: 'teams', text: 'Team', width: '15' },
           { id: 'dependencyVersion', text: 'Dependency version', width: '10' },
           { id: 'environments', text: 'Environments', width: '25' }
         ],
