af06acbe108c243344e43c6db2a45c3e42837a83
David Beale <david.beale@bealearts.co.uk>
2026-03-19 16:20:09 +0000

    Filters

--- src/server/dependencies-explorer/controllers/dependencies-list.js
+++ src/server/dependencies-explorer/controllers/dependencies-list.js
@@ -4,7 +4,26 @@ export default {
   },
   handler: async (request, h) => {
     return h.view('dependencies-explorer/views/dependencies-list', {
-      pageTitle: 'Dependencies Explorer'
+      pageTitle: 'Dependencies Explorer',
+
+      tableData: {
+        headers: [
+          {
+            id: 'entity',
+            text: 'Consuming Service',
+            width: '15',
+            isLeftAligned: true
+          },
+          { id: 'entityVersion', text: 'Service version', width: '10' },
+          { id: 'teams', text: 'Teams', width: '15' },
+          { id: 'dependencyVersion', text: 'Dependency version', width: '10' },
+          { id: 'environments', text: 'Environments', width: '25' }
+        ],
+        rows: [],
+        noResult: 'No dependencies found',
+        isWide: false,
+        isInverse: true
+      }
     })
   }
 }
