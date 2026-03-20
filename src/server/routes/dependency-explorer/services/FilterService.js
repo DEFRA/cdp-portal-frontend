b6d7d68b52abf833391080dfdc9ef84eb90fd4d1
David Beale <david.beale@bealearts.co.uk>
2026-03-20 17:55:10 +0000

    Use advanced autocomplete

---
+++ src/server/routes/dependency-explorer/services/FilterService.js
@@ -0,0 +1,37 @@
+import { config } from '#config/config.js'
+
+export async function getDependencyType() {
+  const baseUrl = config.get('sbomExplorerBackendUrl')
+  const searchUrl = new URL('/filters/dependency-type', baseUrl)
+
+  const resp = await fetch(searchUrl, { method: 'GET' })
+  if (resp.status === 200) {
+    return resp.json()
+  }
+
+  return []
+}
+
+export async function searchDependencies(query) {
+  const baseUrl = config.get('sbomExplorerBackendUrl')
+  const searchUrl = new URL('/filters/dependencies', baseUrl)
+
+  if (query.type) {
+    searchUrl.searchParams.set('type', query.type)
+  }
+
+  if (query.partialName) {
+    searchUrl.searchParams.set('partialName', query.partialName + '%')
+  }
+
+  if (query.environment) {
+    searchUrl.searchParams.set('environment', query.environment)
+  }
+
+  const resp = await fetch(searchUrl, { method: 'GET' })
+  if (resp.status === 200) {
+    return resp.json()
+  }
+
+  return []
+}
