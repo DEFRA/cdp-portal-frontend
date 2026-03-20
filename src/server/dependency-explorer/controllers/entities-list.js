b0ca3aabf8a841239d1322f3534cea9324dba2f6
David Beale <david.beale@bealearts.co.uk>
2026-03-20 09:46:28 +0000

    Show envs

--- src/server/dependency-explorer/controllers/entities-list.js
+++ src/server/dependency-explorer/controllers/entities-list.js
@@ -1,24 +1,62 @@
+import { formatText } from '#config/nunjucks/filters/filters.js'
+import { getEnvironments } from '#server/common/helpers/environments/get-environments.js'
+
 export default {
   options: {
     id: 'dependency-explorer'
   },
   handler: async (request, h) => {
+    const userSession = request.auth.credentials
+    const environments = getEnvironments(userSession?.scope)
+
     const rows = [
       {
-        entity: 'cdp-portal-frontend',
+        entity: 'cdp-example-frontend',
         entityVersion: '1.1.1',
         entityVersionTag: 'latest',
-        teams: ['one', 'two'],
-        dependencyVersion: '0.4.5'
+        teams: [{ value: 'Platform', url: '/' }],
+        dependencyVersion: '0.4.5',
+        envs: environments.map((env) => ({
+          id: env.toLowerCase(),
+          selected: [].includes(env)
+        }))
       },
       {
-        entity: 'cdp-portal-backend',
+        entity: 'cdp-example-frontend',
+        entityVersion: '1.1.0',
+        entityVersionTag: '',
+        teams: [{ value: 'Platform', url: '/' }],
+        dependencyVersion: '0.4.5',
+        envs: environments.map((env) => ({
+          id: env.toLowerCase(),
+          selected: ['prod', 'test', 'dev'].includes(env)
+        }))
+      },
+      {
+        entity: 'cdp-example-backend',
         entityVersion: '2.1.0',
         entityVersionTag: 'latest',
-        teams: ['one', 'two'],
-        dependencyVersion: '0.4.5'
+        teams: [{ value: 'Platform', url: '/' }],
+        dependencyVersion: '0.4.5',
+        envs: environments.map((env) => ({
+          id: env.toLowerCase(),
+          selected: ['dev'].includes(env)
+        }))
+      },
+      {
+        entity: 'cdp-example-backend',
+        entityVersion: '2.0.7',
+        entityVersionTag: '',
+        teams: [{ value: 'Platform', url: '/' }],
+        dependencyVersion: '0.4.5',
+        envs: environments.map((env) => ({
+          id: env.toLowerCase(),
+          selected: ['prod', 'test'].includes(env)
+        }))
       }
     ]
+
+    const supportVerticalHeadings = environments.length >= 5
 
     return h.view('dependency-explorer/views/entities-list', {
       pageTitle: 'Dependencies Explorer',
@@ -31,10 +69,14 @@ export default {
             width: '15',
             isLeftAligned: true
           },
-          { id: 'entityVersion', text: 'Service version', width: '10' },
           { id: 'teams', text: 'Team', width: '15' },
-          { id: 'dependencyVersion', text: 'Dependency version', width: '10' },
-          { id: 'environments', text: 'Environments', width: '25' }
+          { id: 'dependencyVersion', text: 'Version', width: '10' },
+          ...environments.map((env) => ({
+            ...(supportVerticalHeadings && { verticalText: true }),
+            id: env.toLowerCase(),
+            text: formatText(env),
+            width: env.length
+          }))
         ],
         rows,
         noResult: 'No dependencies found',
