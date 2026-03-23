d850a9d1f6f72441c5fd232bdae029ba84ae3143
David Beale <david.beale@bealearts.co.uk>
2026-03-23 11:33:49 +0000

    Combined search

--- src/server/routes/dependency-explorer/api/deps-search/route.js
+++ src/server/routes/dependency-explorer/api/deps-search/route.js
@@ -1,9 +1,16 @@
+import { renderTag } from '#server/common/helpers/view/render-tag.js'
 import { searchDependencies } from '../../FilterService.js'
 
 export async function GET(request) {
   const results = await searchDependencies(request.query)
 
   return {
-    suggestions: results.map(({ name }) => ({ text: name, value: name }))
+    suggestions: results.map(({ name, type }) => ({
+      value: `${type}:${name}`,
+      text: name,
+      hint: renderTag({
+        text: type
+      })
+    }))
   }
 }
