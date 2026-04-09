2a4d771917f1a3f4d386181044325c821ddfd6bc
David Beale <david.beale@bealearts.co.uk>
2026-04-09 12:03:17 +0100

    Bring back nodemon for server

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { NodePackageImporter } from 'sass-embedded'
 import path from 'node:path'
+import { spawn } from 'node:child_process'
 
 const govukFrontendPath = path.dirname(
   require.resolve('govuk-frontend/package.json')
