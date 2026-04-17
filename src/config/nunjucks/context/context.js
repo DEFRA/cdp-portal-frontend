0e613f0e5fc87b5bbd5223e612d3e41fe0e79a45
David Beale <david.beale@bealearts.co.uk>
2026-04-17 12:00:27 +0100

    Fix context not caching asset manifest

--- src/config/nunjucks/context/context.js
+++ src/config/nunjucks/context/context.js
@@ -1,91 +1,41 @@
 import path from 'node:path'
 import { readFileSync } from 'node:fs'
-import { scopes } from '@defra/cdp-validation-kit'
 
-import { config } from '../../config.js'
-import { isXhr } from '../../../server/common/helpers/is-xhr.js'
-import { isIe } from './is-ie.js'
-import { createLogger } from '../../../server/common/helpers/logging/logger.js'
+import { config } from '#/config/config.js'
 import { buildNavigation } from './build-navigation.js'
-import { defaultOption } from '../../../server/common/helpers/options/default-option.js'
-import { noValue } from '../../../server/common/constants/no-value.js'
-import { eventName } from '../../../client/common/constants/event-name.js'
-import { getAnnouncements } from './announcements.js'
-import { hasScopeDecorator } from '../../../server/common/helpers/decorators/has-scope.js'
+import { createLogger } from '#/server/common/helpers/logging/logger.js'
 
 const logger = createLogger()
 const assetPath = config.get('assetPath')
+const manifestPath = path.join(
+  config.get('root'),
+  '.public/.vite/manifest.json'
+)
 
-/**
- * Get asset path from assets-manifest.json
- * @param {string} asset
- * @returns {string}
- */
-function getAssetPath(asset) {
-  if (!config.get('isProduction')) {
-    return `${assetPath}/${asset}`
-  }
+let viteManifest
 
-  const manifestPath = path.join(
-    config.get('root'),
-    '.public/.vite/manifest.json'
-  )
-
-  let viteManifest
-
-  if (!viteManifest) {
+export function context(request) {
+  if (config.get('isProduction') && !viteManifest) {
     try {
       viteManifest = JSON.parse(readFileSync(manifestPath, 'utf-8'))
     } catch (error) {
-      logger.error(error, `Webpack ${path.basename(manifestPath)} not found`)
+      logger.error(`Vite ${path.basename(manifestPath)} not found`)
     }
   }
 
-  const webpackAssetPath = viteManifest[`${asset}`]?.file ?? ''
-
-  return `${assetPath}/${webpackAssetPath}`
-}
-
-/**
- * @param {import('@hapi/hapi').Request | null} request
- */
-async function context(request) {
-  const userSession = request?.auth?.credentials
-  const isInternetExplorer = isIe(request.headers['user-agent'])
-  const announcements = await getAnnouncements({
-    request,
-    userSession,
-    isInternetExplorer
-  })
-  const serviceConfig = config.get('service')
-
   return {
-    announcements,
-    appBaseUrl: config.get('appBaseUrl'),
-    appServiceName: serviceConfig.name,
     assetPath: `${assetPath}/assets`,
-    userSession,
-    blankOption: defaultOption,
+    serviceName: config.get('serviceName'),
+    serviceUrl: '/',
     breadcrumbs: [],
-    eventName,
-    getAssetPath,
-    githubOrg: config.get('githubOrg'),
-    hasScope: hasScopeDecorator(request),
-    isAdmin: userSession?.isAdmin ?? false,
-    isAuthenticated: userSession?.isAuthenticated ?? false,
-    isTenant: userSession?.isTenant ?? false,
-    isXhr: isXhr.call(request),
-    navigation: await buildNavigation(request, userSession),
-    noValue,
-    routeLookup: (id, options) => request.routeLookup(id, options),
-    requestPath: request.path,
-    scopes,
-    serviceEnvironment: serviceConfig.environment,
-    serviceVersion: serviceConfig.version,
-    supportChannel: config.get('supportChannel'),
-    isProduction: config.get('isProduction'),
-    isTest: config.get('isTest')
+    navigation: buildNavigation(request),
+    getAssetPath(asset) {
+      if (!config.get('isProduction')) {
+        return `${assetPath}/${asset}`
+      }
+
+      const viteAssetPath = viteManifest?.[asset]?.file
+      return `${assetPath}/${viteAssetPath ?? asset}`
+    }
   }
 }
-
-export { context, getAssetPath }
