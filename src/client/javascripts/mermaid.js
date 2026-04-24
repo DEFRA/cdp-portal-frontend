0a8faa37e618dd3741f92c46e3731db9483118cf
David Beale <DavidBeale@users.noreply.github.com>
2026-04-24 16:25:08 +0100

    Feature/core 2887 mermaid theme (#1283)
    * Define theme and load icons
    
    * Apply theme to permissions diagram

--- src/client/javascripts/mermaid.js
+++ src/client/javascripts/mermaid.js
@@ -1,3 +1,25 @@
 import mermaid from 'mermaid'
 
-mermaid.initialize({ startOnLoad: true })
+mermaid.initialize({
+  startOnLoad: true,
+  logLevel: 'error',
+  securityLevel: 'loose',
+  theme: 'base',
+  themeVariables: {
+    lineColor: '#0b0c0c',
+    primaryColor: '#bbd4e6',
+    primaryTextColor: '#0b0c0c',
+    primaryBorderColor: '#d2e2f1',
+    secondaryColor: '#F5F5F5',
+    secondaryBorderColor: '#d2e2f1',
+    tertiaryColor: '#f4f8fb',
+    tertiaryBorderColor: '#d2e2f1'
+  }
+})
+
+mermaid.registerIconPacks([
+  {
+    name: 'logos',
+    loader: () => import('@iconify-json/logos').then((module) => module.icons)
+  }
+])
