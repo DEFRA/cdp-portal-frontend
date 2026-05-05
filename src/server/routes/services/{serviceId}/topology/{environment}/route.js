71d5d13f250d88dd3358db72eacc93d5fc502211
David Beale <david.beale@bealearts.co.uk>
2026-05-05 11:13:06 +0100

    Better resource key

--- src/server/routes/services/{serviceId}/topology/{environment}/route.js
+++ src/server/routes/services/{serviceId}/topology/{environment}/route.js
@@ -39,7 +39,70 @@ export default async function (request) {
   const { entity } = request.app
   const environment = request.params.environment
 
-  const topology = await fetchTopology(entity.name, environment)
+  //  const topology = await fetchTopology(entity.name, environment)
+
+  const topology = [
+    {
+      name: 'cdp-portal-backend',
+      type: 4,
+      teams: [
+        {
+          teamId: 'platform',
+          name: 'Platform'
+        }
+      ],
+      resources: [
+        {
+          name: 'cdp_platform_portal_events',
+          icon: 'aws-sns',
+          links: []
+        },
+        {
+          name: 'ecr_image_push',
+          icon: 'aws-sns',
+          links: []
+        },
+        {
+          name: 'cdp_platform_portal_events',
+          icon: 'aws-sqs',
+          links: [
+            {
+              service: 'cdp-portal-backend',
+              resource: 'cdp_platform_portal_events',
+              type: 'subscription'
+            }
+          ]
+        },
+        {
+          name: 'cdp_workflow_events',
+          icon: 'aws-sqs',
+          links: []
+        },
+        {
+          name: 'ecr_image_push',
+          icon: 'aws-sqs',
+          links: [
+            {
+              service: 'cdp-portal-backend',
+              resource: 'ecr_image_push',
+              type: 'subscription'
+            }
+          ]
+        },
+        {
+          name: 'ecs_deployments',
+          icon: 'aws-sqs',
+          links: [
+            {
+              service: null,
+              resource: 'ecs_deployments',
+              type: 'subscription'
+            }
+          ]
+        }
+      ]
+    }
+  ]
 
   const servicesPerTeam = Object.groupBy(topology, ({ teams }) =>
     teams.map(({ teamId }) => teamId).join('_')
@@ -48,6 +111,9 @@ export default async function (request) {
   return {
     topology,
     servicesPerTeam,
-    environment
+    environment,
+    nodeKey: (service, resource) =>
+      `${[service.name, resource?.name, resource?.icon].join('_')}`,
+    linkKey: (link) => `${[link.service, link?.resource].join('_')}`
   }
 }
