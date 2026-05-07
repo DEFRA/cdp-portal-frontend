71c0b898340e776a33d9d1621689b461a1631cf5
David Beale <david.beale@bealearts.co.uk>
2026-05-07 11:13:05 +0100

    Init page

---
+++ src/server/routes/services/{serviceId}/diagnostics/{environment}/route.js
@@ -0,0 +1,71 @@
+import {
+  commonServiceExtensions,
+  provideNotFoundIfPrototypeExtension
+} from '#server/common/helpers/ext/extensions.js'
+import { SERVICE } from '#server/common/patterns/entities/tabs/constants.js'
+import { provideSubNav } from '#server/helpers/provide-sub-navigation.js'
+import { serviceParamsValidation } from '#server/services/helpers/schema/service-params-validation.js'
+import { scopes } from '@defra/cdp-validation-kit'
+import { Boom } from '@hapi/boom'
+import { formatText } from '#config/nunjucks/filters/filters.js'
+
+export const ext = [
+  ...commonServiceExtensions,
+  provideNotFoundIfPrototypeExtension,
+  {
+    type: 'onPostHandler',
+    method: provideSubNav('diagnostics', SERVICE, { withoutAll: true }),
+    options: {
+      sandbox: 'plugin'
+    }
+  }
+]
+
+export const options = {
+  id: 'services/{serviceId}/diagnostics/{environment}',
+  validate: {
+    params: serviceParamsValidation,
+    failAction: () => Boom.boomify(Boom.notFound())
+  },
+  auth: {
+    mode: 'required',
+    access: {
+      scope: [/* scopes.tenant, */ scopes.admin] // TODO: Enable for tenants
+    }
+  }
+}
+
+export default async function (request) {
+  const { entity } = request.app
+  const environment = request.params.environment
+
+  const nodeKey = (service, resource) =>
+    `${[service.name, resource?.type, resource?.name].filter(Boolean).join('_')}`
+
+  const linkKey = (link) =>
+    link.service
+      ? `${[link.service, link.type, link.resource].filter(Boolean).join('_')}`
+      : `AWS`
+
+  return {
+    environment,
+    nodeKey,
+    linkKey,
+    breadcrumbs: [
+      {
+        text: 'Services',
+        href: '/services'
+      },
+      {
+        text: entity.name,
+        href: `/services/${entity.name}`
+      },
+      {
+        text: 'Diagnostics'
+      },
+      {
+        text: formatText(environment)
+      }
+    ]
+  }
+}
