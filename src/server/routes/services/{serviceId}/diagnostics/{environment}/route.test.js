dea3a9020996da8ac16548b999710084ae9dbe2e
David Beale <david.beale@bealearts.co.uk>
2026-05-11 09:27:53 +0100

    Snapshots

--- src/server/routes/services/{serviceId}/diagnostics/{environment}/route.test.js
+++ src/server/routes/services/{serviceId}/diagnostics/{environment}/route.test.js
@@ -4,59 +4,18 @@ import {
   mockServiceEntityCall
 } from '#test-helpers/common-page-rendering.js'
 import { statusCodes } from '@defra/cdp-validation-kit'
-import { fetchTopology } from '#server/services/helpers/fetch/fetch-topology.js'
 
 vi.mock('#server/common/helpers/fetch/fetch-entities.js')
 vi.mock('#server/common/helpers/auth/get-user-session.js')
 vi.mock('#server/services/helpers/fetch/fetch-shuttering-urls.js')
-vi.mock('#server/services/helpers/fetch/fetch-topology.js')
 
 const serviceName = 'mock-service-with-resources'
 
-describe('Topology page', () => {
+describe('Diagnostics page', () => {
   let server
 
   beforeAll(async () => {
     mockServiceEntityCall(serviceName)
-    fetchTopology.mockResolvedValue([
-      {
-        name: serviceName,
-        type: 'Backend',
-        teams: [
-          {
-            teamId: 'platform',
-            name: 'Platform'
-          }
-        ],
-        resources: [
-          {
-            name: 'decision_notification',
-            type: 'sns',
-            icon: 'aws-sns',
-            links: []
-          },
-          {
-            name: 'error_notification',
-            type: 'sns',
-            icon: 'aws-sns',
-            links: []
-          },
-          {
-            name: 'message_clearance_request',
-            type: 'sqs',
-            icon: 'aws-sqs',
-            links: [
-              {
-                service: null,
-                resource: 'error_notification.fifo',
-                type: 'sns',
-                access: 'subscription'
-              }
-            ]
-          }
-        ]
-      }
-    ])
 
     server = await initialiseServer()
   })
