1408a2177cdf8d12c9de047ea668de8ca86504a2
David Beale <david.beale@bealearts.co.uk>
2026-05-19 13:51:05 +0100

    Edit flow

--- src/server/admin/users/controllers/edit/edit-user-form.js
+++ src/server/admin/users/controllers/edit/edit-user-form.js
@@ -1,9 +1,13 @@
 import Joi from 'joi'
 import Boom from '@hapi/boom'
 
-import { sessionNames } from '../../../../common/constants/session-names.js'
-import { fetchCdpUser } from '../../helpers/fetch/fetchers.js'
+import { sessionNames } from '#server/common/constants/session-names.js'
+import {
+  fetchCdpUser,
+  searchGithubUsers
+} from '../../helpers/fetch/fetchers.js'
 import { userIdValidation } from '@defra/cdp-validation-kit'
+import { buildOptions } from '#server/common/helpers/options/build-options.js'
 
 const startEditUserController = {
   options: {
@@ -11,6 +15,10 @@ const startEditUserController = {
     validate: {
       params: Joi.object({
         userId: userIdValidation
+      }),
+      query: Joi.object({
+        githubSearch: Joi.string().allow(''),
+        github: Joi.string().allow('')
       }),
       failAction: () => Boom.boomify(Boom.badRequest())
     }
@@ -20,8 +28,27 @@ const startEditUserController = {
 
     const user = await fetchCdpUser(request.params?.userId)
 
+    const query = request?.query
+    const githubSearch = query?.githubSearch ?? user?.github
+    const github = query?.github
+
+    const searchGithubUsersResponse = githubSearch
+      ? await searchGithubUsers(githubSearch)
+      : null
+    const githubUsers = searchGithubUsersResponse ?? []
+
     return h.view('admin/users/views/edit/edit-user-form', {
       user,
+      formValues: { githubSearch, github },
+      githubUsers: buildOptions(
+        githubUsers.map((githubUser) => ({
+          text: `@${githubUser.github} ${
+            githubUser.name ? `- ${githubUser.name}` : ''
+          }`,
+          value: githubUser.github
+        })),
+        false
+      ),
       splitPaneBreadcrumbs: [
         {
           text: 'Admin',
--- src/server/admin/users/controllers/edit/edit-user.js
+++ src/server/admin/users/controllers/edit/edit-user.js
@@ -1,20 +1,41 @@
+import Joi from 'joi'
 import { config } from '../../../../../config/config.js'
 import { sessionNames } from '../../../../common/constants/session-names.js'
+import { userIdValidation } from '@defra/cdp-validation-kit'
+import Boom from '@hapi/boom'
 
 const editUserController = {
+  options: {
+    validate: {
+      params: Joi.object({
+        userId: userIdValidation
+      }),
+      payload: Joi.object({
+        actionType: Joi.string().valid('cancel', 'save'),
+        github: Joi.string().allow(''),
+        githubSearch: Joi.string().allow('')
+      }),
+      failAction: () => Boom.boomify(Boom.badRequest())
+    }
+  },
   handler: async (request, h) => {
-    const cdpUser = request.pre?.stepData
+    const userId = request.params.userId
+    const actionType = request.payload.actionType
+    const github = request.payload.github
+
+    if (actionType === 'cancel') {
+      return h.redirect('/admin/users/' + userId)
+    }
+
     const editUserEndpointUrl = `${config.get('userServiceBackendUrl')}/users/${
-      cdpUser.userId
+      userId
     }`
 
     try {
       await request.authedFetchJson(editUserEndpointUrl, {
         method: 'patch',
         payload: {
-          name: cdpUser.name,
-          email: cdpUser.email,
-          github: cdpUser.github
+          github
         }
       })
 
@@ -23,11 +44,11 @@ const editUserController = {
         type: 'success'
       })
 
-      return h.redirect('/admin/users/' + cdpUser.userId)
+      return h.redirect('/admin/users/' + userId)
     } catch (error) {
       request.yar.flash(sessionNames.globalValidationFailures, error.message)
 
-      return h.redirect('/admin/users/summary')
+      return h.redirect('/admin/users/' + userId)
     }
   }
 }
