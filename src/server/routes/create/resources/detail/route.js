6b003691fb9f3f40bd007be3faee86c84927ad1f
David Beale <david.beale@bealearts.co.uk>
2026-06-18 15:08:12 +0100

    Have to stringify logs

--- src/server/routes/create/resources/detail/route.js
+++ src/server/routes/create/resources/detail/route.js
@@ -52,8 +52,7 @@ export async function POST(request, h) {
 
   const resourceRequest = serializeBasket(basket)
 
-  request.logger.info(basket, 'Basket:')
-  request.logger.info(resourceRequest, 'Requested resources:')
+  request.logger.info(`Requested resources: ${JSON.stringify(resourceRequest)}`)
 
   try {
     const { payload } = await request.authedFetchJson(
@@ -72,7 +71,7 @@ export async function POST(request, h) {
 
     return h.redirect('/create/resources/summary')
   } catch (error) {
-    request.logger.error(error, 'Request failed:')
+    request.logger.error(error, 'Resources request failed:')
 
     if (error?.data?.res.statusCode === 400 && error.data.payload?.errors) {
       request.yar.flash(sessionNames.validationFailure, {
