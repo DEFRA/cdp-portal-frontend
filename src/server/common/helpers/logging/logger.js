e5bdfef4c3f9b4787fb13165a09d1fa9479400f2
David Beale <david.beale@bealearts.co.uk>
2026-06-24 13:30:32 +0100

    cleanup for new rules

--- src/server/common/helpers/logging/logger.js
+++ src/server/common/helpers/logging/logger.js
@@ -4,8 +4,6 @@ import { loggerOptions } from './logger-options.js'
 
 const logger = pino(loggerOptions)
 
-function createLogger() {
+export function createLogger() {
   return logger
 }
-
-export { createLogger }
