1d01dd23db913d20a1561af4fa51703b7fb80977
David Beale <david.beale@bealearts.co.uk>
2026-07-01 11:35:06 +0100

    Show mock playground data

--- src/server/routes/services/{serviceId}/diagnostics/{environment}/route.js
+++ src/server/routes/services/{serviceId}/diagnostics/{environment}/route.js
@@ -43,13 +43,39 @@ export default async function (request) {
   const environment = request.params.environment
 
   const runningServices = await fetchRunningServices(entity.name)
+  // TODO: Fetch if dev
+  const playground = {
+    alerts: [
+      {
+        name: 'btms-gateway-health-status',
+        type: 'custom',
+        uid: 'e9821b3c84ffdfc70d30319ed096a0343f5aecf0',
+        annotations: {
+          description: 'BTMS Gateway service health check',
+          runbook_url:
+            'https://eaflood.atlassian.net/wiki/spaces/ALVS/pages/5735743637/BTMS+Gateway+Support+Runbook',
+          summary:
+            "BTMS Gateway health check of the application and it's dependencies."
+        }
+      }
+    ],
+    dashboards: [
+      {
+        url: 'https://metrics.prod.cdp-int.defra.cloud/d/btms-gateway-all-3da12fe2/btms-gateway-all',
+        type: 'custom',
+        uid: 'btms-gateway-all-3da12fe2',
+        scope: '',
+        version: 1
+      }
+    ]
+  }
 
   const serviceDeployedInEnvironment = runningServices.some(
     (service) => service.environment === environment
   )
 
   const resources = transformResources(entity.environments[environment])
-
+  console.log(resources)
   function logViewUrl(type) {
     return `https://logs.${environment}.cdp-int.defra.cloud/_dashboards/app/discover#/view/${entity.name}-${type}`
   }
@@ -58,6 +84,7 @@ export default async function (request) {
     environment,
     serviceDeployedInEnvironment,
     resources,
+    playground,
     renderLinks,
     logViewUrl,
     apigwMetricLink,
