4d58fdc63e05ee98fcf0ecbfcbfdc66126900300
David Beale <david.beale@bealearts.co.uk>
2026-07-03 11:21:07 +0100

    Call to BE

--- src/server/routes/services/{serviceId}/diagnostics/PlaygroundService.js
+++ src/server/routes/services/{serviceId}/diagnostics/PlaygroundService.js
@@ -1,28 +1,19 @@
+import { config } from '#config/config.js'
+import { fetchJson } from '#server/common/helpers/fetch/fetch-json.js'
+
 export async function getPlayground(serviceName) {
-  // TODO: fetch
-  return Promise.resolve({
-    alerts: [
-      {
-        name: 'btms-gateway-health-status',
-        type: 'custom',
-        uid: 'e9821b3c84ffdfc70d30319ed096a0343f5aecf0',
-        annotations: {
-          description: 'BTMS Gateway service health check',
-          runbook_url:
-            'https://eaflood.atlassian.net/wiki/spaces/ALVS/pages/5735743637/BTMS+Gateway+Support+Runbook',
-          summary:
-            "BTMS Gateway health check of the application and it's dependencies."
-        }
-      }
-    ],
-    dashboards: [
-      {
-        url: 'https://metrics.prod.cdp-int.defra.cloud/d/btms-gateway-all-3da12fe2/btms-gateway-all',
-        type: 'custom',
-        uid: 'btms-gateway-all-3da12fe2',
-        scope: '',
-        version: 1
-      }
-    ]
-  })
+  const endpoint = `${config.get('portalBackendUrl')}/entities/${serviceName}/diagnostics/playground`
+
+  try {
+    const { payload = {} } = await fetchJson(endpoint)
+    return {
+      alerts: payload?.alerts ?? [],
+      dashboards: payload?.dashboards ?? []
+    }
+  } catch (_) {
+    return {
+      alerts: 'PENDING',
+      dashboards: 'PENDING'
+    }
+  }
 }
