aa79ced353a7d432088bf564de0289969d6c911c
David Beale <david.beale@bealearts.co.uk>
2026-07-03 11:58:35 +0100

    Handle 204 from BE

--- src/server/routes/services/{serviceId}/diagnostics/PlaygroundService.js
+++ src/server/routes/services/{serviceId}/diagnostics/PlaygroundService.js
@@ -4,16 +4,18 @@ import { fetchJson } from '#server/common/helpers/fetch/fetch-json.js'
 export async function getPlayground(serviceName) {
   const endpoint = `${config.get('portalBackendUrl')}/entities/${serviceName}/diagnostics/playground`
 
-  try {
-    const { payload = {} } = await fetchJson(endpoint)
-    return {
-      alerts: payload?.alerts ?? [],
-      dashboards: payload?.dashboards ?? []
-    }
-  } catch (_) {
+  const { res, payload = {} } = await fetchJson(endpoint)
+
+  // BE fetching from queues
+  if (res.statusCode === 204) {
     return {
       alerts: 'PENDING',
       dashboards: 'PENDING'
     }
+  }
+
+  return {
+    alerts: payload?.alerts ?? [],
+    dashboards: payload?.dashboards ?? []
   }
 }
