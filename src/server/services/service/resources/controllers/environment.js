ccec61135860ef3466293a2a51eaa91439a13321
David Beale <david.beale@bealearts.co.uk>
2026-07-06 15:04:17 +0100

    Mock pending queue

--- src/server/services/service/resources/controllers/environment.js
+++ src/server/services/service/resources/controllers/environment.js
@@ -23,6 +23,20 @@ export const environmentResourcesController = {
 
     const resources = await fetchResources(entity.name, environment)
 
+    resources.sqs_queues.push({
+      resource: 'sqs',
+      icon: 'aws-sqs',
+      name: 'decision_notification',
+      resourceRequestId: '1234',
+      properties: {
+        name: 'decision_notification',
+        fifo_queue: true,
+        content_based_deduplication: false,
+        receive_wait_time_seconds: 30,
+        subscriptions: ['decision_notification.fifo']
+      }
+    })
+
     if (!resources) throw new Error('Failed to load resources')
 
     const hasNoResources = !Object.entries(resources).some(
