48eac6064d665c4c44ca25c0bb4ab12bbb0d46b9
David Beale <david.beale@bealearts.co.uk>
2026-07-10 12:47:28 +0100

    Update tests

--- src/config/nunjucks/context/build-navigation.test.js
+++ src/config/nunjucks/context/build-navigation.test.js
@@ -112,7 +112,7 @@ describe('#buildNavigation', () => {
             attributes: { 'data-testid': 'nav-create' }
           }
         ],
-        primary: buildPrimaryNav({ isTenant: true, includeRequests: true }),
+        primary: buildPrimaryNav({ isTenant: true, includeRequests: false }),
         admin: undefined
       })
     })
@@ -135,7 +135,7 @@ describe('#buildNavigation', () => {
             attributes: { 'data-testid': 'nav-create' }
           }
         ],
-        primary: buildPrimaryNav({ includeRequests: true }),
+        primary: buildPrimaryNav({ includeRequests: false }),
         admin: undefined
       })
     })
@@ -201,7 +201,7 @@ describe('#buildNavigation', () => {
               attributes: { 'data-testid': 'nav-create' }
             }
           ],
-          primary: buildPrimaryNav({ includeRequests: true })
+          primary: buildPrimaryNav({ includeRequests: false })
         })
       })
     })
@@ -228,7 +228,7 @@ describe('#buildNavigation', () => {
               attributes: { 'data-testid': 'nav-create' }
             }
           ],
-          primary: buildPrimaryNav({ includeRequests: true }),
+          primary: buildPrimaryNav({ includeRequests: false }),
           admin: [
             {
               text: 'Exit Test as Tenant Mode',
--- src/server/routes/requests/ResourcesService.js
+++ src/server/routes/requests/ResourcesService.js
@@ -4,44 +4,7 @@ import { fetchJson } from '#server/common/helpers/fetch/fetch-json.js'
 export async function getPendingResourceRequests() {
   const endpoint = `${config.get('portalBackendUrl')}/resources/requests?status=requested&status=pending`
 
-  // const { payload = {} } = await fetchJson(endpoint)
+  const { payload = {} } = await fetchJson(endpoint)
 
-  return [
-    {
-      requestedAt: '2026-07-10T09:19:51.28Z',
-      requestedBy: {
-        id: '94f3387e-8ad7-4bbe-8a99-0f118a85240b',
-        displayName: 'David Beale  (Defra)'
-      },
-      workflow: {
-        workflow_run_id: 29082683001,
-        run_url:
-          'https://api.github.com/repos/DEFRA/cdp-tenant-config/actions/runs/29082683001',
-        html_url:
-          'https://github.com/DEFRA/cdp-tenant-config/actions/runs/29082683001'
-      },
-      pullRequest: {
-        url: 'https://github.com/DEFRA/cdp-tenant-config/pull/1424',
-        number: 1424
-      },
-      status: 'requested',
-      teams: [{ teamId: 'platform', name: 'Platform' }]
-    },
-    {
-      requestedAt: '2026-07-10T10:19:51.28Z',
-      requestedBy: {
-        id: '94f3387e-8ad7-4bbe-8a99-0f118a85240b',
-        displayName: 'Joe Blogs  (Defra)'
-      },
-      workflow: {
-        workflow_run_id: 29082683001,
-        run_url:
-          'https://api.github.com/repos/DEFRA/cdp-tenant-config/actions/runs/29082683001',
-        html_url:
-          'https://github.com/DEFRA/cdp-tenant-config/actions/runs/29082683001'
-      },
-      status: 'pending',
-      teams: [{ teamId: 'platform', name: 'Platform' }]
-    }
-  ]
+  return payload
 }
