6ffa88bab21e920a8d1a5b1698d21f496b598175
David Beale <david.beale@bealearts.co.uk>
2026-07-13 14:50:14 +0100

    Require non-empty basket

--- src/server/routes/create/resources/detail/route.js
+++ src/server/routes/create/resources/detail/route.js
@@ -2,7 +2,11 @@ import { formatText } from '#config/nunjucks/filters/filters.js'
 import { sessionNames } from '#server/common/constants/session-names.js'
 import { scopes } from '@defra/cdp-validation-kit'
 import { provideFormContextValues } from '#server/common/helpers/form/provide-form-context-values.js'
-import { formatBasketResource, initBasket } from '../domain/basket.js'
+import {
+  formatBasketResource,
+  initBasket,
+  numberOfItemsInBasket
+} from '../domain/basket.js'
 
 export const options = {
   auth: {
@@ -42,6 +46,16 @@ export async function POST(request, h) {
   const basket = request.yar.get(sessionNames.resourcesBasket)
 
   if (!basket) {
+    return h.redirect('/create/resources/detail')
+  }
+
+  if (numberOfItemsInBasket(basket) === 0) {
+    request.yar.flash(sessionNames.validationFailure, {
+      formMessages: [
+        { text: 'Please add at least one resource to the request' }
+      ]
+    })
+
     return h.redirect('/create/resources/detail')
   }
 
