91affec8bf9ff95477849e8817d860866839311d
David Beale <david.beale@bealearts.co.uk>
2026-07-17 12:36:49 +0100

    Show recent non-pending request and their status

--- src/server/routes/requests/route.js
+++ src/server/routes/requests/route.js
@@ -1,7 +1,12 @@
 import { scopes } from '@defra/cdp-validation-kit'
-import { getPendingResourceRequests } from './ResourcesService.js'
+import {
+  getPendingResourceRequests,
+  getRecentNonPendingResourceRequests
+} from './ResourcesService.js'
 import { parseISO, subMinutes } from 'date-fns'
 import { fetchCdpUser } from '#server/admin/users/helpers/fetch/fetchers.js'
+import { formatText } from '#config/nunjucks/filters/filters.js'
+
 
 export const options = {
   auth: {
@@ -21,9 +26,15 @@ export default async function (request) {
     cdpUser = await fetchCdpUser(userSession.id)
   }
 
-  const pendingResourceRequests = await getPendingResourceRequests(
-    userIsAdmin ? [] : cdpUser.teams.map(({ teamId }) => teamId)
-  )
+  const [pendingResourceRequests, recentNonPendingResourceRequests] =
+    await Promise.all([
+      getPendingResourceRequests(
+        userIsAdmin ? [] : cdpUser.teams.map(({ teamId }) => teamId)
+      ),
+      getRecentNonPendingResourceRequests(
+        userIsAdmin ? [] : cdpUser.teams.map(({ teamId }) => teamId)
+      )
+    ])
 
   const hasGeneratingRequests = pendingResourceRequests.some(
     (request) => request.status === 'pending'
@@ -36,14 +47,39 @@ export default async function (request) {
       parseISO(request.requestedAt) < subMinutes(now, 20)
   )
 
+  const hasGeneratingRequestsFailed = recentNonPendingResourceRequests.some(
+    (request) => request.status === 'failed'
+  )
+
   return {
-    pendingResourceRequests,
+    pendingOrRecentResourceRequests: [
+      ...pendingResourceRequests,
+      ...recentNonPendingResourceRequests
+    ],
     hasGeneratingRequests,
     hasGeneratingRequestsTakingTooLong,
+    hasGeneratingRequestsFailed,
+    formatStatus,
+    statusTagColour,
     breadcrumbs: [
       {
         text: 'Requests'
       }
     ]
   }
+}
+
+function formatStatus(status) {
+  if (status === 'closed') return 'Cancelled'
+  if (status === 'merged') return 'Provisioning'
+
+  return formatText(status)
+}
+
+function statusTagColour(status) {
+  if (status === 'closed') return 'govuk-tag--grey'
+  if (status === 'failed') return 'govuk-tag--red'
+  if (status === 'merged') return 'govuk-tag--green'
+
+  return ''
 }
