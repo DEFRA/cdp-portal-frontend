74e6f039072fdbf55767709b8cf6c80f68caff69
David Beale <DavidBeale@users.noreply.github.com>
2026-07-17 14:29:02 +0100

    Feature/core 3599 show resource request status (#1400)
    * Show recent non-pending request and their status
    
    * order by requestedAt
    
    * Clean up
    
    * lint

--- src/server/routes/requests/ResourcesService.js
+++ src/server/routes/requests/ResourcesService.js
@@ -1,9 +1,20 @@
 import { config } from '#config/config.js'
 import { fetchJson } from '#server/common/helpers/fetch/fetch-json.js'
+import { addDays } from 'date-fns'
 
 export async function getPendingResourceRequests(teamIds = []) {
   const teamFilters = teamIds.map((teamId) => `&teamIds=${teamId}`).join('')
   const endpoint = `${config.get('portalBackendUrl')}/resources/requests?status=requested&status=pending${teamFilters}`
+
+  const { payload = {} } = await fetchJson(endpoint)
+
+  return payload
+}
+
+export async function getRecentNonPendingResourceRequests(teamIds = []) {
+  const teamFilters = teamIds.map((teamId) => `&teamIds=${teamId}`).join('')
+  const OneDayOld = addDays(new Date(), -1).toISOString()
+  const endpoint = `${config.get('portalBackendUrl')}/resources/requests?modifiedAfter=${OneDayOld}&status=created&status=closed&status=failed${teamFilters}`
 
   const { payload = {} } = await fetchJson(endpoint)
 
