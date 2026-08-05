6b17e3f06118b904e70648e9b3c75c4e385eaf91
David Beale <david.beale@bealearts.co.uk>
2026-08-05 10:51:18 +0100

    Allow admin to create env specfic resources

--- src/server/routes/create/resources/domain/create-environment-options.js
+++ src/server/routes/create/resources/domain/create-environment-options.js
@@ -1,5 +1,12 @@
+import { formatText } from '#config/nunjucks/filters/filters.js'
+import { orderedEnvironments } from '@defra/cdp-validation-kit'
+
 export default [
   { value: 'tenants', text: 'Tenant environments' },
   { value: 'platform', text: 'Platform environments' },
-  { value: 'all', text: 'All environments' }
+  { value: 'all', text: 'All environments' },
+  ...orderedEnvironments.map((env) => ({
+    value: env,
+    text: `${formatText(env)} only`
+  }))
 ]
