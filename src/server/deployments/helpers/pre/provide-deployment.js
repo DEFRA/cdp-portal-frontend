085c22611a8fbfc2dc74f8077cbd56dc849c3d49
Lukasz Tymoszczuk <publicperson@gmail.com>
2026-08-06 13:05:13 +0100

    CORE-3744: fix: deployment team ownership from entities instead of GitHub-synced repository data

--- src/server/deployments/helpers/pre/provide-deployment.js
+++ src/server/deployments/helpers/pre/provide-deployment.js
@@ -2,25 +2,21 @@ import { fetchDeployment } from '../fetch/fetch-deployment.js'
 import { nullify404 } from '../../../common/helpers/nullify-404.js'
 import { provideDeploymentStatusClassname } from '../provide-deployment-status-classname.js'
 import { augmentStatus } from '../augment-status.js'
-import { fetchRepository } from '../../../common/helpers/fetch/fetch-repository.js'
+import { fetchEntity } from '../../../common/helpers/fetch/fetch-entities.js'
 
 const provideDeployment = {
   method: async function (request) {
     const deploymentId = request.params?.deploymentId
     const deployment = await fetchDeployment(deploymentId)
 
-    const repository = await fetchRepository(deployment?.service).catch(
-      nullify404
-    )
+    const entity = await fetchEntity(deployment?.service).catch(nullify404)
     const status = augmentStatus(deployment)
 
     return {
       ...deployment,
-      ...repository,
+      teams: entity?.teams?.filter((team) => team.teamId) ?? [],
       status,
-      statusClass: provideDeploymentStatusClassname(status),
-      isFrontend: repository?.topics?.includes('frontend') ?? false,
-      isBackend: repository?.topics?.includes('backend') ?? false
+      statusClass: provideDeploymentStatusClassname(status)
     }
   },
   assign: 'deployment'
