a264c8eb09cd0056a31cba03e3a34118fcaf58ac
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2026-08-10 11:24:35 +0100

    Fixes team edit flow
    Also changes github box from search to auto-complete

--- src/server/admin/teams/controllers/save/find-github-team.js
+++ src/server/admin/teams/controllers/save/find-github-team.js
@@ -1,5 +1,3 @@
-import qs from 'qs'
-
 import { sessionNames } from '#server/common/constants/session-names.js'
 import { buildErrorDetails } from '#server/common/helpers/build-error-details.js'
 import { githubTeamNameValidation } from '../../helpers/schema/github-team-name-validation.js'
@@ -8,9 +6,6 @@ const findGithubTeamController = {
   handler: async (request, h) => {
     const payload = request?.payload
     const button = payload?.button
-    const redirectLocation = payload?.redirectLocation
-
-    const githubSearch = payload?.githubSearch || null
     const github = payload?.github || null
 
     const validationResult = githubTeamNameValidation(button).validate(
@@ -21,29 +16,17 @@ const findGithubTeamController = {
     )
 
     const sanitisedPayload = {
-      githubSearch,
       github
     }
 
     if (validationResult?.error) {
       const errorDetails = buildErrorDetails(validationResult.error.details)
-
       request.yar.flash(sessionNames.validationFailure, {
         formValues: sanitisedPayload,
         formErrors: errorDetails
       })
 
-      const queryString = qs.stringify(
-        {
-          ...(redirectLocation && { redirectLocation }),
-          ...(githubSearch && { githubSearch })
-        },
-        {
-          addQueryPrefix: true
-        }
-      )
-
-      return h.redirect(`/admin/teams/find-github-team${queryString}`)
+      return h.redirect(`/admin/teams/find-github-team`)
     }
 
     if (!validationResult.error) {
--- src/server/admin/teams/controllers/save/start-edit-team.js
+++ src/server/admin/teams/controllers/save/start-edit-team.js
@@ -3,6 +3,7 @@ import Boom from '@hapi/boom'
 
 import { fetchCdpTeam } from '../../helpers/fetch/fetchers.js'
 import { teamIdValidation } from '@defra/cdp-validation-kit'
+import { sessionNames } from '#server/common/constants/session-names.js'
 
 const startEditTeamController = {
   options: {
@@ -14,15 +15,21 @@ const startEditTeamController = {
     }
   },
   handler: async (request, h) => {
+    request.yar.clear(sessionNames.validationFailure)
     const team = await fetchCdpTeam(request.params?.teamId)
 
-    await request.app.initStepData({
+    // Reset the multi-step form
+    await request.app.initStepData()
+
+    const data = {
       ...team,
       // user-service-backend supports multiple service codes - we want to just allow one service code for now
       ...(team.serviceCodes?.at(0) && { serviceCode: team.serviceCodes.at(0) }),
       isEdit: true
-    })
+    }
 
+    // Update multi-step with existing data
+    await request.app.initStepData(data)
     await request.yar.commit(h)
 
     return h.redirect(`/admin/teams/team-details`)
