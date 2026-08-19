2a7d626168ee65d20113e04e5b788c1b752ffb69
David Beale <david.beale@bealearts.co.uk>
2026-08-19 14:12:03 +0100

    icons

--- src/server/routes/services/{serviceId}/files/{path...}/route.js
+++ src/server/routes/services/{serviceId}/files/{path...}/route.js
@@ -8,6 +8,32 @@ export const options = {
   id: 'services/{serviceId}/files'
 }
 
+const data = [
+  {
+    name: 'cdp-portal-bakcend',
+    type: 'directory',
+    created: '2026-07-04:09:45:34',
+    modified: '2026-07-04:09:45:34',
+    items: [
+      {
+        name: 'migrations',
+        type: 'directory',
+        created: '2026-07-04:09:45:34',
+        modified: '2026-07-04:09:45:34',
+        items: [
+          {
+            name: 'my-file.json',
+            type: 'file',
+            size: '12500',
+            created: '2026-07-04:10:35:43',
+            modified: '2026-07-08:13:42:42'
+          }
+        ]
+      }
+    ]
+  }
+]
+
 export default async function (request, h) {
   const { path = '' } = request.params
   const entity = request.app.entity
@@ -20,10 +46,19 @@ export default async function (request, h) {
     return Boom.boomify(Boom.badRequest('Invalid path'))
   }
 
+  const relativePathParts = [
+    '/',
+    ...fsPath
+      .replace(serviceBasePath, '')
+      .split('/')
+      .filter((seg) => seg !== '')
+  ]
+
+  console.log(relativePathParts)
 
   return {
     entity,
-    fsPath,
+    relativePathParts,
     breadcrumbs: [
       {
         text: 'Services',
@@ -33,9 +68,14 @@ export default async function (request, h) {
         text: entity.name,
         href: `/services/${entity.name}`
       },
-      {
-        text: 'Files'
-      }
+      ...buildFsBreadcrumbs(entity, relativePathParts)
     ]
   }
+}
+
+function buildFsBreadcrumbs(entity, relativePathParts) {
+  return relativePathParts.map((path) => ({
+    text: path === '/' ? 'Files' : path,
+    href: `/services/${entity.name}${relativePathParts.join('/')}`
+  }))
 }
