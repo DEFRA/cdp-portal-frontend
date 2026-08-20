a0a3ae02ecbb625d4627a43823f15b94906021a4
David Beale <david.beale@bealearts.co.uk>
2026-08-20 15:39:04 +0100

    Fix / consistency

--- src/server/routes/services/{serviceId}/files/{path...}/route.js
+++ src/server/routes/services/{serviceId}/files/{path...}/route.js
@@ -22,19 +22,11 @@ export const options = {
 }
 
 export default async function (request, h) {
-  const { path = '/' } = request.params
+  const path = `/${request.params.path ?? ''}`
   const entity = request.app.entity
 
   const folderContents = await listPathContents(request, path)
-
-
   const relativePathParts = [...path.split('/').filter((seg) => seg !== '')]
-
-  // rows: [
-  //   [ { html: "<span style='display: flex; gap: 5px;'>" + appOpenFolderIcon({ classes: "app-icon--small" }) + "<a href='/'>..</a>" + "</span>" }, { text: '- - -' }, { text: '2026-07-08:13:42:42' }, { html: '<a class="app-link app-link--underline" href="/">New Folder</a>'} ],
-  //   [ { html: "<span style='display: flex; gap: 5px;'>" + appFolderIcon({ classes: "app-icon--small" }) + "<a href='/'>batch-one</a>" + "</span>"}, { text: '- - -' }, { text: '2026-07-08:13:42:42' }, { html: '<a class="app-link app-link--underline" href="/">Rename</a>&nbsp;&nbsp;<a class="app-link app-link--underline" href="/">Move</a>&nbsp;&nbsp;<a class="app-link app-link--underline" href="/">Delete</a>' } ],
-  //   [ { html: "<span style='display: flex; gap: 5px;'>" + appFileIcon({ classes: "app-icon--small" }) + "my-file.json" + "</span>" }, { text: '12.5K' }, { text: '2026-07-08:13:42:42' }, { html: '<a class="app-link app-link--underline" href="/">Download</a>&nbsp;&nbsp;<a class="app-link app-link--underline" href="/">Rename</a>&nbsp;&nbsp;<a class="app-link app-link--underline" href="/">Move</a>&nbsp;&nbsp;<a class="app-link app-link--underline" href="/">Delete</a>' } ]
-  // ]
 
   return {
     entity,
