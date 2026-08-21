deb8e06e5a5bb1ba5a09c69435c3d845223aa92b
David Beale <david.beale@bealearts.co.uk>
2026-08-21 15:03:46 +0100

    clean up

--- src/server/routes/services/{serviceId}/files-download/{path...}/route.js
+++ src/server/routes/services/{serviceId}/files-download/{path...}/route.js
@@ -1,6 +1,6 @@
 import { commonServiceExtensions } from '#server/common/helpers/ext/extensions.js'
-import { scopes, statusCodes } from '@defra/cdp-validation-kit'
-import { getFile, getFileUrl } from '../../files/BucketService.js'
+import { scopes } from '@defra/cdp-validation-kit'
+import { getFileUrl } from '../../files/BucketService.js'
 
 export const ext = [...commonServiceExtensions]
 
@@ -16,13 +16,6 @@ export const options = {
 export default async function (request, h) {
   const path = `/${request.params.path ?? ''}`
 
-  // const s3File = await getFile(request, path)
-
-  // return h
-  //   .response(s3File.Body)
-  //   .header('Content-Type', s3File.ContentType)
-  //   .code(statusCodes.ok)
-  //
   const url = await getFileUrl(request, path)
 
   return h.redirect(url)
