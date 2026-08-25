5d4e9ee7cce99bcac87a911efee0dba398eeac36
Lukasz Tymoszczuk <publicperson@gmail.com>
2026-08-25 11:39:36 +0100

    CORE-3804: Fix logging

--- src/server/common/patterns/entities/tabs/secrets/controllers/create.js
+++ src/server/common/patterns/entities/tabs/secrets/controllers/create.js
@@ -80,7 +80,7 @@ function createSecretController(entityKind) {
             type: 'success'
           })
         } catch (error) {
-          request.logger.error({ error }, 'Create secret call failed')
+          request.logger.error(error, 'Create secret call failed')
           request.yar.flash(
             sessionNames.globalValidationFailures,
             error.message
