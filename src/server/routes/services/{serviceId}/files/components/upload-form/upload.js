f1851c772e4c053088737309b406544daa1d5006
David Beale <david.beale@bealearts.co.uk>
2026-08-26 11:46:22 +0100

    Use comp attributes

--- src/server/routes/services/{serviceId}/files/components/upload-form/upload.js
+++ src/server/routes/services/{serviceId}/files/components/upload-form/upload.js
@@ -51,13 +51,12 @@ function startUpload() {
     progress = Math.round((bytes / fileSize) * 100)
 
     const $progress = document.getElementById(
-      'upload-progress-59112_292802ecca29e95423e18627cf2759da.jpg'
+      'upload-progress-58961_57dc36b325be5001a5710653b34efa1d.jpg'
     )
-    $progress.querySelector('.app-progress__indicator').style.width =
-      `${progress}%`
 
-    $progress.querySelector('.app-progress__readout').textContent =
-      `${bytes} / ${fileSize}`
+    $progress.setAttribute('data-progress', progress)
+    $progress.setAttribute('data-complete', bytes)
+
 
     if (progress >= 100) {
       clearInterval(interval)
