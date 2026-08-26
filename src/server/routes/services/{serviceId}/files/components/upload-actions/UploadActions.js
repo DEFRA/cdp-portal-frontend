f90fe21fd7893ac52c3d4b40bb3381a913ade0b3
David Beale <david.beale@bealearts.co.uk>
2026-08-26 16:09:39 +0100

    Try to load nested comps

---
+++ src/server/routes/services/{serviceId}/files/components/upload-actions/UploadActions.js
@@ -0,0 +1,19 @@
+import template from './template.njk'
+import NunjucksComponent from '#client/common/web-components/NunjucksComponent.js'
+
+import 'govuk-frontend/dist/govuk/components/button/macro.njk'
+import 'govuk-frontend/dist/govuk/components/button/template.njk'
+import 'govuk-frontend/dist/govuk/macros/attributes.njk'
+// import '#server/common/components/tag/macro.njk'
+
+export default class UploadActions extends NunjucksComponent {
+  static get observedAttributes() {
+    return ['data-status']
+  }
+
+  constructor() {
+    super(template)
+  }
+}
+
+window.customElements.define('upload-actions', UploadActions)
