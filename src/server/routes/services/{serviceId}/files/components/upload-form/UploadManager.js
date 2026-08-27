59ef4026f4eaf9b14b8e87f185a0920fc9c3e025
David Beale <david.beale@bealearts.co.uk>
2026-08-27 15:11:03 +0100

    init UploadManager

--- src/server/routes/services/{serviceId}/files/components/upload-form/UploadManager.js
+++ src/server/routes/services/{serviceId}/files/components/upload-form/UploadManager.js
@@ -1,26 +1,26 @@
 
 const fileSize = 129456
 
-function startUpload() {
-  let progress = 0
-  let bytes = 0
+export default class UploadManager {
+  startUpload(files) {
+    let progress = 0
+    let bytes = 0
 
-  const interval = setInterval(() => {
-    bytes += 10
-    progress = Math.round((bytes / fileSize) * 100)
+    const interval = setInterval(() => {
+      bytes += 10
+      progress = Math.round((bytes / fileSize) * 100)
 
-    const $progress = document.getElementById(
-      'upload-progress-58961_57dc36b325be5001a5710653b34efa1d.jpg'
-    )
+      const $progress = document.getElementById(
+        'upload-progress-58961_57dc36b325be5001a5710653b34efa1d.jpg'
+      )
 
-    $progress.setAttribute('data-progress', progress)
-    $progress.setAttribute('data-complete', bytes)
+      $progress.setAttribute('data-progress', progress)
+      $progress.setAttribute('data-complete', bytes)
 
 
-    if (progress >= 100) {
-      clearInterval(interval)
-    }
-  }, 1)
+      if (progress >= 100) {
+        clearInterval(interval)
+      }
+    }, 1)
+  }
 }
-
-window.startTest = startUpload
