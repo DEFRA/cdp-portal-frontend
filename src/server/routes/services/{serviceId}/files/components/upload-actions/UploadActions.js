fb7e211e513e66f7ad6fc38a7d0bf55f92b51377
David Beale <david.beale@bealearts.co.uk>
2026-08-27 12:44:43 +0100

    Resolve nunjucks component dependencies

--- src/server/routes/services/{serviceId}/files/components/upload-actions/UploadActions.js
+++ src/server/routes/services/{serviceId}/files/components/upload-actions/UploadActions.js
@@ -1,18 +1,6 @@
 import template from './template.njk'
 import NunjucksComponent from '#client/common/web-components/NunjucksComponent.js'
 
-import 'govuk-frontend/dist/govuk/components/button/macro.njk'
-import 'govuk-frontend/dist/govuk/components/button/template.njk'
-import 'govuk-frontend/dist/govuk/macros/attributes.njk'
-
-import '#server/common/components/tag/macro.njk'
-import '#server/common/components/tag/template.njk'
-import '#server/common/components/loader/macro.njk'
-import '#server/common/components/loader/template.njk'
-
-import 'govuk-frontend/dist/govuk/components/tag/macro.njk'
-import 'govuk-frontend/dist/govuk/components/tag/template.njk'
-
 export default class UploadActions extends NunjucksComponent {
   static get observedAttributes() {
     return ['data-status']
