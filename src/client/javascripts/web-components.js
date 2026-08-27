659e0b0308880e02870a983f54ec588e6d8073cc
David Beale <david.beale@bealearts.co.uk>
2026-08-27 13:01:51 +0100

    Init UploadForm WC

--- src/client/javascripts/web-components.js
+++ src/client/javascripts/web-components.js
@@ -1,2 +1,3 @@
 import '#server/common/components/progress/Progress.js'
 import '#server/routes/services/{serviceId}/files/components/upload-actions/UploadActions.js'
+import '#server/routes/services/{serviceId}/files/components/upload-form/UploadForm.js'
