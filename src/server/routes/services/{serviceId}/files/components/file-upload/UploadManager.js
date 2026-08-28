e634f96e8afddceaebbb1fc8dcddf732222570e1
David Beale <david.beale@bealearts.co.uk>
2026-08-28 15:16:55 +0100

    Upload url generation

--- src/server/routes/services/{serviceId}/files/components/file-upload/UploadManager.js
+++ src/server/routes/services/{serviceId}/files/components/file-upload/UploadManager.js
@@ -1,52 +1,56 @@
 export default class UploadManager extends EventTarget {
   #files
 
-  startUpload(files) {
+  startUpload(service, path, files, csrfToken) {
     this.#files = files
 
-    // TODO: Replace with actual upload
-    setInterval(() => {
-      for (const file of this.#files) {
-        file.status = file.status ?? 'uploading'
-        file.bytesDownloaded = file.bytesDownloaded ?? 0
+    for (const file of this.#files) {
+      this.#uploadFile(service, path, file, csrfToken)
+    }
 
-        file.bytesDownloaded += 100
+    // // TODO: Replace with actual upload
+    // setInterval(() => {
+    //   for (const file of this.#files) {
+    //     file.status = file.status ?? 'uploading'
+    //     file.bytesDownloaded = file.bytesDownloaded ?? 0
 
-        if (file.bytesDownloaded >= file.size) {
-          if (file.status !== 'complete') {
-            file.status = 'complete'
+    //     file.bytesDownloaded += 100
 
-            this.dispatchEvent(
-              new CustomEvent('complete', {
-                detail: {
-                  name: file.name,
-                  size: file.size,
-                  bytesUploaded: file.size,
-                  progress: 100,
-                  status: file.status
-                }
-              })
-            )
-          }
+    //     if (file.bytesDownloaded >= file.size) {
+    //       if (file.status !== 'complete') {
+    //         file.status = 'complete'
 
-          continue
-        }
+    //         this.dispatchEvent(
+    //           new CustomEvent('complete', {
+    //             detail: {
+    //               name: file.name,
+    //               size: file.size,
+    //               bytesUploaded: file.size,
+    //               progress: 100,
+    //               status: file.status
+    //             }
+    //           })
+    //         )
+    //       }
 
-        const progress = Math.round((file.bytesDownloaded / file.size) * 100)
+    //       continue
+    //     }
 
-        this.dispatchEvent(
-          new CustomEvent('progress', {
-            detail: {
-              name: file.name,
-              size: file.size,
-              bytesUploaded: file.bytesDownloaded,
-              progress,
-              status: file.status
-            }
-          })
-        )
-      }
-    }, 10)
+    //     const progress = Math.round((file.bytesDownloaded / file.size) * 100)
+
+    //     this.dispatchEvent(
+    //       new CustomEvent('progress', {
+    //         detail: {
+    //           name: file.name,
+    //           size: file.size,
+    //           bytesUploaded: file.bytesDownloaded,
+    //           progress,
+    //           status: file.status
+    //         }
+    //       })
+    //     )
+    //   }
+    // }, 10)
   }
 
   getFilesMeta() {
@@ -55,5 +59,31 @@ export default class UploadManager extends EventTarget {
       size,
       status
     }))
+  }
+
+  async #uploadFile(service, path, file, csrfToken) {
+    const response = await fetch(`/services/${service}/files-api/put-url`, {
+      method: 'POST',
+      cache: 'no-store',
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+        'Cache-Control': 'no-cache, no-store, max-age=0',
+        Expires: 'Thu, 1 Jan 1970 00:00:00 GMT',
+        Pragma: 'no-cache',
+        'X-CSRF-Token': csrfToken
+      },
+      body: JSON.stringify({
+        path
+      })
+    })
+
+    const { url } = await response.json()
+
+    console.log(url)
+    // upload
+  }
+
+  async #uploadLargeFile(service, path, file, csrfToken) {
+    // TODO: over a certain size, split the upload into parallel streams
   }
 }
