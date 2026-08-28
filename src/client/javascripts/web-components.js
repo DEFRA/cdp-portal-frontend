79798261db71cbb290ca2cf9c7f45854ecc70fcd
David Beale <david.beale@bealearts.co.uk>
2026-08-28 09:35:51 +0100

    Client render the upload progress

--- src/client/javascripts/web-components.js
+++ src/client/javascripts/web-components.js
@@ -1,4 +1,3 @@
 import '#server/common/components/progress/Progress.js'
 import '#server/routes/services/{serviceId}/files/components/upload-actions/UploadActions.js'
-import '#server/routes/services/{serviceId}/files/components/upload-form/UploadForm.js'
 import '#server/routes/services/{serviceId}/files/components/file-upload/FileUpload.js'
--- src/server/routes/services/{serviceId}/files/components/file-upload/FileUpload.js
+++ src/server/routes/services/{serviceId}/files/components/file-upload/FileUpload.js
@@ -1,17 +1,29 @@
 import template from './template.njk'
 import NunjucksComponent from '#client/common/web-components/NunjucksComponent.js'
 import { formatFileSize } from '#config/nunjucks/filters/filters.js'
+import UploadManager from './UploadManager.js'
+
+window.cdp = window.cdp ?? {}
+window.cdp.uploadManager = window.cdp.uploadManager ?? new UploadManager()
+
 
 export default class FileUpload extends NunjucksComponent {
+  #form
+  #onSubmitHandler
   #onProgressHandler
+
 
   constructor() {
     super(template)
 
+    this.#onSubmitHandler = this.#onSubmit.bind(this)
     this.#onProgressHandler = this.#onProgress.bind(this)
   }
 
   mounted() {
+    this.#form = this.querySelector('form')
+    this.#form.addEventListener('submit', this.#onSubmitHandler)
+
     window.cdp.uploadManager.addEventListener(
       'progress',
       this.#onProgressHandler
@@ -19,11 +31,28 @@ export default class FileUpload extends NunjucksComponent {
   }
 
   dismounted() {
+    this.#form.addEventListener('submit', this.#onSubmitHandler)
+
     window.cdp.uploadManager.removeEventListener(
       'progress',
       this.#onProgressHandler
     )
   }
+
+  #onSubmit(event) {
+    event.preventDefault()
+
+    this.#form.dataset.isSubmitting = 'true'
+
+    const files = this.#form.querySelector('input[name="files"]')?.files ?? []
+
+    window.cdp.uploadManager.startUpload(files)
+
+    this.render({
+      filesMeta: window.cdp.uploadManager.getFilesMeta()
+    })
+  }
+
 
   #onProgress(event) {
     const file = event.detail
