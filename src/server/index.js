955954b7d7d49d6b362709a87fb093dd346df4b0
David Beale <david.beale@bealearts.co.uk>
2026-08-28 15:38:42 +0100

    Handle upload failed

--- src/server/index.js
+++ src/server/index.js
@@ -100,7 +100,7 @@ async function createServer() {
     authOidcPlugin,
     sessionCookie,
     Scooter,
-    contentSecurityPolicy,
+    // contentSecurityPolicy,
     csrf,
     nunjucksConfig,
     sanitise,
