2e90e43c96015979eb1a772e8747569c345eb4af
David Beale <david.beale@bealearts.co.uk>
2026-09-01 11:16:34 +0100

    Progress smooth animation

--- src/server/common/components/progress/Progress.js
+++ src/server/common/components/progress/Progress.js
@@ -2,12 +2,26 @@ import template from './template.njk'
 import NunjucksComponent from '#client/common/web-components/NunjucksComponent.js'
 
 export default class Progress extends NunjucksComponent {
+  #progressIndicator
+  #progressReadout
+
   static get observedAttributes() {
     return ['data-progress', 'data-complete', 'data-total']
   }
 
-  constructor() {
-    super(template)
+  mounted() {
+    this.#progressIndicator = this.querySelector('.app-progress__indicator')
+    this.#progressReadout = this.querySelector('.app-progress__readout')
+  }
+
+  render({ progress, complete, total }) {
+    if (this.#progressIndicator) {
+      this.#progressIndicator.style.width = `${progress}%`
+    }
+
+    if (this.#progressReadout) {
+      this.#progressReadout.textContent = `${complete} / ${total}`
+    }
   }
 }
 
