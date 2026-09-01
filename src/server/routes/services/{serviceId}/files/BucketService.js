42c382467d44355bed03c857d95c2a105061e51c
David Beale <david.beale@bealearts.co.uk>
2026-09-01 12:18:37 +0100

    init multipart upload

--- src/server/routes/services/{serviceId}/files/BucketService.js
+++ src/server/routes/services/{serviceId}/files/BucketService.js
@@ -1,5 +1,9 @@
 import { config } from '#config/config.js'
-import { GetObjectCommand, ListObjectsV2Command, PutObjectCommand } from '@aws-sdk/client-s3'
+import {
+  GetObjectCommand,
+  ListObjectsV2Command,
+  PutObjectCommand
+} from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 
 const SIGNED_URL_TTL_SECONDS = 10
@@ -141,4 +145,13 @@ function formatAsS3Path(path = '', withTrailingSlash) {
   }
 
   return result
+}
+
+export async function startMultipartUpload(request, path) {
+  const { UploadId } = await request.s3Client.createMultipartUpload({
+    Bucket: bucket,
+    Key: formatAsS3Path(path)
+  })
+
+  return UploadId
 }
