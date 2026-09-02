39dde288d5dfad6c1eacfd9ce64a2655df2f7196
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2026-09-02 11:22:17 +0100

    (temp) restrict pgweb to CAS team

--- src/server/services/service/terminal/helpers/get-available-tools.js
+++ src/server/services/service/terminal/helpers/get-available-tools.js
@@ -1,18 +1,20 @@
 import { scopes } from '@defra/cdp-validation-kit'
-import { config } from '#config/config.js'
 
 export function getAvailableTools(entity, userScopes) {
   const isAdmin = userScopes.includes(scopes.admin)
-  const hasPostgres =
-    Object.values(entity.environments).some((env) => env.sql_database?.arn) &&
-    config.get('enablePgWeb')
+  const hasPostgres = Object.values(entity.environments).some(
+    (env) => env.sql_database?.arn
+  )
+
+  const canUsedPgWeb =
+    isAdmin || userScopes.includes('team:central-animal-store')
 
   const tools = [{ text: 'Terminal', value: 'terminal' }]
   if (isAdmin) {
     tools.push({ text: 'Terminal (latest)', value: 'terminal_latest' })
   }
 
-  if (hasPostgres) {
+  if (canUsedPgWeb && hasPostgres) {
     tools.push({ text: 'Postgres Web UI', value: 'pgweb' })
     if (isAdmin) {
       tools.push({ text: 'Postgres Web UI (latest)', value: 'pgweb_latest' })
