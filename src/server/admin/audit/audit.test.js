4cc5745225fd652c34e907dda3b166f3cc9cee8b
Lukasz Tymoszczuk <publicperson@gmail.com>
2026-09-14 12:33:07 +0100

    Fix flaky audit page test by freezing the clock (#1483)
    * fix flaky unit test by setting deterministic time for audit
    
    * clean up
    
    * clean up

--- src/server/admin/audit/audit.test.js
+++ src/server/admin/audit/audit.test.js
@@ -111,10 +111,14 @@ describe('Audit page', () => {
       }
     ])
     server = await initialiseServer()
+
+    vi.useFakeTimers({ advanceTimers: true })
+    vi.setSystemTime(new Date('2026-08-01T00:00:00.000Z'))
   })
 
   afterAll(async () => {
     await server.stop({ timeout: 0 })
+    vi.useRealTimers()
   })
 
   describe('list view', () => {
