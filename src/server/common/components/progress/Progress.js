95cd8f095e27e99a64781e15614c40207ffb0737
David Beale <david.beale@bealearts.co.uk>
2026-09-18 14:42:16 +0100

    Simplify WC render using a DOM morph lib

--- src/server/common/components/progress/Progress.js
+++ src/server/common/components/progress/Progress.js
@@ -1,27 +1,13 @@
-import './template.njk'
+import template from './template.njk'
 import NunjucksComponent from '#client/common/web-components/NunjucksComponent.js'
 
 export default class Progress extends NunjucksComponent {
-  #progressIndicator
-  #progressReadout
-
   static get observedAttributes() {
     return ['data-progress', 'data-complete', 'data-total']
   }
 
-  mounted() {
-    this.#progressIndicator = this.querySelector('.app-progress__indicator')
-    this.#progressReadout = this.querySelector('.app-progress__readout')
-  }
-
-  render({ progress, complete, total }) {
-    if (this.#progressIndicator) {
-      this.#progressIndicator.style.width = `${progress}%`
-    }
-
-    if (this.#progressReadout) {
-      this.#progressReadout.textContent = `${complete} / ${total}`
-    }
+  constructor() {
+    super(template)
   }
 }
 
