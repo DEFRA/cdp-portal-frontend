25dcfdd9b78bde140a1601e33ff51d49fc9413bd
David Beale <david.beale@bealearts.co.uk>
2026-09-22 14:39:18 +0100

    Test file upload

--- src/server/common/components/file-upload/FileUpload.browser.test.js
+++ src/server/common/components/file-upload/FileUpload.browser.test.js
@@ -1,4 +1,4 @@
-import { test, expect } from 'vitest'
+import { test, expect, vi } from 'vitest'
 import { page } from 'vitest/browser'
 
 import './FileUpload.js'
@@ -12,6 +12,7 @@ const component = `
 
 test('Renders correctly', async () => {
   const { element, getByRole } = await page.render(component)
+  window.cdp.uploadManager.getUploads = vi.fn().mockReturnValue(null)
   element.render()
 
   await expect
@@ -21,11 +22,40 @@ test('Renders correctly', async () => {
 
 test('Prompts to select at least one file', async () => {
   const { element, getByRole, getByText } = await page.render(component)
+  window.cdp.uploadManager.getUploads = vi.fn().mockReturnValue([])
   element.render()
 
   await getByRole('button', { name: 'Upload' }).click()
 
   await expect
-    .element(getByText('Please select at least one file.'))
+    .element(getByText('Error: Please select at least one file.'))
     .toBeInTheDocument()
+})
+
+test('Intercepts submit and starts the upload', async () => {
+  const { element, getByRole } = await page.render(component)
+  window.cdp.uploadManager.getUploads = vi.fn().mockReturnValue(null)
+  element.render()
+
+  window.cdp.uploadManager.startUpload = vi.fn()
+
+  await getByRole('button').first().upload('./README.md')
+
+  await getByRole('button', { name: 'Upload' }).click()
+  expect(window.cdp.uploadManager.startUpload).toHaveBeenCalled()
+})
+
+test('Renders upload progress', async () => {
+  const { element, getByText } = await page.render(component)
+  window.cdp.uploadManager.getUploads = vi.fn().mockReturnValue([
+    {
+      name: 'README.MD',
+      size: 3000,
+      bytesUploaded: 2000,
+      progress: 75
+    }
+  ])
+  element.render()
+
+  await expect.element(getByText('README.MD')).toBeInTheDocument()
 })
--- src/server/common/components/file-upload/upload-actions/UploadActions.browser.test.js
+++ src/server/common/components/file-upload/upload-actions/UploadActions.browser.test.js
@@ -1,4 +1,4 @@
-import { test, expect } from 'vitest'
+import { test, expect, vi } from 'vitest'
 import { page } from 'vitest/browser'
 
 import './UploadActions.js'
