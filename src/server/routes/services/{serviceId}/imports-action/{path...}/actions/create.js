b866faa18a4555ecc624458d4c597ab48a17e2b0
David Beale <david.beale@bealearts.co.uk>
2026-09-24 16:00:26 +0100

    Prevent existing name

--- src/server/routes/services/{serviceId}/imports-action/{path...}/actions/create.js
+++ src/server/routes/services/{serviceId}/imports-action/{path...}/actions/create.js
@@ -1,12 +1,27 @@
-import { createEmptyFolder } from '#server/common/services/bucket-service/BucketService.js'
+import {
+  createEmptyFolder,
+  listPathContents
+} from '#server/common/services/bucket-service/BucketService.js'
 import Joi from 'joi'
 
 export default {
-  async schema() {
+  async schema(request) {
+    const { path = '' } = request.params
+    const entity = request.app.entity
+
+    const resources = await listPathContents(
+      request,
+      `/entities/${entity.name}/imports/`,
+      path
+    )
+
+    const folders = resources.filter(({ isFolder }) => isFolder === true)
+
     return Joi.object({
       name: Joi.string()
         .label('Folder name')
         .description('Name of the new folder')
+        .invalid(...folders.map(({ name }) => name))
         .min(1)
         .max(100)
         .regex(/^[a-zA-Z0-9-_]+$/)
